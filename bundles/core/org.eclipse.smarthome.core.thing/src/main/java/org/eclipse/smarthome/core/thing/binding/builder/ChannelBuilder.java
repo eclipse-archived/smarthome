4bef2b919499ffd7b9ea08a0d2ae3cbda7c6f98c
Dennis Nobel <d.nobel@external.telekom.de>
2014-06-18 16:41:25 +0200

    Added Javadoc and license header.
    Signed-off-by: Dennis Nobel <d.nobel@external.telekom.de>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/binding/builder/ChannelBuilder.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/binding/builder/ChannelBuilder.java
@@ -11,6 +11,11 @@ import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 
+/**
+ * {@link ChannelBuilder} is responsible for creating {@link Channel}s.
+ * 
+ * @author Dennis Nobel - Initial contribution
+ */
 public class ChannelBuilder {
 
     private ChannelUID channelUID;
@@ -22,15 +27,36 @@ public class ChannelBuilder {
         this.acceptedItemType = acceptedItemType;
     }
 
+    /**
+     * Creates a channel builder for the given channel UID and item type.
+     * 
+     * @param channelUID
+     *            channel UID
+     * @param acceptedItemType
+     *            item type that is accepted by this channel
+     * @return channe builder
+     */
     public static ChannelBuilder create(ChannelUID channelUID, String acceptedItemType) {
         return new ChannelBuilder(channelUID, acceptedItemType);
     }
 
+    /**
+     * Appends a configuration to the channel to build.
+     * 
+     * @param configuration
+     *            configuration
+     * @return channel builder
+     */
     public ChannelBuilder withConfiguration(Configuration configuration) {
         this.configuration = configuration;
         return this;
     }
 
+    /**
+     * Builds and returns the channel.
+     * 
+     * @return channel
+     */
     public Channel build() {
         return new Channel(channelUID, acceptedItemType, configuration);
     }
