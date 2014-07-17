32aa534cac5921ce6a91f0805e518637c54d2229
Dennis Nobel <d.nobel@external.telekom.de>
2014-07-17 17:14:42 +0200

    Changed equals and hashCode method at GenericItem to use name only. Added equals and hashCode method to ThingImpl based on UID only.
    Signed-off-by: Dennis Nobel <d.nobel@external.telekom.de>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/ThingImpl.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/ThingImpl.java
@@ -168,4 +168,29 @@ public class ThingImpl implements Thing {
         return this.thingTypeUID;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((uid == null) ? 0 : uid.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ThingImpl other = (ThingImpl) obj;
+        if (uid == null) {
+            if (other.uid != null)
+                return false;
+        } else if (!uid.equals(other.uid))
+            return false;
+        return true;
+    }
+
 }
