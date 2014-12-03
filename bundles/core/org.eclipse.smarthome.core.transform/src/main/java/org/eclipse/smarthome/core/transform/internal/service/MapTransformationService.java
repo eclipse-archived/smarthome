473bbdef03bacc0324683540b2f7d7ca50af2ccf
Kai Kreuzer <kai@openhab.org>
2014-12-03 11:18:30 +0100

    fixed map transform service and improved file handling
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/core/org.eclipse.smarthome.core.transform/src/main/java/org/eclipse/smarthome/core/transform/internal/service/MapTransformationService.java
+++ bundles/core/org.eclipse.smarthome.core.transform/src/main/java/org/eclipse/smarthome/core/transform/internal/service/MapTransformationService.java
@@ -7,13 +7,24 @@
  */
 package org.eclipse.smarthome.core.transform.internal.service;
 
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.io.IOUtils;
 import org.eclipse.smarthome.config.core.ConfigConstants;
 import org.eclipse.smarthome.core.transform.TransformationException;
 import org.eclipse.smarthome.core.transform.TransformationService;
@@ -32,6 +43,22 @@ public class MapTransformationService implements TransformationService {
 
 	static final Logger logger = LoggerFactory.getLogger(MapTransformationService.class);
 
+	protected WatchService watchService = null;
+	
+	protected final Map<String, Properties> cachedProperties = new ConcurrentHashMap<>();
+	
+	protected void deactivate() {
+		cachedProperties.clear();
+		if(watchService != null) {
+			try {
+				watchService.close();
+			} catch (IOException e) {
+				logger.debug("Cannot cancel watch service for folder '{}'", getSourcePath());
+			}
+			watchService = null;
+		}
+	}
+	
 	/**
 	 * <p>
 	 * Transforms the input <code>source</code> by mapping it to another string. It expects the mappings to be read from a file which
@@ -54,27 +81,66 @@ public class MapTransformationService implements TransformationService {
 			throw new TransformationException("the given parameters 'filename' and 'source' must not be null");
 		}
 
-		Reader reader = null;
-		try {
-			String path = ConfigConstants.getConfigFolder() + File.separator + TransformationActivator.TRANSFORM_FOLDER_NAME + File.separator + filename;
-			Properties properties = new Properties();
-			reader = new FileReader(path);
-			properties.load(reader);
-			String target = properties.getProperty(source);
-			if(target!=null) {
-				logger.debug("transformation resulted in '{}'", target);
-				return target;
-			} else {
-				logger.warn("Could not find a mapping for '{}' in the file '{}'.", source, filename);
-				return "";
+		if (watchService == null) {
+			try {
+				initializeWatchService();
+			} catch (IOException e) {
+				// we cannot watch the folder, so let's at least clear the cache
+				cachedProperties.clear();
 			}
-		} catch (IOException e) {
-			String message = "opening file '" + filename + "' throws exception";
-			logger.error(message, e);
-			throw new TransformationException(message, e);
-		} finally {
-			IOUtils.closeQuietly(reader);
+		} else {
+			processFolderEvents();
 		}
+		Properties properties = cachedProperties.get(filename);
+		if (properties == null) {
+			String path = getSourcePath() + File.separator + filename;
+			try(Reader reader = new FileReader(path)) {
+				properties = new Properties();
+				properties.load(reader);
+				cachedProperties.put(filename, properties);
+			} catch (IOException e) {
+				String message = "opening file '" + filename + "' throws exception";
+				logger.error(message, e);
+				throw new TransformationException(message, e);
+			}
+		}		
+		String target = properties.getProperty(source);
+		if(target!=null) {
+			logger.debug("transformation resulted in '{}'", target);
+			return target;
+		} else {
+			logger.warn("Could not find a mapping for '{}' in the file '{}'.", source, filename);
+			return "";
+		}
+	}
+
+	private void processFolderEvents() {
+		WatchKey key = watchService.poll();
+		if (key != null) {
+			for(WatchEvent<?> e : key.pollEvents()) {
+				if (e.kind() == OVERFLOW) {
+					continue;
+				}
+
+				// Context for directory entry event is the file name of entry
+				@SuppressWarnings("unchecked")
+				WatchEvent<Path> ev = (WatchEvent<Path>) e;
+				Path path = ev.context();
+				logger.debug("Refreshing transformation file '{}'", path);
+				cachedProperties.remove(path.getFileName().toString());
+			}
+			key.reset();
+		}
+	}
+
+	private void initializeWatchService() throws IOException {
+		watchService = FileSystems.getDefault().newWatchService();
+		Path transformFilePath = Paths.get(getSourcePath()); 
+		transformFilePath.register(watchService, ENTRY_DELETE, ENTRY_MODIFY);
+	}
+
+	protected String getSourcePath() {
+		return ConfigConstants.getConfigFolder() + File.separator + TransformationActivator.TRANSFORM_FOLDER_NAME;
 	}
 
 }
