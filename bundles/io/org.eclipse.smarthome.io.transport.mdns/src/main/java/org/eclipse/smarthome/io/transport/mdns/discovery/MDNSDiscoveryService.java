65aae53127b340460f7437dcfb8abc3ca6afb542
Tobias Bräutigam <tbraeutigam@gmail.com>
2015-01-27 11:45:37 +0100

    initial contribution of a generic mDNS discovery approach
    Signed-off-by: Tobias Bräutigam <tbraeutigam@gmail.com>

---
+++ bundles/io/org.eclipse.smarthome.io.transport.mdns/src/main/java/org/eclipse/smarthome/io/transport/mdns/discovery/MDNSDiscoveryService.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.transport.mdns.discovery;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+
+import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
+import org.eclipse.smarthome.config.discovery.DiscoveryResult;
+import org.eclipse.smarthome.config.discovery.DiscoveryService;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.eclipse.smarthome.io.transport.mdns.MDNSClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is a {@link DiscoveryService} implementation, which can find mDNS services in the network.
+ * Support for further devices can be added by implementing and registering a {@link MDNSDiscoveryParticipant}.
+ *  
+ * @author Tobias Bräutigam - Initial contribution
+ *
+ */
+public class MDNSDiscoveryService extends AbstractDiscoveryService implements ServiceListener {
+	private final Logger logger = LoggerFactory.getLogger(MDNSDiscoveryService.class);
+	
+	private Set<MDNSDiscoveryParticipant> participants = new CopyOnWriteArraySet<>();
+	
+	private MDNSClient mdnsClient;
+
+	public MDNSDiscoveryService() {
+		super(5);
+	}			
+
+	
+	public void setMDNSClient(MDNSClient mdnsClient) {
+		this.mdnsClient = mdnsClient;
+		startScan();
+	}
+	
+	public void unsetMDNSClient(MDNSClient mdnsClient) {
+		this.mdnsClient = null;
+	}
+	
+	protected void activate() {
+		
+	}
+
+	@Override
+	protected void startScan() {
+		logger.debug("mDNS discovery service started");
+		initializeParticipants();
+	}
+		
+	protected void initializeParticipants() {
+		for(MDNSDiscoveryParticipant participant : participants) {
+			mdnsClient.getClient().removeServiceListener(participant.getServiceType(), this);
+			ServiceInfo[] services = mdnsClient.getClient().list(participant.getServiceType());
+			logger.debug(services.length+" services found for "+participant.getServiceType());
+			for(ServiceInfo service : services) {
+				participant.createResult(service);
+			}
+			mdnsClient.getClient().addServiceListener(participant.getServiceType(), this);
+		}
+	}
+	
+	protected void addMdnsDiscoveryParticipant(MDNSDiscoveryParticipant participant) {
+		this.participants.add(participant);
+		logger.debug("adding mDNS listener to type: "+participant.getServiceType());
+		if (mdnsClient!=null && mdnsClient.getClient() != null) {
+			
+			ServiceInfo[] services = mdnsClient.getClient().list(participant.getServiceType());
+			
+			logger.debug(services.length+" services found");
+			for(ServiceInfo service : services) {
+				participant.createResult(service);
+			}
+			mdnsClient.getClient().addServiceListener(participant.getServiceType(), this);
+		}
+	}
+
+	protected void removeMdnsDiscoveryParticipant(MDNSDiscoveryParticipant participant) {
+		this.participants.remove(participant);
+		mdnsClient.getClient().removeServiceListener(participant.getServiceType(), this);
+	}
+	
+	@Override
+	public Set<ThingTypeUID> getSupportedThingTypes() {
+		Set<ThingTypeUID> supportedThingTypes = new HashSet<>();
+		for(MDNSDiscoveryParticipant participant : participants) {
+			supportedThingTypes.addAll(participant.getSupportedThingTypeUIDs());
+		}
+		return supportedThingTypes;
+	}
+
+	@Override
+	public void serviceAdded(ServiceEvent serviceEvent) {
+		for(MDNSDiscoveryParticipant participant : participants) {
+			try {
+				DiscoveryResult result = participant.createResult(serviceEvent.getInfo());
+				if(result!=null) {
+					thingDiscovered(result);
+				}
+			} catch(Exception e) {
+				logger.error("Participant '{}' threw an exception", participant.getClass().getName(), e);
+			}
+		}	   
+	}
+
+	@Override
+	public void serviceRemoved(ServiceEvent serviceEvent) {
+		for(MDNSDiscoveryParticipant participant : participants) {
+			try {
+				ThingUID thingUID = participant.getThingUID(serviceEvent.getInfo());
+				if(thingUID!=null) {
+					thingRemoved(thingUID);
+				}
+			} catch(Exception e) {
+				logger.error("Participant '{}' threw an exception", participant.getClass().getName(), e);
+			}
+		}
+	}
+
+	@Override
+	public void serviceResolved(ServiceEvent serviceEvent) {
+		
+	}
+
+}
---
+++ bundles/io/org.eclipse.smarthome.io.transport.mdns/src/main/java/org/eclipse/smarthome/io/transport/mdns/internal/MDNSServiceImpl.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.transport.mdns.internal;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.Executors;
+
+import javax.jmdns.ServiceInfo;
+
+import org.eclipse.smarthome.io.transport.mdns.MDNSClient;
+import org.eclipse.smarthome.io.transport.mdns.MDNSService;
+import org.eclipse.smarthome.io.transport.mdns.ServiceDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class starts the JmDNS and implements interface to register and
+ * unregister services.
+ * 
+ * @author Victor Belov
+ *
+ */
+public class MDNSServiceImpl implements MDNSService {
+
+	private final Logger logger = LoggerFactory
+			.getLogger(MDNSServiceImpl.class);
+	private MDNSClient mdnsClient;
+
+	private Set<ServiceInfo> servicesToRegisterQueue = new CopyOnWriteArraySet<>();
+
+	public MDNSServiceImpl() {
+	}
+
+	public void setMDNSClient(MDNSClient client) {
+		this.mdnsClient = client;
+		// register queued services
+		if (servicesToRegisterQueue.size()>0) {
+			Runnable runnable = new Runnable() {
+				public void run() {
+					for(ServiceInfo serviceInfo : servicesToRegisterQueue) {
+						try {
+							logger.debug("Registering new service "
+									+ serviceInfo.getType() + " at port "
+									+ String.valueOf(serviceInfo.getPort()));
+							mdnsClient.getClient().registerService(serviceInfo);
+						} catch (IOException e) {
+							logger.error(e.getMessage());
+						}
+					}
+					servicesToRegisterQueue.clear();
+				}
+			};
+			Executors.newSingleThreadExecutor().execute(runnable);
+		}
+	}
+
+	public void unsetMDNSClient(MDNSClient mdnsClient) {
+		this.mdnsClient = null;
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public void registerService(final ServiceDescription description) {
+		if (mdnsClient == null) {
+			// queue the service to register it as soon as the mDNS client is
+			// available
+			ServiceInfo serviceInfo = ServiceInfo.create(
+					description.serviceType, description.serviceName,
+					description.servicePort, 0, 0,
+					description.serviceProperties);
+			servicesToRegisterQueue.add(serviceInfo);
+		} else {
+			Runnable runnable = new Runnable() {
+				public void run() {
+					ServiceInfo serviceInfo = ServiceInfo.create(
+							description.serviceType, description.serviceName,
+							description.servicePort, 0, 0,
+							description.serviceProperties);
+					try {
+						logger.debug("Registering new service "
+								+ description.serviceType + " at port "
+								+ String.valueOf(description.servicePort));
+						mdnsClient.getClient().registerService(serviceInfo);
+					} catch (IOException e) {
+						logger.error(e.getMessage());
+					}
+
+				}
+			};
+			Executors.newSingleThreadExecutor().execute(runnable);
+		}
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	public void unregisterService(ServiceDescription description) {
+		if (mdnsClient==null) return;
+		ServiceInfo serviceInfo = ServiceInfo.create(description.serviceType,
+				description.serviceName, description.servicePort, 0, 0,
+				description.serviceProperties);
+		logger.debug("Unregistering service " + description.serviceType
+				+ " at port " + String.valueOf(description.servicePort));
+		mdnsClient.getClient().unregisterService(serviceInfo);
+	}
+
+	/**
+	 * This method unregisters all services from Bonjour/MDNS
+	 */
+	protected void unregisterAllServices() {
+		if (mdnsClient!=null)
+			mdnsClient.getClient().unregisterAllServices();
+	}
+
+	public void activate() {
+
+	}
+
+	public void deactivate() {
+		unregisterAllServices();
+		try {
+			if (mdnsClient!=null) {
+				mdnsClient.getClient().close();
+				logger.debug("mDNS service has been stopped");
+			}
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+}
