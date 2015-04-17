a4a04c44945fb39bb32a431fa4b4f940ba13a69f
Thomas Höfer <t.hoefer@telekom.de>
2015-04-17 18:24:48 +0200

    added discovery result representation property
    Signed-off-by: Thomas Höfer <t.hoefer@telekom.de>

--- bundles/config/org.eclipse.smarthome.config.discovery.test/src/test/groovy/org/eclipse/smarthome/config/setup/test/discovery/DiscoveryResultImplTest.groovy
+++ bundles/config/org.eclipse.smarthome.config.discovery.test/src/test/groovy/org/eclipse/smarthome/config/setup/test/discovery/DiscoveryResultImplTest.groovy
@@ -21,31 +21,32 @@ import org.junit.Test
  * and the synchronization of {@link DiscoveryResult}s work in a correct way.
  * 
  * @author Michael Grammling - Initial Contribution
+ * @author Thomas Höfer - Added representation
  */
 class DiscoveryResultImplTest {
-    
+
     def DEFAULT_TTL = 60
 
     @Test
     public void testInvalidConstructorForThingType() {
         try {
-            new DiscoveryResultImpl(new ThingUID("aa"), null, null, null, DEFAULT_TTL)
+            new DiscoveryResultImpl(new ThingUID("aa"), null, null, null, null, DEFAULT_TTL)
             fail "The constructor must throw an IllegalArgumentException if null is used"
             + " as Thing type!"
-        } catch (IllegalArgumentException iae) {
+        } catch (IllegalArgumentException expected) {
         }
     }
 
     @Test
     public void testInvalidConstructorForTTL() {
         try {
-       def thingTypeUID = new ThingTypeUID("bindingId", "thingType")
+            def thingTypeUID = new ThingTypeUID("bindingId", "thingType")
 
-        DiscoveryResultImpl discoveryResult =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, null, null, -2)
+            DiscoveryResultImpl discoveryResult =
+                    new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, null, null, null, -2)
             fail "The constructor must throw an IllegalArgumentException if negative value is used"
             + " as ttl!"
-        } catch (IllegalArgumentException iae) {
+        } catch (IllegalArgumentException expected) {
         }
     }
 
@@ -54,15 +55,16 @@ class DiscoveryResultImplTest {
         def thingTypeUID = new ThingTypeUID("bindingId", "thingType")
 
         DiscoveryResultImpl discoveryResult =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, null, null, DEFAULT_TTL)
+                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, null, null, null, DEFAULT_TTL)
 
         assertEquals("bindingId:thingType", discoveryResult.getThingTypeUID().toString())
         assertEquals("bindingId:thingType:thingId", discoveryResult.getThingUID().toString())
         assertEquals("bindingId", discoveryResult.getBindingId())
         assertEquals("", discoveryResult.getLabel())
         assertEquals(DiscoveryResultFlag.NEW, discoveryResult.getFlag())
-        
+
         assertNotNull("The properties must never be null!", discoveryResult.getProperties())
+        assertNull(discoveryResult.getRepresentationProperty())
     }
 
     @Test
@@ -71,58 +73,62 @@ class DiscoveryResultImplTest {
         def thingTypeUID = new ThingTypeUID("bindingId", "thingType")
         def discoveryResultSourceMap = [ "ipAddress" : "127.0.0.1" ]
         DiscoveryResultImpl discoveryResult =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultSourceMap, "TARGET", DEFAULT_TTL)
+                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultSourceMap, "ipAddress", "TARGET", DEFAULT_TTL)
 
         discoveryResult.setFlag(DiscoveryResultFlag.IGNORED)
 
         discoveryResult.synchronize(null)
 
         assertEquals("127.0.0.1", discoveryResult.getProperties().get("ipAddress"))
+        assertEquals("ipAddress", discoveryResult.getRepresentationProperty())
         assertEquals("TARGET", discoveryResult.getLabel())
         assertEquals(DiscoveryResultFlag.IGNORED, discoveryResult.getFlag())
     }
 
     @Test
     public void testIrrelevantSynchronize() {
-        
+
         def thingTypeUID = new ThingTypeUID("bindingId", "thingType")
         def discoveryResultSourceMap = [ "ipAddress" : "127.0.0.1" ]
         DiscoveryResultImpl discoveryResult =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultSourceMap, "TARGET", DEFAULT_TTL)
+                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultSourceMap, "ipAddress", "TARGET", DEFAULT_TTL)
 
         discoveryResult.setFlag(DiscoveryResultFlag.IGNORED)
 
         DiscoveryResultImpl discoveryResultSource =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "anotherThingId"), null, null, null, DEFAULT_TTL)
-        
+                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "anotherThingId"), null, null, null, null, DEFAULT_TTL)
+
 
         discoveryResult.synchronize(discoveryResultSource)
 
         assertEquals("127.0.0.1", discoveryResult.getProperties().get("ipAddress"))
+        assertEquals("ipAddress", discoveryResult.getRepresentationProperty())
         assertEquals("TARGET", discoveryResult.getLabel())
         assertEquals(DiscoveryResultFlag.IGNORED, discoveryResult.getFlag())
     }
 
     @Test
     public void testSynchronize() {
-        
+
         def thingTypeUID = new ThingTypeUID("bindingId", "thingType")
         def discoveryResultSourceMap = [ "ipAddress" : "127.0.0.1" ]
         DiscoveryResultImpl discoveryResult =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultSourceMap, "TARGET", DEFAULT_TTL)
+                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultSourceMap, "ipAddress", "TARGET", DEFAULT_TTL)
 
         discoveryResult.setFlag(DiscoveryResultFlag.IGNORED)
-        
-        def discoveryResultMap = [ "ipAddress" : "192.168.178.1" ]
-        DiscoveryResultImpl discoveryResultSource =
-                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultMap, "SOURCE", DEFAULT_TTL)
 
-  
+        def discoveryResultMap = [ "ipAddress" : "192.168.178.1", "macAddress" : "AA:BB:CC:DD:EE:FF" ]
+        DiscoveryResultImpl discoveryResultSource =
+                new DiscoveryResultImpl(new ThingUID(thingTypeUID, "thingId"), null, discoveryResultMap, "macAddress", "SOURCE", DEFAULT_TTL)
+
+
         discoveryResultSource.setFlag(DiscoveryResultFlag.NEW)
 
         discoveryResult.synchronize(discoveryResultSource);
 
         assertEquals("192.168.178.1", discoveryResult.getProperties().get("ipAddress"))
+        assertEquals("AA:BB:CC:DD:EE:FF", discoveryResult.getProperties().get("macAddress"))
+        assertEquals("macAddress", discoveryResult.getRepresentationProperty())
         assertEquals("SOURCE", discoveryResult.getLabel())
         assertEquals(DiscoveryResultFlag.IGNORED, discoveryResult.getFlag())
     }
--- bundles/config/org.eclipse.smarthome.config.discovery/src/main/java/org/eclipse/smarthome/config/discovery/internal/DiscoveryResultImpl.java
+++ bundles/config/org.eclipse.smarthome.config.discovery/src/main/java/org/eclipse/smarthome/config/discovery/internal/DiscoveryResultImpl.java
@@ -24,10 +24,11 @@ public class DiscoveryResultImpl implements DiscoveryResult {
     private ThingUID thingUID;
 
     private Map<String, Object> properties;
+    private String representationProperty;
     private DiscoveryResultFlag flag;
     private String label;
-    private long timestamp; 
-    private long timeToLive = TTL_UNLIMITED; 
+    private long timestamp;
+    private long timeToLive = TTL_UNLIMITED;
 
     /**
      * Package protected default constructor to allow reflective instantiation.
@@ -44,6 +45,7 @@ public class DiscoveryResultImpl implements DiscoveryResult {
      *            must be created. A typical {@code Thing} ID could be the
      *            serial number. It's usually <i>not</i> a product name.
      * @param properties the properties to be set (could be null or empty)
+     * @param representationProperty the representationProperty to be set (could be null or empty)
      * @param label the human readable label to set (could be null or empty)
      * @param bridgeUID the unique bridge ID to be set
      * @param timeToLive time to live in seconds
@@ -51,8 +53,8 @@ public class DiscoveryResultImpl implements DiscoveryResult {
      * @throws IllegalArgumentException
      *             if the Thing type UID or the Thing UID is null
      */
-    public DiscoveryResultImpl(ThingUID thingUID, ThingUID bridgeUID, Map<String, Object> properties, String label, long timeToLive)
-            throws IllegalArgumentException {
+    public DiscoveryResultImpl(ThingUID thingUID, ThingUID bridgeUID, Map<String, Object> properties,
+            String representationProperty, String label, long timeToLive) throws IllegalArgumentException {
 
         if (thingUID == null) {
             throw new IllegalArgumentException("The thing UID must not be null!");
@@ -60,16 +62,17 @@ public class DiscoveryResultImpl implements DiscoveryResult {
         if (timeToLive < 1 && timeToLive != TTL_UNLIMITED) {
             throw new IllegalArgumentException("The ttl must not be 0 or negative!");
         }
-        
+
         this.thingUID = thingUID;
 
         this.bridgeUID = bridgeUID;
         this.properties = Collections.unmodifiableMap((properties != null) ? new HashMap<>(properties)
                 : new HashMap<String, Object>());
+        this.representationProperty = representationProperty;
         this.label = label == null ? "" : label;
-        
-        this.timestamp = new Date().getTime(); 
-        this.timeToLive = timeToLive; 
+
+        this.timestamp = new Date().getTime();
+        this.timeToLive = timeToLive;
 
         this.flag = DiscoveryResultFlag.NEW;
     }
@@ -132,6 +135,21 @@ public class DiscoveryResultImpl implements DiscoveryResult {
     }
 
     /**
+     * Returns the representation property of this result object.
+     * <p>
+     * The representation property represents an unique human and/or machine readable identifier of the thing that was
+     * discovered. Its actual value can be retrieved from the {@link DiscoveryResult#getProperties()} map. Such unique
+     * identifiers are among others the <code>ipAddress</code>, the <code>macAddress</code> or the
+     * <code>serialNumber</code> of the discovered thing.
+     * 
+     * @return the representation property of this result object (could be null)
+     */
+    @Override
+    public String getRepresentationProperty() {
+        return this.representationProperty;
+    }
+
+    /**
      * Returns the flag of this result object.<br>
      * The flag signals e.g. if the result is {@link DiscoveryResultFlag#NEW} or has been marked as
      * {@link DiscoveryResultFlag#IGNORED}. In the latter
@@ -179,8 +197,9 @@ public class DiscoveryResultImpl implements DiscoveryResult {
         if ((sourceResult != null) && (sourceResult.getThingUID().equals(this.thingUID))) {
 
             this.properties = sourceResult.getProperties();
+            this.representationProperty = sourceResult.getRepresentationProperty();
             this.label = sourceResult.getLabel();
-            this.timestamp = new Date().getTime(); 
+            this.timestamp = new Date().getTime();
             this.timeToLive = sourceResult.getTimeToLive();
         }
     }
@@ -230,7 +249,7 @@ public class DiscoveryResultImpl implements DiscoveryResult {
         return "DiscoveryResult [thingUID=" + thingUID + ", properties=" + properties + ", flag=" + flag + ", label="
                 + label + ", bridgeUID=" + bridgeUID + ", ttl=" + timeToLive + ", timestamp=" + timestamp + "]";
     }
-    
+
     @Override
     public long getTimestamp() {
         return timestamp;
