a46f0299cb0f75ca05b37948e3e701242526fe42
Chris Jackson <chris@cd-jackson.com>
2015-05-20 17:48:54 +0100

    enhance getStateAs for DimmerItem and RollershutterItem
    This commit also contains work of 'Chris Jackson <chris@cd-jackson.com>'
    who started the SwitchItemTest and DimmerItemTest implementation.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=467739
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

---
+++ bundles/core/org.eclipse.smarthome.core.library.test/src/test/java/org/eclipse/smarthome/core/library/items/SwitchItemTest.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.core.library.items;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.smarthome.core.library.types.OnOffType;
+import org.eclipse.smarthome.core.library.types.PercentType;
+import org.junit.Test;
+
+/**
+ * @author Chris Jackson - Initial contribution
+ */
+public class SwitchItemTest {
+
+    @Test
+    public void getAsPercentFromSwitch() {
+        SwitchItem item = new SwitchItem("Test");
+        item.setState(OnOffType.ON);
+        assertEquals("100", item.getStateAs(PercentType.class).toString());
+    }
+}
