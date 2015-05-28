e021157b77abbbdbe32f7a77bc866119eb18d807
Kai Kreuzer <kai@openhab.org>
2015-05-28 13:36:46 +0200

    fixed problem that ItemRegistry returned a new instance of items, without sending any events to change listeners
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=468545
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/core/org.eclipse.smarthome.core.test/src/test/groovy/org/eclipse/smarthome/core/internal/ItemRegistryOSGiTest.groovy
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/groovy/org/eclipse/smarthome/core/internal/ItemRegistryOSGiTest.groovy
@@ -21,6 +21,7 @@ import org.eclipse.smarthome.core.items.ItemRegistryChangeListener
 import org.eclipse.smarthome.core.items.ItemsChangeListener
 import org.eclipse.smarthome.core.library.items.NumberItem
 import org.eclipse.smarthome.core.library.items.SwitchItem
+import org.eclipse.smarthome.core.library.items.StringItem
 import org.eclipse.smarthome.test.OSGiTest
 import org.junit.Before
 import org.junit.Test
@@ -30,23 +31,24 @@ import org.junit.Test
  * 
  * @author Dennis Nobel - Initial contribution
  * @author Andre Fuechsel - extended with tag tests
+ * @author Kai Kreuzer - added tests for all items changed cases
  */
 class ItemRegistryOSGiTest extends OSGiTest {
 
-	ItemRegistry itemRegistry
-	ItemProvider itemProvider
-	ItemsChangeListener itemsChangeListener
-	def ITEM_NAME = "switchItem"
+    ItemRegistry itemRegistry
+    ItemProvider itemProvider
+    ItemsChangeListener itemsChangeListener
+    def ITEM_NAME = "switchItem"
     def CAMERA_ITEM_NAME1 = "cameraItem1"
     def CAMERA_ITEM_NAME2 = "cameraItem2"
     def CAMERA_ITEM_NAME3 = "cameraItem3"
     def CAMERA_TAG = "camera"
     def SENSOR_TAG = "sensor"
     def OTHER_TAG = "other"
-    
-	@Before
-	void setUp() {
-		itemRegistry = getService(ItemRegistry)
+
+    @Before
+    void setUp() {
+        itemRegistry = getService(ItemRegistry)
         def cameraItem1 = new SwitchItem(CAMERA_ITEM_NAME1)
         def cameraItem2 = new SwitchItem(CAMERA_ITEM_NAME2)
         def cameraItem3 = new NumberItem(CAMERA_ITEM_NAME3)
@@ -54,12 +56,19 @@ class ItemRegistryOSGiTest extends OSGiTest {
         cameraItem2.addTag(CAMERA_TAG)
         cameraItem2.addTag(SENSOR_TAG)
         cameraItem3.addTag(CAMERA_TAG)
-		itemProvider = [
-			getAll: {[new SwitchItem(ITEM_NAME), cameraItem1, cameraItem2, cameraItem3 ]}, 
-			addProviderChangeListener: {def icl -> itemsChangeListener = icl},
-			removeProviderChangeListener: {def icl -> itemsChangeListener = icl },
-			allItemsChanged: {}] as ItemProvider
-	}
+        itemProvider = [
+            getAll: {
+                [
+                    new SwitchItem(ITEM_NAME),
+                    cameraItem1,
+                    cameraItem2,
+                    cameraItem3
+                ]
+            },
+            addProviderChangeListener: {def icl -> itemsChangeListener = icl},
+            removeProviderChangeListener: {def icl -> itemsChangeListener = icl },
+            allItemsChanged: {}] as ItemProvider
+    }
 
     @Test
     void 'assert getItems returns item from registered ItemProvider'() {
@@ -76,7 +85,7 @@ class ItemRegistryOSGiTest extends OSGiTest {
 
         assertThat itemRegistry.getItems().size(), is(0)
     }
-    
+
     @Test
     void 'assert getItemsOfType returns item from registered ItemProvider'() {
 
@@ -92,7 +101,7 @@ class ItemRegistryOSGiTest extends OSGiTest {
 
         assertThat itemRegistry.getItems().size(), is(0)
     }
-    
+
     @Test
     void 'assert getItemsByTag returns item from registered ItemProvider'() {
 
@@ -105,7 +114,7 @@ class ItemRegistryOSGiTest extends OSGiTest {
         assertThat items.first().name, is(equalTo(CAMERA_ITEM_NAME1))
         assertThat items.get(1).name, is(equalTo(CAMERA_ITEM_NAME2))
         assertThat items.last().name, is(equalTo(CAMERA_ITEM_NAME3))
-        
+
         unregisterService itemProvider
 
         assertThat itemRegistry.getItems().size(), is(0)
@@ -122,7 +131,7 @@ class ItemRegistryOSGiTest extends OSGiTest {
         assertThat items.size(), is(2)
         assertThat items.first().name, is(equalTo(CAMERA_ITEM_NAME1))
         assertThat items.last().name, is(equalTo(CAMERA_ITEM_NAME2))
-        
+
         unregisterService itemProvider
 
         assertThat itemRegistry.getItems().size(), is(0)
@@ -138,7 +147,7 @@ class ItemRegistryOSGiTest extends OSGiTest {
         def items = itemRegistry.getItemsByTag(CAMERA_TAG, SENSOR_TAG)
         assertThat items.size(), is(1)
         assertThat items.first().name, is(equalTo(CAMERA_ITEM_NAME2))
-        
+
         unregisterService itemProvider
 
         assertThat itemRegistry.getItems().size(), is(0)
@@ -153,90 +162,121 @@ class ItemRegistryOSGiTest extends OSGiTest {
 
         def items = itemRegistry.getItemsByTag(OTHER_TAG)
         assertThat items.size(), is(0)
-        
+
         unregisterService itemProvider
 
         assertThat itemRegistry.getItems().size(), is(0)
     }
 
     @Test
-	void 'assert itemRegistry sets and removes members of GroupItems'() {
+    void 'assert itemRegistry sets and removes members of GroupItems'() {
 
-		assertThat itemRegistry.getItems().size(), is(0)
+        assertThat itemRegistry.getItems().size(), is(0)
 
-		registerService itemProvider
+        registerService itemProvider
 
-		// test added item with group name is added as member to group
-		itemsChangeListener.added(itemProvider, new GroupItem("group"))
-		SwitchItem switchItem = new SwitchItem("switch")
-		switchItem.addGroupName("group")
-		itemsChangeListener.added(itemProvider, switchItem)
+        // test added item with group name is added as member to group
+        itemsChangeListener.added(itemProvider, new GroupItem("group"))
+        SwitchItem switchItem = new SwitchItem("switch")
+        switchItem.addGroupName("group")
+        itemsChangeListener.added(itemProvider, switchItem)
 
 
-		def groupItem = (itemRegistry.getItem("group") as GroupItem)
-		assertThat groupItem.members.contains(switchItem), is(true)
+        def groupItem = (itemRegistry.getItem("group") as GroupItem)
+        assertThat groupItem.members.contains(switchItem), is(true)
 
-		// test removed item is removed as member from group
-		itemsChangeListener.removed(itemProvider, switchItem)
-		assertThat groupItem.members.contains(switchItem), is(false)
-		
-		// test added group item with gets all members set when it is added at last
-		switchItem.addGroupName("group2")
-		itemsChangeListener.added(itemProvider, switchItem)
-		itemsChangeListener.added(itemProvider, new GroupItem("group2"))
-		
-		def groupItem2 = (itemRegistry.getItem("group2") as GroupItem)
-		assertThat groupItem2.members.contains(switchItem), is(true)
-		
-		// test update item
-		itemsChangeListener.added(itemProvider, new GroupItem("group3"))
-		def groupItem3 = (itemRegistry.getItem("group") as GroupItem)
-		
-		SwitchItem updatedSwitchItem = new SwitchItem("switch")
-		
-		updatedSwitchItem.addGroupName("group")
-		updatedSwitchItem.addGroupName("group3")
-		
-		// old item has group: [group, group2], new item has [group, group3]
-		itemsChangeListener.updated(itemProvider, switchItem, updatedSwitchItem)
-		
-		assertThat groupItem.members.contains(updatedSwitchItem), is(true)
-		assertThat groupItem2.members.contains(updatedSwitchItem), is(false)
-		assertThat groupItem3.members.contains(updatedSwitchItem), is(true)
-	}
-	
-	@Test
-	void 'assert itemRegistryChangeListeners are informed about item changes'() {
-		registerService itemProvider
-		
-		def itemAddedCalled = false
-		def itemRemovedCalled = false
-		def itemUpdatedCalled = false
-		
-		itemRegistry.addRegistryChangeListener([
-			added: {Item item -> itemAddedCalled = true},
-			removed: {Item item -> itemRemovedCalled = true },
-			updated: { Item oldItem, Item item -> itemUpdatedCalled = true },
-			allItemsChanged: {}
-		] as ItemRegistryChangeListener)
-		
-		def item = new SwitchItem("switch")
-		itemsChangeListener.added(itemProvider, item)
-		itemsChangeListener.removed(itemProvider, item)
-		itemsChangeListener.added(itemProvider, item)
-		def oldItem = item;
-		def newItem = new SwitchItem("switch")
-		itemsChangeListener.updated(itemProvider, oldItem, newItem)
-		
-		assertThat itemAddedCalled && itemRemovedCalled && itemUpdatedCalled, is(true)
-	}
-    
+        // test removed item is removed as member from group
+        itemsChangeListener.removed(itemProvider, switchItem)
+        assertThat groupItem.members.contains(switchItem), is(false)
+
+        // test added group item with gets all members set when it is added at last
+        switchItem.addGroupName("group2")
+        itemsChangeListener.added(itemProvider, switchItem)
+        itemsChangeListener.added(itemProvider, new GroupItem("group2"))
+
+        def groupItem2 = (itemRegistry.getItem("group2") as GroupItem)
+        assertThat groupItem2.members.contains(switchItem), is(true)
+
+        // test update item
+        itemsChangeListener.added(itemProvider, new GroupItem("group3"))
+        def groupItem3 = (itemRegistry.getItem("group") as GroupItem)
+
+        SwitchItem updatedSwitchItem = new SwitchItem("switch")
+
+        updatedSwitchItem.addGroupName("group")
+        updatedSwitchItem.addGroupName("group3")
+
+        // old item has group: [group, group2], new item has [group, group3]
+        itemsChangeListener.updated(itemProvider, switchItem, updatedSwitchItem)
+
+        assertThat groupItem.members.contains(updatedSwitchItem), is(true)
+        assertThat groupItem2.members.contains(updatedSwitchItem), is(false)
+        assertThat groupItem3.members.contains(updatedSwitchItem), is(true)
+    }
+
+    @Test
+    void 'assert itemRegistryChangeListeners are informed about item changes'() {
+        registerService itemProvider
+
+        def itemAddedCalled = false
+        def itemRemovedCalled = false
+        def itemUpdatedCalled = false
+
+        itemRegistry.addRegistryChangeListener([
+            added: {Item item -> itemAddedCalled = true},
+            removed: {Item item -> itemRemovedCalled = true },
+            updated: { Item oldItem, Item item -> itemUpdatedCalled = true },
+            allItemsChanged: {
+            }
+        ] as ItemRegistryChangeListener)
+
+        def item = new SwitchItem("switch")
+        itemsChangeListener.added(itemProvider, item)
+        itemsChangeListener.removed(itemProvider, item)
+        itemsChangeListener.added(itemProvider, item)
+        def oldItem = item;
+        def newItem = new SwitchItem("switch")
+        itemsChangeListener.updated(itemProvider, oldItem, newItem)
+
+        assertThat itemAddedCalled && itemRemovedCalled && itemUpdatedCalled, is(true)
+    }
+
+    @Test
+    void 'assert itemRegistry keeps same instance on item updates without changes'() {
+        def items = new HashSet<Item>()
+        def itemToKeep = new SwitchItem("Keep");
+        items.add(itemToKeep);
+
+        def itemProvider2 = [
+            getAll: { items },
+            addProviderChangeListener: {def icl -> itemsChangeListener = icl},
+            removeProviderChangeListener: {def icl -> itemsChangeListener = icl },
+            allItemsChanged: {}] as ItemProvider
+
+        registerService itemProvider2
+
+        assertThat itemRegistry.getItem("Keep") == itemToKeep, is(true)
+
+        items.clear()
+        items.add(new SwitchItem("Keep"))
+        itemsChangeListener.allItemsChanged(itemProvider2, null)
+
+        assertThat itemRegistry.getItem("Keep") == itemToKeep, is(true)
+
+        items.clear()
+        def modifiedItem = new StringItem("Keep")
+        items.add(modifiedItem)
+        itemsChangeListener.allItemsChanged(itemProvider2, null)
+
+        assertThat itemRegistry.getItem("Keep") == modifiedItem, is(true)
+    }
+
     @Test
     void 'assert itemRegistry is thread safe'() {
         registerService itemProvider
-        
+
         AtomicInteger numberOfSuccessfulGetItemCalls = new AtomicInteger(0);
-        
+
         10.times({
             Thread.start {
                 10.times({
@@ -246,7 +286,7 @@ class ItemRegistryOSGiTest extends OSGiTest {
                 })
             }
         });
-    
+
         waitFor({numberOfSuccessfulGetItemCalls.get() >= 100})
         assertThat numberOfSuccessfulGetItemCalls.get(), is(100)
     }
