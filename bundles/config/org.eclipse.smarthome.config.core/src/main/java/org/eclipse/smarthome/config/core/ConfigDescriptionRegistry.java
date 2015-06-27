70356cf6582f81ba404b66b7753187e4e5fc0f5d
Chris Jackson <chris@cd-jackson.com>
2015-06-27 20:19:03 +0100

    Allow multiple config description providers This will merge configuration from the same URI from multiple config description providers As discussed on the Eclipse forum https://www.eclipse.org/forums/index.php/m/1699697/#msg_1699697
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ConfigDescriptionRegistry.java
+++ bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ConfigDescriptionRegistry.java
@@ -11,8 +11,10 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -23,6 +25,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  *
  * @author Dennis Nobel - Initial contribution, added locale support
  * @author Michael Grammling - Initial contribution
+ * @author Chris Jackson - Added compatibility with multiple ConfigDescriptionProviders
  */
 public class ConfigDescriptionRegistry {
 
@@ -44,6 +47,13 @@ public class ConfigDescriptionRegistry {
 
     /**
      * Returns all config descriptions.
+     * <p>
+     * If more than one {@link ConfigDescriptionProvider} is registered for a specific URI, then the returned
+     * {@link ConfigDescription} collection will contain the data from all providers.
+     * <p>
+     * No checking is performed to ensure that multiple providers don't provide the same configuration data. It is up to
+     * the binding to ensure that multiple sources (eg static XML and dynamic binding data) do not contain overlapping
+     * information.
      *
      * @param locale
      *            locale
@@ -51,10 +61,40 @@ public class ConfigDescriptionRegistry {
      *         description exists
      */
     public Collection<ConfigDescription> getConfigDescriptions(Locale locale) {
-        Collection<ConfigDescription> configDescriptions = new ArrayList<>(10);
+        Map<URI, ConfigDescription> configMap = new HashMap<URI, ConfigDescription>(); 
+        
+        // Loop over all providers
         for (ConfigDescriptionProvider configDescriptionProvider : this.configDescriptionProviders) {
-            configDescriptions.addAll(configDescriptionProvider.getConfigDescriptions(locale));
+            // And for each provider, loop over all their config descriptions
+            for(ConfigDescription configDescription : configDescriptionProvider.getConfigDescriptions(locale)) {
+                // See if there already exists a configuration for this URI in the map
+                ConfigDescription configFromMap = configMap.get(configDescription.getURI());
+                if(configFromMap != null) {
+                    // Yes - Merge the groups and parameters
+                    List<ConfigDescriptionParameter> parameters = new ArrayList<ConfigDescriptionParameter>();
+                    parameters.addAll(configFromMap.getParameters());
+                    parameters.addAll(configDescription.getParameters());
+
+                    List<ConfigDescriptionParameterGroup> parameterGroups = new ArrayList<ConfigDescriptionParameterGroup>();
+                    parameterGroups.addAll(configFromMap.getParameterGroups());
+                    parameterGroups.addAll(configDescription.getParameterGroups());
+
+                    // And add the combined configuration to the map
+                    configMap.put(configDescription.getURI(), new ConfigDescription(configDescription.getURI(), parameters, parameterGroups));
+                }
+                else {
+                    // No - Just add the new configuration to the map
+                    configMap.put(configDescription.getURI(), configDescription);
+                }
+            }
         }
+
+        // Now convert the map into the collection
+        Collection<ConfigDescription> configDescriptions = new ArrayList<ConfigDescription>(configMap.size());
+        for (ConfigDescription configDescription : configMap.values()) {
+            configDescriptions.add(configDescription);
+        }
+        
         return Collections.unmodifiableCollection(configDescriptions);
     }
 
@@ -70,6 +110,13 @@ public class ConfigDescriptionRegistry {
 
     /**
      * Returns a config description for a given URI.
+     * <p>
+     * If more than one {@link ConfigDescriptionProvider} is registered for the requested URI, then the returned
+     * {@link ConfigDescription} will contain the data from all providers.
+     * <p>
+     * No checking is performed to ensure that multiple providers don't provide the same configuration data. It is up to
+     * the binding to ensure that multiple sources (eg static XML and dynamic binding data) do not contain overlapping
+     * information.
      *
      * @param uri
      *            the URI to which the config description to be returned (must
@@ -80,14 +127,29 @@ public class ConfigDescriptionRegistry {
      *         the given name
      */
     public ConfigDescription getConfigDescription(URI uri, Locale locale) {
-        for (ConfigDescriptionProvider configDescriptionProvider : this.configDescriptionProviders) {
-            ConfigDescription configDescription = configDescriptionProvider.getConfigDescription(uri, locale);
+        List<ConfigDescriptionParameter> parameters = new ArrayList<ConfigDescriptionParameter>();
+        List<ConfigDescriptionParameterGroup> parameterGroups = new ArrayList<ConfigDescriptionParameterGroup>();
 
-            if (configDescription != null) {
-                return configDescription;
+        boolean found = false;
+        for (ConfigDescriptionProvider configDescriptionProvider : this.configDescriptionProviders) {
+            ConfigDescription config = configDescriptionProvider.getConfigDescription(uri, locale);
+
+            if (config != null) {
+                found = true;
+
+                // Simply merge the groups and parameters
+                parameters.addAll(config.getParameters());
+                parameterGroups.addAll(config.getParameterGroups());
             }
         }
-        return null;
+
+        if (found) {
+            // Return the new configuration description
+            return new ConfigDescription(uri, parameters, parameterGroups);
+        } else {
+            // Otherwise null
+            return null;
+        }
     }
 
     /**
