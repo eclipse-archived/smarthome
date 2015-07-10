495e653ad1034d0e02edecdaf46e8bac5101ee2f
Vlad Ivanov <vlad-mbx@ya.ru>
2015-07-10 16:01:43 +0300

    Initial contribution of the Basic UI
    Also-by: Vlad Ivanov <vlad-mbx@ya.ru>
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ bundles/ui/org.eclipse.smarthome.ui.basic/src/main/java/org/eclipse/smarthome/ui/basic/internal/render/ImageRenderer.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.ui.basic.internal.render;
+
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.smarthome.model.sitemap.Image;
+import org.eclipse.smarthome.model.sitemap.Widget;
+import org.eclipse.smarthome.ui.basic.render.RenderException;
+import org.eclipse.smarthome.ui.basic.render.WidgetRenderer;
+
+/**
+ * This is an implementation of the {@link WidgetRenderer} interface, which
+ * can produce HTML code for Image widgets.
+ *
+ * @author Kai Kreuzer - Initial contribution and API
+ * @author Vlad Ivanov - BasicUI changes
+ *
+ */
+public class ImageRenderer extends AbstractWidgetRenderer {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean canRender(Widget w) {
+        return w instanceof Image;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EList<Widget> renderWidget(Widget w, StringBuilder sb) throws RenderException {
+        Image image = (Image) w;
+        String snippet = (image.getChildren().size() > 0) ? getSnippet("image_link") : getSnippet("image");
+
+        if (image.getRefresh() > 0) {
+            snippet = StringUtils.replace(snippet, "%update_interval%", Integer.toString(image.getRefresh()));
+        } else {
+            snippet = StringUtils.replace(snippet, "%update_interval%", "0");
+        }
+
+        String widgetId = itemUIRegistry.getWidgetId(w);
+        snippet = StringUtils.replace(snippet, "%id%", widgetId);
+
+        String sitemap = w.eResource().getURI().path();
+
+        String url = "../proxy?sitemap=" + sitemap + "&amp;widgetId=" + widgetId + "&amp;t=" + (new Date()).getTime();
+        snippet = StringUtils.replace(snippet, "%url%", url);
+
+        sb.append(snippet);
+        return null;
+    }
+}
---
+++ bundles/ui/org.eclipse.smarthome.ui.basic/src/main/java/org/eclipse/smarthome/ui/basic/internal/render/SelectionRenderer.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.ui.basic.internal.render;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.smarthome.model.sitemap.Mapping;
+import org.eclipse.smarthome.model.sitemap.Selection;
+import org.eclipse.smarthome.model.sitemap.Widget;
+import org.eclipse.smarthome.ui.basic.render.RenderException;
+import org.eclipse.smarthome.ui.basic.render.WidgetRenderer;
+
+/**
+ * This is an implementation of the {@link WidgetRenderer} interface, which
+ * can produce HTML code for Selection widgets.
+ *
+ * @author Kai Kreuzer - Initial contribution and API
+ * @author Vlad Ivanov - BasicUI changes
+ *
+ */
+public class SelectionRenderer extends AbstractWidgetRenderer {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean canRender(Widget w) {
+        return w instanceof Selection;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EList<Widget> renderWidget(Widget w, StringBuilder sb) throws RenderException {
+        String snippet = getSnippet("selection");
+
+        snippet = StringUtils.replace(snippet, "%category%", getCategory(w));
+        snippet = StringUtils.replace(snippet, "%icon_type%", config.getIconType());
+        snippet = StringUtils.replace(snippet, "%state%", getState(w));
+        snippet = StringUtils.replace(snippet, "%label_header%", getLabel(w));
+        snippet = StringUtils.replace(snippet, "%value%", getValue(w));
+        snippet = StringUtils.replace(snippet, "%item%", w.getItem() != null ? w.getItem() : "");
+
+        String state = itemUIRegistry.getState(w).toString();
+        Selection selection = (Selection) w;
+
+        StringBuilder rowSB = new StringBuilder();
+        for (Mapping mapping : selection.getMappings()) {
+            String rowSnippet = getSnippet("selection_row");
+            rowSnippet = StringUtils.replace(rowSnippet, "%item%", w.getItem() != null ? w.getItem() : "");
+            rowSnippet = StringUtils.replace(rowSnippet, "%cmd%", mapping.getCmd() != null ? mapping.getCmd() : "");
+            rowSnippet = StringUtils.replace(rowSnippet, "%label%",
+                    mapping.getLabel() != null ? mapping.getLabel() : "");
+            if (state.equals(mapping.getCmd())) {
+                rowSnippet = StringUtils.replace(rowSnippet, "%checked%", "checked=\"true\"");
+            } else {
+                rowSnippet = StringUtils.replace(rowSnippet, "%checked%", "");
+            }
+            rowSB.append(rowSnippet);
+        }
+        snippet = StringUtils.replace(snippet, "%rows%", rowSB.toString());
+
+        // Process the color tags
+        snippet = processColor(w, snippet);
+
+        sb.append(snippet);
+        return null;
+    }
+}
