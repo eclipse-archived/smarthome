72595e764fcfafbf1284e46d06010ddc11cb266b
Vasil Ilchev <v.ilchev@prosyst.bg>
2015-08-04 15:05:10 +0300

    License headers refactor
    Signed-off-by: Vasil Ilchev <v.ilchev@prosyst.bg>

--- bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/RuleError.java
+++ bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/RuleError.java
@@ -1,12 +1,9 @@
-/*
- * Copyright (c) 2015 by ProSyst Software GmbH
- * http://www.prosyst.com
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of ProSyst Software GmbH. You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms
- * of the license agreement you entered into with ProSyst.
+/**
+ * Copyright (c) 1997, 2015 by ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.eclipse.smarthome.automation;
 
--- bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/template/TemplateProvider.java
+++ bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/template/TemplateProvider.java
@@ -1,15 +1,10 @@
-/*******************************************************************************
- * Copyright (c) 1997, 2015 by ProSyst Software GmbH
- * http://www.prosyst.com
+/**
+ * Copyright (c) 1997, 2015 by ProSyst Software GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    ProSyst Software GmbH - initial API and implementation
- *******************************************************************************/
-
+ */
 package org.eclipse.smarthome.automation.template;
 
 import java.util.Collection;
