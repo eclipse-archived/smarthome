e16ab74be78fb9147ba86418d2a1d7c037e9d034
Patrick Koenemann <patrick.koenemann@itemis.de>
2015-08-27 11:24:10 +0200

    Initial version of FS internet radio binding.
    This binding is based on openhab 1.x binding frontiersiliconradio and is
    migrated to the new ESH API, including device discovery.
    
    Bug: https://github.com/eclipse/smarthome/pull/429
    Signed-off-by: Patrick Koenemann <patrick.koenemann@itemis.de>
    Also-by: Rainer Ostendorf <github@linlab.de>

---
+++ extensions/binding/org.eclipse.smarthome.binding.fsinternetradio/src/main/java/org/eclipse/smarthome/binding/fsinternetradio/FSInternetRadioBindingConstants.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.binding.fsinternetradio;
+
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * This {@link FSInternetRadioBindingConstants} interface defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author Patrick Koenemann - Initial contribution
+ */
+public interface FSInternetRadioBindingConstants {
+
+    String BINDING_ID = "fsinternetradio";
+
+    // List of all Thing Type UIDs
+    ThingTypeUID THING_TYPE_RADIO = new ThingTypeUID(BINDING_ID, "radio");
+
+    // List of all Channel ids
+    String CHANNEL_POWER = "power";
+    String CHANNEL_PRESET = "preset";
+    String CHANNEL_VOLUME_PERCENT = "volume-percent";
+    String CHANNEL_VOLUME_ABSOLUTE = "volume-absolute";
+    String CHANNEL_MUTE = "mute";
+    String CHANNEL_PLAY_INFO_NAME = "play-info-name";
+    String CHANNEL_PLAY_INFO_TEXT = "play-info-text";
+    String CHANNEL_MODE = "mode";
+
+    // config properties
+    String CONFIG_PROPERTY_IP = "ip";
+    String CONFIG_PROPERTY_PIN = "pin";
+    String CONFIG_PROPERTY_PORT = "port";
+    String CONFIG_PROPERTY_REFRESH = "refresh";
+
+    // further properties
+    String PROPERTY_MANUFACTURER = "manufacturer";
+    String PROPERTY_MODEL = "model";
+}
