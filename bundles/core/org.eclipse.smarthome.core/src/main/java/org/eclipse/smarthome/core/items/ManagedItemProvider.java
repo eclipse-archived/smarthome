67cf8e830f0af231666a9e75d9924b3e56a6dde3
Dennis Nobel <d.nobel@external.telekom.de>
2015-09-15 20:12:21 +0200

    Changed signature of ItemRegistry recursive removed method to return old item and fixed NPE.
    Bug: 477492
    Signed-off-by: Dennis Nobel <d.nobel@external.telekom.de>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/ManagedItemProvider.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/ManagedItemProvider.java
@@ -32,7 +32,7 @@ import org.slf4j.LoggerFactory;
  * @author Kai Kreuzer - improved return values
  * @author Alex Tugarev - added tags
  */
-public class ManagedItemProvider extends AbstractManagedProvider<Item, String, PersistedItem> implements ItemProvider {
+public class ManagedItemProvider extends AbstractManagedProvider<Item, String, PersistedItem>implements ItemProvider {
 
     public static class PersistedItem {
 
@@ -63,8 +63,10 @@ public class ManagedItemProvider extends AbstractManagedProvider<Item, String, P
      *            item name to remove
      * @param recursive
      *            if set to true all members of the item will be removed, too.
+     * @return
+     *         removed item or null if no item with that name exists
      */
-    public void remove(String itemName, boolean recursive) {
+    public Item remove(String itemName, boolean recursive) {
         Item item = get(itemName);
         if (recursive && item instanceof GroupItem) {
             List<String> members = getMemberNamesRecursively((GroupItem) item, getAll());
@@ -72,7 +74,12 @@ public class ManagedItemProvider extends AbstractManagedProvider<Item, String, P
                 this.remove(member);
             }
         }
-        this.remove(item.getName());
+        if (item != null) {
+            this.remove(item.getName());
+            return item;
+        } else {
+            return null;
+        }
     }
 
     private List<String> getMemberNamesRecursively(GroupItem groupItem, Collection<Item> allItems) {
