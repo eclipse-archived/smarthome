d6833767bd42733cbd00c2d5ac354978cdb87e4f
YordanDZhelev <zhelev.yordan@gmail.com>
2015-09-17 13:50:23 +0300

    Swagger annottaions added to all bundles containing REST resources
    Signed-off-by: YordanDZhelev <zhelev.yordan@gmail.com>

---
+++ docs/src/main/scripts/CollectRestFiles.groovy
@@ -0,0 +1,20 @@
+def baseDir = new File("$project.basedir")
+def restFolder = new File(baseDir, '/documentation/features/rest/')
+restFolder.mkdirs()
+def parentFolder = new File(baseDir.getParent())
+def bundlesFolder = new File(parentFolder, '/bundles/')
+bundlesFolder.eachFileRecurse(){
+	def swaggerFolderName = 'target' + File.separator + 'generated' + File.separator + 'swagger'
+	if(it.isFile() && it.getParent().endsWith(swaggerFolderName) && it.getName().endsWith('.md')){
+		def copy = new File(restFolder.getPath() + '/' +  it.getName())
+		def input = it.newInputStream()
+		def output = copy.newOutputStream()
+
+		output << input
+
+		input.close()
+		output.close()
+	}
+}
+
+
