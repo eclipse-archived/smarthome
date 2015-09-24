e9a121e4826613e3bea36eb02f3f41ed23b757de
Marcel Verpaalen <marcel@verpaalen.com>
2015-09-24 23:53:51 +0200

    fix wrong status update for bridge child things Addressing issue https://bugs.eclipse.org/bugs/show_bug.cgi?id=465480
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/ThingManager.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/ThingManager.java
@@ -159,17 +159,24 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
             setThingStatus(thing, thingStatus);
 
             if (thing instanceof Bridge) {
-                Bridge bridge = (Bridge) thing;
-                for (Thing bridgeThing : bridge.getThings()) {
-                    if (thingStatus.getStatus() == ThingStatus.ONLINE) {
-                        ThingStatusInfo statusInfo = ThingStatusInfoBuilder.create(ThingStatus.ONLINE).build();
-                        setThingStatus(bridgeThing, statusInfo);
-                    } else if (thingStatus.getStatus() == ThingStatus.OFFLINE) {
-                        ThingStatusInfo statusInfo = ThingStatusInfoBuilder
-                                .create(ThingStatus.OFFLINE, ThingStatusDetail.BRIDGE_OFFLINE).build();
-                        setThingStatus(bridgeThing, statusInfo);
-                    }
-                }
+            	Bridge bridge = (Bridge) thing;
+            	for (Thing bridgeChildThing : bridge.getThings()) {
+            		ThingStatusInfo bridgeChildThingStatus = bridgeChildThing.getStatusInfo();
+            		if (bridgeChildThingStatus.getStatus() == ThingStatus.ONLINE
+            				|| bridgeChildThingStatus.getStatus() == ThingStatus.OFFLINE) {
+
+            			if (thingStatus.getStatus() == ThingStatus.ONLINE
+            					&& bridgeChildThingStatus.getStatusDetail() == ThingStatusDetail.BRIDGE_OFFLINE) {
+            				ThingStatusInfo statusInfo = ThingStatusInfoBuilder.create(ThingStatus.OFFLINE,
+            						ThingStatusDetail.NONE).build();
+            				setThingStatus(bridgeChildThing, statusInfo);
+            			} else if (thingStatus.getStatus() == ThingStatus.OFFLINE) {
+            				ThingStatusInfo statusInfo = ThingStatusInfoBuilder.create(ThingStatus.OFFLINE,
+            						ThingStatusDetail.BRIDGE_OFFLINE).build();
+            				setThingStatus(bridgeChildThing, statusInfo);
+            			}
+            		}
+            	}
             }
 
             if (ThingStatus.REMOVING.equals(thing.getStatus())) {
