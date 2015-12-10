8ca62f8dab486ce0342bd3c5f3661e202ea6d3a3
Hans-Jörg Merk <hans-joerg.merk@t-online.de>
2015-12-11 00:14:46 +0100

    Wemo binding : Added new devices - Wemo Bridge, Wemo LED bulb and Wemo Maker Change autodiscovery to UPnP Introduce GENA event usage See discussion at #662 and #461 This change also needs jupnp PR41 to be merged.
    Signed-off-by: Hans-Jörg Merk <hans-joerg.merk@t-online.de>

--- extensions/binding/org.eclipse.smarthome.binding.wemo/src/main/java/org/eclipse/smarthome/binding/wemo/WemoBindingConstants.java
+++ extensions/binding/org.eclipse.smarthome.binding.wemo/src/main/java/org/eclipse/smarthome/binding/wemo/WemoBindingConstants.java
@@ -7,7 +7,11 @@
  */
 package org.eclipse.smarthome.binding.wemo;
 
+import java.util.Set;
+
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+import com.google.common.collect.ImmutableSet;
 
 /**
  * The {@link WemoBinding} class defines common constants, which are
@@ -20,21 +24,43 @@ public class WemoBindingConstants {
     public static final String BINDING_ID = "wemo";
 
     // List of all Thing Type UIDs
-    public final static ThingTypeUID WEMO_SOCKET_TYPE_UID = new ThingTypeUID(BINDING_ID, "socket");
-    public final static ThingTypeUID WEMO_INSIGHT_TYPE_UID = new ThingTypeUID(BINDING_ID, "insight");
-    public final static ThingTypeUID WEMO_LIGHTSWITCH_TYPE_UID = new ThingTypeUID(BINDING_ID, "lightswitch");
-    public final static ThingTypeUID WEMO_MOTION_TYPE_UID = new ThingTypeUID(BINDING_ID, "motion");
-    public final static ThingTypeUID WEMO_SENSOR_TYPE_UID = new ThingTypeUID(BINDING_ID, "sensor");
+    public final static ThingTypeUID THING_TYPE_SOCKET = new ThingTypeUID(BINDING_ID, "socket");
+    public final static ThingTypeUID THING_TYPE_INSIGHT = new ThingTypeUID(BINDING_ID, "insight");
+    public final static ThingTypeUID THING_TYPE_LIGHTSWITCH = new ThingTypeUID(BINDING_ID, "lightswitch");
+    public final static ThingTypeUID THING_TYPE_MOTION = new ThingTypeUID(BINDING_ID, "motion");
+    public final static ThingTypeUID THING_TYPE_BRIDGE = new ThingTypeUID(BINDING_ID, "bridge");
+    public final static ThingTypeUID THING_TYPE_MZ100 = new ThingTypeUID(BINDING_ID, "MZ100");
+    public final static ThingTypeUID THING_TYPE_MAKER = new ThingTypeUID(BINDING_ID, "Maker");
 
     // List of all Channel ids
     public final static String CHANNEL_STATE = "state";
-
-    // List of thing configuration properties
-    public static final String UDN = "udn";
-    public static final String LOCATION = "location";
-    public final static String CHANNEL_CURRENTPOWER = "currentPower";
+    public final static String CHANNEL_MOTIONDETECTION = "motionDetection";
+    public final static String CHANNEL_LASTCHANGEDAT = "lastChangedAt";
     public final static String CHANNEL_LASTONFOR = "lastOnFor";
     public final static String CHANNEL_ONTODAY = "onToday";
     public final static String CHANNEL_ONTOTAL = "onTotal";
+    public final static String CHANNEL_TIMESPAN = "timespan";
+    public final static String CHANNEL_AVERAGEPOWER = "averagePower";
+    public final static String CHANNEL_CURRENTPOWER = "currentPower";
+    public final static String CHANNEL_ENERGYTODAY = "energyToday";
+    public final static String CHANNEL_ENERGYTOTAL = "energyTotal";
+    public final static String CHANNEL_STANDBYLIMIT = "standByLimit";
+    public static final String CHANNEL_BRIGHTNESS = "brightness";
+    public final static String CHANNEL_RELAY = "relay";
+    public final static String CHANNEL_SENSOR = "sensor";
+
+    // List of thing configuration properties
+    public static final String UDN = "udn";
+    public static final String DEVICE_ID = "deviceID";
+
+    public final static Set<ThingTypeUID> SUPPORTED_BRIDGE_THING_TYPES = ImmutableSet.of(THING_TYPE_BRIDGE);
+
+    public final static Set<ThingTypeUID> SUPPORTED_LIGHT_THING_TYPES = ImmutableSet.of(THING_TYPE_MZ100);
+
+    public final static Set<ThingTypeUID> SUPPORTED_DEVICE_THING_TYPES = ImmutableSet.of(THING_TYPE_SOCKET,
+            THING_TYPE_INSIGHT, THING_TYPE_LIGHTSWITCH, THING_TYPE_MOTION);
+
+    public final static Set<ThingTypeUID> SUPPORTED_THING_TYPES = ImmutableSet.of(THING_TYPE_SOCKET, THING_TYPE_INSIGHT,
+            THING_TYPE_LIGHTSWITCH, THING_TYPE_MOTION, THING_TYPE_BRIDGE, THING_TYPE_MZ100, THING_TYPE_MAKER);
 
 }
