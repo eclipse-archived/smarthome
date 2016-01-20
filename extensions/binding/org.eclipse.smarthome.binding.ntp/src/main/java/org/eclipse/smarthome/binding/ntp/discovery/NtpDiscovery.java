4506b4e688f790ba51d2ecbd02bc0f35ec7864c6
Kai Kreuzer <kai@openhab.org>
2016-01-20 13:09:09 +0100

    applied code formatter (no code changes in this commit)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- extensions/binding/org.eclipse.smarthome.binding.ntp/src/main/java/org/eclipse/smarthome/binding/ntp/discovery/NtpDiscovery.java
+++ extensions/binding/org.eclipse.smarthome.binding.ntp/src/main/java/org/eclipse/smarthome/binding/ntp/discovery/NtpDiscovery.java
@@ -7,6 +7,8 @@
  */
 package org.eclipse.smarthome.binding.ntp.discovery;
 
+import static org.eclipse.smarthome.binding.ntp.NtpBindingConstants.*;
+
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Locale;
@@ -19,56 +21,55 @@ import org.eclipse.smarthome.config.discovery.DiscoveryResult;
 import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
 import org.eclipse.smarthome.core.thing.ThingUID;
 
-import static org.eclipse.smarthome.binding.ntp.NtpBindingConstants.*;
-
 /**
- * 
+ *
  * The {@link NtpDiscovery} is used to add a ntp Thing for the local time in the discovery inbox
- *  * 
+ * *
+ * 
  * @author Marcel Verpaalen - Initial contribution
  */
 public class NtpDiscovery extends AbstractDiscoveryService {
 
-	private final static String DEFAULT_NTP_SERVER = "0.pool.ntp.org";
-	private final static BigDecimal DEFAULT_REFRESH_INTERVAL = new BigDecimal(60);
-	private final static BigDecimal DEFAULT_NTP_INTERVAL = new BigDecimal(30);
+    private final static String DEFAULT_NTP_SERVER = "0.pool.ntp.org";
+    private final static BigDecimal DEFAULT_REFRESH_INTERVAL = new BigDecimal(60);
+    private final static BigDecimal DEFAULT_NTP_INTERVAL = new BigDecimal(30);
 
-	public NtpDiscovery() throws IllegalArgumentException {
-		super(SUPPORTED_THING_TYPES_UIDS, 10);
-	}
+    public NtpDiscovery() throws IllegalArgumentException {
+        super(SUPPORTED_THING_TYPES_UIDS, 10);
+    }
 
-	@Override
-	protected void startBackgroundDiscovery() {
-		scheduler.schedule(new Runnable() {
-			@Override
-			public void run() {
-				discoverNtp();
-			}
-		}, 1, TimeUnit.SECONDS);
-	}
-	
-	@Override
-	protected void startScan() {
-		discoverNtp();
-	}
+    @Override
+    protected void startBackgroundDiscovery() {
+        scheduler.schedule(new Runnable() {
+            @Override
+            public void run() {
+                discoverNtp();
+            }
+        }, 1, TimeUnit.SECONDS);
+    }
 
-	/**
-	 *  Add a ntp Thing for the local time in the discovery inbox
-	 */
-	private void discoverNtp() {
-		Map<String, Object> properties = new HashMap<>(4);
-		properties.put(PROPERTY_NTP_SERVER, DEFAULT_NTP_SERVER);
-		properties.put(PROPERTY_REFRESH_INTERVAL, DEFAULT_REFRESH_INTERVAL);
-		properties.put(PROPERTY_REFRESH_NTP, DEFAULT_NTP_INTERVAL);
-		properties.put(PROPERTY_TIMEZONE, TimeZone.getDefault().getID());
-		properties.put(PROPERTY_LOCALE, Locale.getDefault());
-		ThingUID uid = new ThingUID(THING_TYPE_NTP, "local");
-		if (uid != null) {
-			DiscoveryResult result = DiscoveryResultBuilder.create(uid).withProperties(properties)
-					.withLabel("Local Time").build();
-			thingDiscovered(result);
-		}
+    @Override
+    protected void startScan() {
+        discoverNtp();
+    }
 
-	}
+    /**
+     * Add a ntp Thing for the local time in the discovery inbox
+     */
+    private void discoverNtp() {
+        Map<String, Object> properties = new HashMap<>(4);
+        properties.put(PROPERTY_NTP_SERVER, DEFAULT_NTP_SERVER);
+        properties.put(PROPERTY_REFRESH_INTERVAL, DEFAULT_REFRESH_INTERVAL);
+        properties.put(PROPERTY_REFRESH_NTP, DEFAULT_NTP_INTERVAL);
+        properties.put(PROPERTY_TIMEZONE, TimeZone.getDefault().getID());
+        properties.put(PROPERTY_LOCALE, Locale.getDefault());
+        ThingUID uid = new ThingUID(THING_TYPE_NTP, "local");
+        if (uid != null) {
+            DiscoveryResult result = DiscoveryResultBuilder.create(uid).withProperties(properties)
+                    .withLabel("Local Time").build();
+            thingDiscovered(result);
+        }
+
+    }
 
 }
