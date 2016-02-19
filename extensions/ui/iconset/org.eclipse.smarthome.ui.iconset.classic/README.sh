9e834532f54749226a8f02bba95e0c56466a6531
Markus Reiter <me@reitermark.us>
2016-02-19 17:23:57 +0100

    Import iconset from OpenHAB.
    Import iconset from OpenHAB, fix office icon and add script to generate iconset ReadMe.
    
    Signed-off-by: Markus Reiter <me@reitermark.us>

---
+++ extensions/ui/iconset/org.eclipse.smarthome.ui.iconset.classic/README.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+
+README="$(cd "$(dirname "$0")"; pwd)/README.md"
+
+
+cat <<EOF > "${README}"
+---
+layout: documentation
+---
+
+{% include base.html %}
+
+# Classic Icon Set
+
+This is a modernized version of the original icon set of openHAB 1.<br/><br/>
+
+EOF
+
+
+for icon in icons/*.png; do
+
+  name="$(basename "${icon}" | cut -d '.' -f1)"
+
+  if [ "${name}" != 'none' ]; then
+    echo "![${name}](${icon})" >> "${README}"
+  fi
+
+done
