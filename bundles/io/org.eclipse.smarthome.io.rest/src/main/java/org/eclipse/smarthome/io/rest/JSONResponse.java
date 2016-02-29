ab088ab5b3aab76123f7e4418ba4ddbc5abf84d2
Bas Huisman <bhuism@odee.net>
2016-02-29 21:01:15 +0100

    Added exeption logging to REST Exception handler
    Signed-off-by: Bas Huisman <bhuism@odee.net>

--- bundles/io/org.eclipse.smarthome.io.rest/src/main/java/org/eclipse/smarthome/io/rest/JSONResponse.java
+++ bundles/io/org.eclipse.smarthome.io.rest/src/main/java/org/eclipse/smarthome/io/rest/JSONResponse.java
@@ -13,6 +13,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -142,11 +145,18 @@ public class JSONResponse {
      */
     @Provider
     public static class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception> {
+
+        private final static Logger logger = LoggerFactory.getLogger(ExceptionMapper.class);
+
         /**
          * create JSON Response
          */
+
         @Override
         public Response toResponse(Exception e) {
+
+            logger.debug("exception during REST Handling", e);
+
             Response.Status status = Response.Status.INTERNAL_SERVER_ERROR;
 
             // in case the Exception is a WebApplicationException, it already carries a Status
