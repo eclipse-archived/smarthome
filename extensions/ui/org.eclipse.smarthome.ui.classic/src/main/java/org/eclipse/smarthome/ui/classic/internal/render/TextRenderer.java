5fc9213a346ee6cdda7ce2a01de1b8779f07447c
Gaël L hopital <gael@lhopital.org>
2016-03-01 16:00:53 +0100

    Correction of Issue #1034
    Signed-off-by: Gaël L'hopital <glhopital@gmail.com>

--- extensions/ui/org.eclipse.smarthome.ui.classic/src/main/java/org/eclipse/smarthome/ui/classic/internal/render/TextRenderer.java
+++ extensions/ui/org.eclipse.smarthome.ui.classic/src/main/java/org/eclipse/smarthome/ui/classic/internal/render/TextRenderer.java
@@ -9,6 +9,7 @@ package org.eclipse.smarthome.ui.classic.internal.render;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.smarthome.core.types.State;
 import org.eclipse.smarthome.model.sitemap.Text;
 import org.eclipse.smarthome.model.sitemap.Widget;
 import org.eclipse.smarthome.ui.classic.render.RenderException;
@@ -37,12 +38,15 @@ public class TextRenderer extends AbstractWidgetRenderer {
     @Override
     public EList<Widget> renderWidget(Widget w, StringBuilder sb) throws RenderException {
         Text text = (Text) w;
+        State state = itemUIRegistry.getState(w);
+
         String snippet = (text.getChildren().size() > 0) ? getSnippet("text_link") : getSnippet("text");
 
         snippet = StringUtils.replace(snippet, "%id%", itemUIRegistry.getWidgetId(w));
         snippet = StringUtils.replace(snippet, "%category%", escapeURLPath(itemUIRegistry.getCategory(w)));
         snippet = StringUtils.replace(snippet, "%label%", getLabel(w));
         snippet = StringUtils.replace(snippet, "%format%", getFormat());
+        snippet = StringUtils.replace(snippet, "%state%", state.toString());
 
         // Process the color tags
         snippet = processColor(w, snippet);
