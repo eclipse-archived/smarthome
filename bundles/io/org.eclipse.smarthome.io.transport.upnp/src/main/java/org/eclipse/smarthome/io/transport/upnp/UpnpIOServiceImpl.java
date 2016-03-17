49b68a4041149acf52f572d5ff04ac13a4447e35
IVAN GEORGIEV ILIEV <ivan.iliev@musala.com>
2016-03-17 12:19:26 +0100

    Made sure resubscribe is only done when subscription is ended with CancelReasons EXPIRED or RENEW_FAILED as it does not make sense to SUBSCRIBE in the other cases.
    Signed-off-by: IVAN GEORGIEV ILIEV <ivan.iliev@musala.com>

--- bundles/io/org.eclipse.smarthome.io.transport.upnp/src/main/java/org/eclipse/smarthome/io/transport/upnp/UpnpIOServiceImpl.java
+++ bundles/io/org.eclipse.smarthome.io.transport.upnp/src/main/java/org/eclipse/smarthome/io/transport/upnp/UpnpIOServiceImpl.java
@@ -46,6 +46,8 @@ import org.slf4j.LoggerFactory;
  * @author Kai Kreuzer - added descriptor url retrieval
  * @author Markus Rathgeb - added NP checks in subscription ended callback
  * @author Andre Fuechsel - added methods to remove subscriptions
+ * @author Ivan Iliev - made sure resubscribe is only done when subscription ended CancelReason was EXPIRED or
+ *         RENEW_FAILED
  */
 @SuppressWarnings("rawtypes")
 public class UpnpIOServiceImpl implements UpnpIOService {
@@ -91,7 +93,7 @@ public class UpnpIOServiceImpl implements UpnpIOService {
                     }
                 }
 
-                if (upnpService != null) {
+                if ((CancelReason.EXPIRED.equals(reason) || CancelReason.RENEWAL_FAILED.equals(reason)) && upnpService != null) {
                     final ControlPoint cp = upnpService.getControlPoint();
                     if (cp != null) {
                         final UpnpSubscriptionCallback callback = new UpnpSubscriptionCallback(service,
