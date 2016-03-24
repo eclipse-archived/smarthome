689f8f8078158292003fa5029d001125ae42f22c
Aoun Bukhari <bukhari@itemis.de>
2016-03-24 11:45:52 +0100

    Support html tags in item description
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/gulpfile.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/gulpfile.js
@@ -39,7 +39,8 @@ var paths = {
             './node_modules/angular-animate/angular-animate.min.js',
             './node_modules/angular-aria/angular-aria.min.js',
             './node_modules/angular-material/angular-material.min.js',
-            './node_modules/angular-messages/angular-messages.min.js'
+            './node_modules/angular-messages/angular-messages.min.js',
+            './node_modules/angular-sanitize/angular-sanitize.min.js'
         ],
         'name': 'angular-bundle.js'
     }],
