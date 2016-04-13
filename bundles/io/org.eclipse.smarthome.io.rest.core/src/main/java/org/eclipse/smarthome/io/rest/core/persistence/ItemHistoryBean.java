898f2e9172127d2a94adaa21fd493b1a906a9fae
Simon Kaufmann <simon.kfm@gmail.com>
2016-04-13 14:50:17 +0200

    moved persistence REST resource into io.rest.core bundle (#1365)
    PR: https://github.com/eclipse/smarthome/pull/1365
    
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/persistence/ItemHistoryBean.java
+++ bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/persistence/ItemHistoryBean.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.core.persistence.internal.rest;
+package org.eclipse.smarthome.io.rest.core.persistence;
 
 import java.util.ArrayList;
 import java.util.List;
