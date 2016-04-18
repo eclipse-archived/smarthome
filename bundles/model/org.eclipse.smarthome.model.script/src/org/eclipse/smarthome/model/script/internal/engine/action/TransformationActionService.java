21cfa01d87dad2f7dc01756c9305a8b7b6d1ef05
Markus Rathgeb <maggu2810@gmail.com>
2016-04-18 09:15:56 +0200

    Fix CNFE by removal of optional resolution (#1395)
    Move the service that needs packages from core.transform and
    model.script bundle to model.script. The core.transform is a core bundle
    and also part of the base target platform, so it should be available.
    The model script bundle is optional.
    
    PR: https://github.com/eclipse/smarthome/pull/1395
    Bug: https://github.com/eclipse/smarthome/issues/1394
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/model/org.eclipse.smarthome.model.script/src/org/eclipse/smarthome/model/script/internal/engine/action/TransformationActionService.java
+++ bundles/model/org.eclipse.smarthome.model.script/src/org/eclipse/smarthome/model/script/internal/engine/action/TransformationActionService.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.core.transform.internal;
+package org.eclipse.smarthome.model.script.internal.engine.action;
 
 import org.eclipse.smarthome.core.transform.actions.Transformation;
 import org.eclipse.smarthome.model.script.engine.action.ActionService;
