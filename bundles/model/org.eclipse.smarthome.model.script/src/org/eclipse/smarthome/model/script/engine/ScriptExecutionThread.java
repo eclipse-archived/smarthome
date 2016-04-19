4acfbbadf93e78d82f9e88194b3109ef785cc9e4
Markus Rathgeb <maggu2810@gmail.com>
2016-04-19 18:26:09 +0200

    show message of exception (cause could be null)
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/model/org.eclipse.smarthome.model.script/src/org/eclipse/smarthome/model/script/engine/ScriptExecutionThread.java
+++ bundles/model/org.eclipse.smarthome.model.script/src/org/eclipse/smarthome/model/script/engine/ScriptExecutionThread.java
@@ -40,14 +40,19 @@ public class ScriptExecutionThread extends Thread {
         super.run();
         try {
             result = script.execute(context);
-        } catch (final ScriptExecutionException ex) {
-            logger.error("Error during the execution of rule '{}'.", getName(), ex);
+        } catch (ScriptExecutionException e) {
+            String msg = e.getMessage();
+            if (msg == null) {
+                logger.error("Error during the execution of rule '{}'", getName(), e.getCause());
+            } else {
+                logger.error("Error during the execution of rule '{}': {}", new Object[] { getName(), msg });
+            }
         }
     }
 
     /**
      * Returns the script evaluation result (or null, if thread is still active)
-     *
+     * 
      * @return the script evaluation result
      */
     public Object getResult() {
