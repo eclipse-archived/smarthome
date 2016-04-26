744f2bd607a98b934e18217b782877ff8ae2973d
Karel Goderis <karel.goderis@me.com>
2016-04-26 08:47:37 +0200

    Initial contribution of the  bespoke scheduler infrastructure for ESH (#1239)
    Signed-off-by: Karel Goderis <karel.goderis@me.com>

---
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/common/DateExpressionTest.java
@@ -0,0 +1,63 @@
+package org.eclipse.smarthome.core.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.junit.Test;
+
+public class DateExpressionTest {
+
+    @Test(expected = ParseException.class)
+    public void garbageString() throws ParseException {
+        new DateExpression("blahblahblah");
+    }
+
+    @Test
+    public void getTimeAfterCheck() throws ParseException {
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, 0, 1, 0, 0, 0); // set to Jan 1st 2016, 00:00
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+00"));
+        Date startDate = cal.getTime();
+
+        DateExpression expr = new DateExpression("2016-01-31T00:00:00+00:00");
+        expr.setStartDate(startDate);
+        expr.setTimeZone(TimeZone.getTimeZone("GMT+00"));
+
+        Date nextDate = expr.getTimeAfter(startDate);
+
+        cal.set(2016, 0, 31, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+00"));
+        Date checkDate = cal.getTime();
+
+        assertEquals(checkDate, nextDate);
+    }
+
+    @Test
+    public void getFinalTimeCheck() throws ParseException {
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, 0, 1, 0, 0, 0); // set to Jan 1st 2016, 00:00
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+00"));
+        Date startDate = cal.getTime();
+
+        DateExpression expr = new DateExpression("2016-01-31T00:00:00+00:00");
+        expr.setStartDate(startDate);
+        expr.setTimeZone(TimeZone.getTimeZone("GMT+00"));
+
+        Date nextDate = expr.getFinalFireTime();
+
+        cal.set(2016, 0, 31, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+00"));
+        Date checkDate = cal.getTime();
+
+        assertEquals(checkDate, nextDate);
+    }
+}
--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/internal/CoreActivator.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/internal/CoreActivator.java
@@ -42,11 +42,10 @@ public class CoreActivator implements BundleActivator {
 
     /**
      * Returns the bundle context of this bundle
-     * 
+     *
      * @return the bundle context
      */
     public static BundleContext getContext() {
         return context;
     }
-
 }
