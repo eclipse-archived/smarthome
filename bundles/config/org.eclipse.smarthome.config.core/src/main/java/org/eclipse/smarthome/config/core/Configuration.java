30c5dd2839a06a121960197b1a109b2d5ba0f638
Simon Kaufmann <simon.kfm@googlemail.com>
2016-04-28 14:02:21 +0200

    aligned key types in Configuration
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/Configuration.java
+++ bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/Configuration.java
@@ -110,7 +110,7 @@ public class Configuration {
         return fields;
     }
 
-    public Object get(Object key) {
+    public Object get(String key) {
         synchronized (this) {
             return properties.get(key);
         }
@@ -122,7 +122,7 @@ public class Configuration {
         }
     }
 
-    public Object remove(Object key) {
+    public Object remove(String key) {
         synchronized (this) {
             return properties.remove(key);
         }
