3c02299e64aad54d296754b51bcd0bea4b5ed0a2
Roland Edelhoff <roland.menznerowski@itemis.de>
2016-05-05 01:23:52 +0200

    update sonos binding (#1196)
    Signed-off-by: Roland Menznerowski <roland.menznerowski@itemis.de>

--- extensions/binding/org.eclipse.smarthome.binding.sonos/src/main/java/org/eclipse/smarthome/binding/sonos/config/ZonePlayerConfiguration.java
+++ extensions/binding/org.eclipse.smarthome.binding.sonos/src/main/java/org/eclipse/smarthome/binding/sonos/config/ZonePlayerConfiguration.java
@@ -10,17 +10,16 @@ package org.eclipse.smarthome.binding.sonos.config;
 
 public class ZonePlayerConfiguration {
 
-	public static final String UDN = "udn";
-	public static final String FRIENDLY_NAME = "friendlyName";
-	public static final String IP_ADDRESS = "ipAddress";
-	public static final String MODEL = "model";
-	public static final String DEVICE = "device";
+    public static final String UDN = "udn";
+    public static final String FRIENDLY_NAME = "friendlyName";
+    public static final String IDENTIFICATION = "identification";
+    public static final String MODEL = "model";
+    public static final String DEVICE = "device";
 
-	
-	public String udn;
-	public String friendlyName;
-	public String ipAddress;
-	public String model;
-	public String device;
-	
+    public String udn;
+    public String friendlyName;
+    public String ipAddress;
+    public String model;
+    public String device;
+
 }
