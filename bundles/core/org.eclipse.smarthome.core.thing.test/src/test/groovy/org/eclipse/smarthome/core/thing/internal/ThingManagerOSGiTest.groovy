eclipse-archived/smarthome

commit 862e906f37acd80bfc94b591e48afc091d96bbd7

Deferring ThingHandler.initialize() until all XMLs have been parsed (#1419)
Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

bundles/config/org.eclipse.smarthome.config.xml/src/main/java/org/eclipse/smarthome/config/xml/osgi/AbstractAsyncBundleProcessor.java
@@ -8,6 +8,8 @@
 package org.eclipse.smarthome.config.xml.osgi;
 
 import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
@@ -19,7 +21,9 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+import org.eclipse.smarthome.core.thing.BundleProcessor;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleReference;
 import org.osgi.util.tracker.BundleTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,7 +49,7 @@ import org.slf4j.LoggerFactory;
  * 
  */
 
-public abstract class AbstractAsyncBundleProcessor {
+public abstract class AbstractAsyncBundleProcessor implements BundleProcessor {
 
     private final Logger logger = LoggerFactory.getLogger(AbstractAsyncBundleProcessor.class);
 
@@ -54,6 +58,8 @@ public abstract class AbstractAsyncBundleProcessor {
     private final Queue<Bundle> queue = new ConcurrentLinkedQueue<>();
 
     private static final Set<AbstractAsyncBundleProcessor> ALL_PROCESSORS = new CopyOnWriteArraySet<>();
+
+    private Set<BundleProcessorListener> listeners = new CopyOnWriteArraySet<>();
 
     /**
      * This method creates a list where all resources are contained
@@ -163,8 +169,32 @@ public abstract class AbstractAsyncBundleProcessor {
         }
     }
 
-    private boolean isFinishedLoading(Bundle bundle) {
-        return !queue.contains(bundle);
+    @Override
+    public Bundle isFinishedLoading(Object object) {
+        Bundle bundle = getBundle(object.getClass());
+        if (queue.contains(bundle)) {
+            logger.debug("##### Bundle {} is still loading", bundle.getSymbolicName());
+            return bundle;
+        } else {
+            logger.debug("##### Bundle {} is not currently loading", bundle.getSymbolicName());
+            return null;
+        }
+    }
+
+    private Bundle getBundle(final Class<?> classFromBundle) {
+        ClassLoader classLoader = AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
+            @Override
+            public ClassLoader run() {
+                return classFromBundle.getClassLoader();
+            }
+        });
+
+        if (classLoader instanceof BundleReference) {
+            Bundle bundle = ((BundleReference) classLoader).getBundle();
+            logger.debug("##### Bundle of {} is {}", classFromBundle, bundle.getSymbolicName());
+            return bundle;
+        }
+        return null;
     }
 
     /**
@@ -179,7 +209,7 @@ public abstract class AbstractAsyncBundleProcessor {
      */
     public static boolean isBundleFinishedLoading(Bundle bundle) {
         for (AbstractAsyncBundleProcessor processor : ALL_PROCESSORS) {
-            if (!processor.isFinishedLoading(bundle)) {
+            if (processor.queue.contains(bundle)) {
                 return false;
             }
         }
@@ -223,11 +253,29 @@ public abstract class AbstractAsyncBundleProcessor {
                 // remove bundle from queue
                 if (bundle != null) {
                     queue.remove(bundle);
+                    informListeners(bundle);
                 }
             }
             AbstractAsyncBundleProcessor.this.logger.trace("Terminating gracefully");
             ALL_PROCESSORS.remove(this);
         }
+
+    };
+
+    private void informListeners(Bundle bundle) {
+        for (BundleProcessorListener listener : listeners) {
+            listener.bundleFinished(this, bundle);
+        }
+    }
+
+    @Override
+    public void registerListener(BundleProcessorListener listener) {
+        listeners.add(listener);
+    };
+
+    @Override
+    public void unregisterListener(BundleProcessorListener listener) {
+        listeners.remove(listener);
     };
 
 }

bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/ThingManager.java
@@ -37,6 +37,8 @@ import org.eclipse.smarthome.core.items.events.ItemCommandEvent;
 import org.eclipse.smarthome.core.items.events.ItemEventFactory;
 import org.eclipse.smarthome.core.items.events.ItemStateEvent;
 import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.BundleProcessor;
+import org.eclipse.smarthome.core.thing.BundleProcessor.BundleProcessorListener;
 import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.ManagedThingProvider;
@@ -58,6 +60,7 @@ import org.eclipse.smarthome.core.thing.type.ThingType;
 import org.eclipse.smarthome.core.thing.type.ThingTypeRegistry;
 import org.eclipse.smarthome.core.types.Command;
 import org.eclipse.smarthome.core.types.State;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentContext;
@@ -65,6 +68,10 @@ import org.osgi.service.event.EventHandler;
 import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 
 /**
  * {@link ThingManager} tracks all things in the {@link ThingRegistry} and
@@ -81,11 +88,16 @@ import org.slf4j.LoggerFactory;
  *         refactorings thing life cycle
  * @author Simon Kaufmann - Added remove handling
  */
-public class ThingManager extends AbstractItemEventSubscriber implements ThingTracker {
+public class ThingManager extends AbstractItemEventSubscriber implements ThingTracker, BundleProcessorListener {
 
     private static final String FORCEREMOVE_THREADPOOL_NAME = "forceRemove";
 
     private static final String THING_MANAGER_THREADPOOL_NAME = "thingManager";
+
+    private final Multimap<Bundle, Object> initializerVetoes = Multimaps
+            .synchronizedListMultimap(LinkedListMultimap.<Bundle, Object> create());
+    private final Multimap<String, ThingHandler> initializerQueue = Multimaps
+            .synchronizedListMultimap(LinkedListMultimap.<String, ThingHandler> create());
 
     private final class ThingHandlerTracker extends ServiceTracker<ThingHandler, ThingHandler> {
 
@@ -305,6 +317,8 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
     private Set<ThingUID> registerHandlerLock = new HashSet<>();
 
     private Set<ThingUID> thingUpdatedLock = new HashSet<>();
+
+    private Set<BundleProcessor> bundleProcessors = new HashSet<>();
 
     /**
      * Method is called when a {@link ThingHandler} is added.
@@ -609,7 +623,7 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
         if (isInitializable(thing, thingType)) {
             ThingStatusInfo statusInfo = buildStatusInfo(ThingStatus.INITIALIZING, ThingStatusDetail.NONE);
             setThingStatus(thing, statusInfo);
-            initializeHandler(thing, thing.getHandler());
+            initializeHandler(thing.getHandler());
         } else {
             logger.debug("Thing '{}' not initializable, check required configuration parameters.", thing.getUID());
             ThingStatusInfo statusInfo = buildStatusInfo(ThingStatus.UNINITIALIZED,
@@ -653,11 +667,31 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
         return requiredParameters;
     }
 
-    private void initializeHandler(final Thing thing, final ThingHandler thingHandler) {
+    private void initializeHandler(final ThingHandler thingHandler) {
+        Bundle bundle = null;
+        for (BundleProcessor proc : bundleProcessors) {
+            bundle = proc.isFinishedLoading(thingHandler);
+            if (bundle != null) {
+                logger.debug("##### Marking '{}' vetoed by '{}'", bundle.getSymbolicName(), proc);
+                initializerVetoes.put(bundle, proc);
+                if (!initializerQueue.containsEntry(bundle, thingHandler)) {
+                    logger.debug("##### Queueing '{}' in bundle '{}'", thingHandler, bundle.getSymbolicName());
+                    initializerQueue.put(bundle.getSymbolicName(), thingHandler);
+                }
+            }
+        }
+        if (bundle != null) {
+            logger.debug(
+                    "##### Meta-data of bundle '{}' is not fully loaded ({}), deferring handler initialization for thing '{}'",
+                    bundle.getSymbolicName(), initializerVetoes.get(bundle), thingHandler.getThing().getUID());
+            return;
+        }
+        logger.debug("##### All data has been loaded, going to initialize '{}'.", thingHandler.getThing().getUID());
         scheduler.schedule(new Runnable() {
             @Override
             public void run() {
-                logger.debug("Calling initialize handler for thing '{}' at '{}'.", thing.getUID(), thingHandler);
+                logger.debug("##### Calling initialize handler for thing '{}' at '{}'.",
+                        thingHandler.getThing().getUID(), thingHandler);
                 try {
                     SafeMethodCaller.call(new SafeMethodCaller.ActionWithException<Void>() {
                         @Override
@@ -667,18 +701,35 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
                         }
                     });
                 } catch (TimeoutException ex) {
-                    logger.warn("Initializing handler for thing '{}' takes more than {}ms.", thing.getUID(),
-                            SafeMethodCaller.DEFAULT_TIMEOUT);
+                    logger.warn("Initializing handler for thing '{}' takes more than {}ms.",
+                            thingHandler.getThing().getUID(), SafeMethodCaller.DEFAULT_TIMEOUT);
                 } catch (Exception ex) {
                     ThingStatusInfo statusInfo = buildStatusInfo(ThingStatus.UNINITIALIZED,
                             ThingStatusDetail.HANDLER_INITIALIZING_ERROR,
                             ex.getCause() != null ? ex.getCause().getMessage() : ex.getMessage());
-                    setThingStatus(thing, statusInfo);
-                    logger.error("Exception occured while initializing handler of thing '" + thing.getUID() + "': "
-                            + ex.getMessage(), ex);
+                    setThingStatus(thingHandler.getThing(), statusInfo);
+                    logger.error("Exception occured while initializing handler of thing '"
+                            + thingHandler.getThing().getUID() + "': " + ex.getMessage(), ex);
                 }
             }
         }, 0, TimeUnit.NANOSECONDS);
+    }
+
+    @Override
+    public void bundleFinished(BundleProcessor context, Bundle bundle) {
+        logger.debug("##### '{}' finished loading meta-data of bundle '{}'", context, bundle.getSymbolicName());
+        initializerVetoes.remove(bundle, context);
+        logger.debug("##### '{}' still vetoed by '{}'", bundle.getSymbolicName(), initializerVetoes.get(bundle));
+        logger.debug("##### '{}' queued '{}'", bundle.getSymbolicName(),
+                initializerQueue.get(bundle.getSymbolicName()));
+        if (initializerVetoes.get(bundle).isEmpty()) {
+            synchronized (initializerQueue) {
+                for (ThingHandler thingHandler : initializerQueue.removeAll(bundle.getSymbolicName())) {
+                    logger.debug("##### Going to initialize '{}'", thingHandler.getThing().getUID());
+                    initializeHandler(thingHandler);
+                }
+            }
+        }
     }
 
     private boolean isInitialized(Thing thing) {
@@ -1009,6 +1060,16 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
 
     protected void unsetThingTypeRegistry(ThingTypeRegistry thingTypeRegistry) {
         this.thingTypeRegistry = null;
+    }
+
+    protected void setBundleProcessor(BundleProcessor bundleProcessor) {
+        bundleProcessors.add(bundleProcessor);
+        bundleProcessor.registerListener(this);
+    }
+
+    protected void unsetBundleProcessor(BundleProcessor bundleProcessor) {
+        bundleProcessor.unregisterListener(this);
+        bundleProcessors.remove(bundleProcessor);
     }
 
 }

bundles/core/org.eclipse.smarthome.core.thing.test/src/test/groovy/org/eclipse/smarthome/core/thing/internal/ThingManagerOSGiTest.groovy
@@ -26,6 +26,7 @@ import org.eclipse.smarthome.core.items.events.ItemStateEvent
 import org.eclipse.smarthome.core.library.types.DecimalType
 import org.eclipse.smarthome.core.library.types.StringType
 import org.eclipse.smarthome.core.thing.Bridge
+import org.eclipse.smarthome.core.thing.BundleProcessor
 import org.eclipse.smarthome.core.thing.Channel
 import org.eclipse.smarthome.core.thing.ChannelUID
 import org.eclipse.smarthome.core.thing.ManagedThingProvider
@@ -36,6 +37,7 @@ import org.eclipse.smarthome.core.thing.ThingStatusDetail
 import org.eclipse.smarthome.core.thing.ThingStatusInfo
 import org.eclipse.smarthome.core.thing.ThingTypeUID
 import org.eclipse.smarthome.core.thing.ThingUID
+import org.eclipse.smarthome.core.thing.BundleProcessor.BundleProcessorListener
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory
 import org.eclipse.smarthome.core.thing.binding.ThingHandler
@@ -58,6 +60,7 @@ import org.eclipse.smarthome.test.OSGiTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.osgi.framework.Bundle
 import org.osgi.service.component.ComponentContext
 
 import com.google.common.collect.Sets
@@ -669,7 +672,9 @@ class ThingManagerOSGiTest extends OSGiTest {
         def thingHandler = [
             setCallback: { callbackArg -> callback = callbackArg },
             initialize: { initializedCalled = true},
-            dispose: {}
+            getThing: {-> return thing},
+            dispose: {
+            }
         ] as ThingHandler
 
         def thingHandlerFactory = [
@@ -704,7 +709,65 @@ class ThingManagerOSGiTest extends OSGiTest {
         // ThingHandler.initialize() called, thing status is ONLINE.NONE
         waitForAssert({
         assertThat initializedCalled, is(true)
+            assertThat initializedCalled, is(true)
+            assertThat thing.getStatusInfo(), is(statusInfo)
+        }, 4000)
+    }
+
+    @Test
+    void 'ThingManager waits with initialize until bundle processing is finished'() {
+        registerThingTypeProvider()
+
+        ThingHandlerCallback callback;
+        def initializedCalled = false;
+        def thing = ThingBuilder.create(new ThingUID("binding:type:thingId")).build()
+        def thingHandler = [
+            setCallback: { callbackArg -> callback = callbackArg },
+            initialize: { initializedCalled = true},
+            getThing: {-> return thing},
+            dispose: {
+            }
+        ] as ThingHandler
+
+        def thingHandlerFactory = [
+            supportsThingType: {ThingTypeUID thingTypeUID -> true},
+            registerHandler: {thingArg, handlerCallback ->
+                registerService(thingHandler,[
+                    (ThingHandler.SERVICE_PROPERTY_THING_ID): thing.getUID(),
+                    (ThingHandler.SERVICE_PROPERTY_THING_TYPE): thing.getThingTypeUID()
+                ] as Hashtable)},
+            unregisterHandler: {},
+            removeThing: {
+            }
+        ] as ThingHandlerFactory
+        registerService(thingHandlerFactory)
+
+        boolean finished = false;
+        BundleProcessorListener listener = null;
+        Bundle bundle = ["getSymbolicName": {-> return "test"}] as Bundle;
+        def bundleProcessor = [
+            "isFinishedLoading": { object -> if (finished) return null else return bundle },
+            "registerListener": {l -> listener = l},
+            "unregisterListener": {l -> listener = null}
+        ] as BundleProcessor
+        registerService(bundleProcessor)
+
+        def statusInfo = ThingStatusInfoBuilder.create(ThingStatus.UNINITIALIZED, ThingStatusDetail.NONE).build()
         assertThat thing.getStatusInfo(), is(statusInfo)
+
+        managedThingProvider.add(thing)
+
+        statusInfo = ThingStatusInfoBuilder.create(ThingStatus.INITIALIZING, ThingStatusDetail.NONE).build()
+        assertThat initializedCalled, is(false)
+        assertThat thing.getStatusInfo(), is(statusInfo)
+
+        finished = true;
+        listener.bundleFinished(bundleProcessor, bundle)
+
+        // ThingHandler.initialize() called, thing status is ONLINE.NONE
+        waitForAssert({
+            assertThat initializedCalled, is(true)
+            assertThat thing.getStatusInfo(), is(statusInfo)
         }, 4000)
     }
 

bundles/config/org.eclipse.smarthome.config.xml.test/src/test/groovy/org/eclipse/smarthome/config/xml/test/AbstractAsyncBundleProcessorTest.groovy
@@ -13,8 +13,9 @@ import static org.junit.Assert.*
 import static org.junit.matchers.JUnitMatchers.*
 
 import org.eclipse.smarthome.config.xml.osgi.AbstractAsyncBundleProcessor
+import org.eclipse.smarthome.core.thing.BundleProcessor.BundleProcessorListener
 import org.eclipse.smarthome.test.SyntheticBundleInstaller
-import org.junit.Before;
+import org.junit.Before
 import org.junit.Test
 import org.osgi.framework.Bundle
 
@@ -120,5 +121,27 @@ class AbstractAsyncBundleProcessorTest {
 
         SyntheticBundleInstaller.waitUntilLoadingFinished(b1)
         SyntheticBundleInstaller.waitUntilLoadingFinished(b2)
+    }
+
+    @Test
+    void 'assert listeners informed when loading completed'() {
+        def List<String> called = new ArrayList<String>()
+        AbstractAsyncBundleProcessor acl = new AbstractAsyncBundleProcessor() {
+                    @Override
+                    protected void processBundle(Bundle bundle) {
+                        Thread.sleep(1000)
+                        called.add(bundle.getSymbolicName())
+                    }
+                };
+        acl.registerListener([
+            "bundleFinished": {context, Bundle bundle ->
+                called.add(bundle.getSymbolicName())
+            }
+        ] as BundleProcessorListener)
+
+        acl.addingBundle b1
+        assertFalse called.contains("b1")
+        SyntheticBundleInstaller.waitUntilLoadingFinished(b1)
+        assertTrue called.contains("b1");
     }
 }

bundles/config/org.eclipse.smarthome.config.xml/META-INF/MANIFEST.MF
@@ -22,6 +22,7 @@ Import-Package: com.thoughtworks.xstream,
  org.eclipse.smarthome.config.xml.util,
  org.eclipse.smarthome.core.common.osgi,
  org.eclipse.smarthome.core.i18n,
+ org.eclipse.smarthome.core.thing,
  org.osgi.framework,
  org.osgi.util.tracker,
  org.slf4j

bundles/config/org.eclipse.smarthome.config.xml/src/main/java/org/eclipse/smarthome/config/xml/osgi/XmlDocumentBundleTracker.java
@@ -14,9 +14,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.smarthome.config.xml.util.XmlDocumentReader;
+import org.eclipse.smarthome.core.thing.BundleProcessor;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.BundleTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,6 +51,10 @@ public class XmlDocumentBundleTracker<T> extends BundleTracker<Bundle> {
 
     private final AbstractAsyncBundleProcessor asyncLoader;
 
+    private BundleContext bundleContext;
+
+    private ServiceRegistration asyncLoaderRegistration;
+
     /**
      * Creates a new instance of this class with the specified parameters.
      *
@@ -71,6 +77,7 @@ public class XmlDocumentBundleTracker<T> extends BundleTracker<Bundle> {
                     throws IllegalArgumentException {
 
         super(bundleContext, Bundle.ACTIVE, null);
+        this.bundleContext = bundleContext;
 
         if (bundleContext == null) {
             throw new IllegalArgumentException("The BundleContext must not be null!");
@@ -141,12 +148,17 @@ public class XmlDocumentBundleTracker<T> extends BundleTracker<Bundle> {
     @Override
     public final synchronized void open() {
         super.open();
+        asyncLoaderRegistration = bundleContext.registerService(BundleProcessor.class.getName(), asyncLoader, null);
     }
 
     @Override
     public final synchronized void close() {
         super.close();
         this.bundleDocumentProviderMap.clear();
+        if (asyncLoaderRegistration != null) {
+            asyncLoaderRegistration.unregister();
+            asyncLoaderRegistration = null;
+        }
     }
 
     private XmlDocumentProvider<T> acquireXmlDocumentProvider(Bundle bundle) {

commit a68821beed355b9223fcef902b256aed94df3d1d

fixed tests & reduced logging spam
...as they got broken by #1419

Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

bundles/config/org.eclipse.smarthome.config.xml/src/main/java/org/eclipse/smarthome/config/xml/osgi/AbstractAsyncBundleProcessor.java
@@ -173,10 +173,10 @@ public abstract class AbstractAsyncBundleProcessor implements BundleProcessor {
     public Bundle isFinishedLoading(Object object) {
         Bundle bundle = getBundle(object.getClass());
         if (queue.contains(bundle)) {
-            logger.debug("##### Bundle {} is still loading", bundle.getSymbolicName());
+            logger.trace("Bundle {} is still loading", bundle.getSymbolicName());
             return bundle;
         } else {
-            logger.debug("##### Bundle {} is not currently loading", bundle.getSymbolicName());
+            logger.trace("Bundle {} is not currently loading", bundle.getSymbolicName());
             return null;
         }
     }
@@ -191,7 +191,7 @@ public abstract class AbstractAsyncBundleProcessor implements BundleProcessor {
 
         if (classLoader instanceof BundleReference) {
             Bundle bundle = ((BundleReference) classLoader).getBundle();
-            logger.debug("##### Bundle of {} is {}", classFromBundle, bundle.getSymbolicName());
+            logger.trace("Bundle of {} is {}", classFromBundle, bundle.getSymbolicName());
             return bundle;
         }
         return null;

bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/internal/ThingManager.java
@@ -672,26 +672,26 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
         for (BundleProcessor proc : bundleProcessors) {
             bundle = proc.isFinishedLoading(thingHandler);
             if (bundle != null) {
-                logger.debug("##### Marking '{}' vetoed by '{}'", bundle.getSymbolicName(), proc);
+                logger.trace("Marking '{}' vetoed by '{}'", bundle.getSymbolicName(), proc);
                 initializerVetoes.put(bundle, proc);
                 if (!initializerQueue.containsEntry(bundle, thingHandler)) {
-                    logger.debug("##### Queueing '{}' in bundle '{}'", thingHandler, bundle.getSymbolicName());
+                    logger.trace("Queueing '{}' in bundle '{}'", thingHandler, bundle.getSymbolicName());
                     initializerQueue.put(bundle.getSymbolicName(), thingHandler);
                 }
             }
         }
         if (bundle != null) {
             logger.debug(
-                    "##### Meta-data of bundle '{}' is not fully loaded ({}), deferring handler initialization for thing '{}'",
+                    "Meta-data of bundle '{}' is not fully loaded ({}), deferring handler initialization for thing '{}'",
                     bundle.getSymbolicName(), initializerVetoes.get(bundle), thingHandler.getThing().getUID());
             return;
         }
-        logger.debug("##### All data has been loaded, going to initialize '{}'.", thingHandler.getThing().getUID());
+        logger.debug("All data has been loaded, going to initialize '{}'.", thingHandler.getThing().getUID());
         scheduler.schedule(new Runnable() {
             @Override
             public void run() {
-                logger.debug("##### Calling initialize handler for thing '{}' at '{}'.",
-                        thingHandler.getThing().getUID(), thingHandler);
+                logger.debug("Calling initialize handler for thing '{}' at '{}'.", thingHandler.getThing().getUID(),
+                        thingHandler);
                 try {
                     SafeMethodCaller.call(new SafeMethodCaller.ActionWithException<Void>() {
                         @Override
@@ -717,15 +717,14 @@ public class ThingManager extends AbstractItemEventSubscriber implements ThingTr
 
     @Override
     public void bundleFinished(BundleProcessor context, Bundle bundle) {
-        logger.debug("##### '{}' finished loading meta-data of bundle '{}'", context, bundle.getSymbolicName());
+        logger.debug("'{}' finished loading meta-data of bundle '{}'", context, bundle.getSymbolicName());
         initializerVetoes.remove(bundle, context);
-        logger.debug("##### '{}' still vetoed by '{}'", bundle.getSymbolicName(), initializerVetoes.get(bundle));
-        logger.debug("##### '{}' queued '{}'", bundle.getSymbolicName(),
-                initializerQueue.get(bundle.getSymbolicName()));
+        logger.trace("'{}' still vetoed by '{}'", bundle.getSymbolicName(), initializerVetoes.get(bundle));
+        logger.trace("'{}' queued '{}'", bundle.getSymbolicName(), initializerQueue.get(bundle.getSymbolicName()));
         if (initializerVetoes.get(bundle).isEmpty()) {
             synchronized (initializerQueue) {
                 for (ThingHandler thingHandler : initializerQueue.removeAll(bundle.getSymbolicName())) {
-                    logger.debug("##### Going to initialize '{}'", thingHandler.getThing().getUID());
+                    logger.trace("Going to initialize '{}'", thingHandler.getThing().getUID());
                     initializeHandler(thingHandler);
                 }
             }

bundles/core/org.eclipse.smarthome.core.thing.test/src/test/groovy/org/eclipse/smarthome/core/thing/internal/ThingManagerOSGiTest.groovy
@@ -127,7 +127,10 @@ class ThingManagerOSGiTest extends OSGiTest {
         def thingHandlerFactory = [
             supportsThingType: {ThingTypeUID thingTypeUID -> true},
             registerHandler: { Thing thing, ThingHandlerCallback callback ->
-                def thingHandler = {setCallback: {}} as ThingHandler
+                def thingHandler = [
+                    setCallback: {},
+                    getThing: {return THING}
+                ] as ThingHandler
                 registerService(thingHandler,[
                     (ThingHandler.SERVICE_PROPERTY_THING_ID): THING.getUID(),
                     (ThingHandler.SERVICE_PROPERTY_THING_TYPE): THING.getThingTypeUID()
@@ -203,7 +206,9 @@ class ThingManagerOSGiTest extends OSGiTest {
             thingUpdated: { thingUpdatedWasCalled = true },
             setCallback: {callbackArg -> callback = callbackArg },
             initialize: {},
-            dispose: {}
+            dispose: {
+            },
+            getThing: {return THING}
         ] as ThingHandler
 
         registerService(thingHandler,[
@@ -323,6 +328,7 @@ class ThingManagerOSGiTest extends OSGiTest {
             setCallback: {},
             initialize: {},
             dispose: {},
+            getThing: {return THING}
         ] as ThingHandler
 
         def thingHandlerFactory = [
@@ -340,12 +346,16 @@ class ThingManagerOSGiTest extends OSGiTest {
         assertThat THING.statusInfo, is(statusInfo)
 
         managedThingProvider.add(THING)
-        statusInfo = ThingStatusInfoBuilder.create(ThingStatus.INITIALIZING, ThingStatusDetail.NONE).build()
-        assertThat THING.statusInfo, is(statusInfo)
+        waitForAssert({
+            statusInfo = ThingStatusInfoBuilder.create(ThingStatus.INITIALIZING, ThingStatusDetail.NONE).build()
+            assertThat THING.statusInfo, is(statusInfo)
+        })
 
         unregisterService(THING.getHandler())
-        statusInfo = ThingStatusInfoBuilder.create(ThingStatus.UNINITIALIZED, ThingStatusDetail.HANDLER_MISSING_ERROR).build()
-        assertThat THING.statusInfo, is(statusInfo)
+        waitForAssert({
+            statusInfo = ThingStatusInfoBuilder.create(ThingStatus.UNINITIALIZED, ThingStatusDetail.HANDLER_MISSING_ERROR).build()
+            assertThat THING.statusInfo, is(statusInfo)
+        })
     }
 
     @Test
@@ -536,7 +546,9 @@ class ThingManagerOSGiTest extends OSGiTest {
         def thingHandler = [
             setCallback: {callbackArg -> callback = callbackArg },
             initialize: {},
-            dispose: {}
+            dispose: {
+            },
+            getThing: {return THING}
         ] as ThingHandler
 
         def thingHandlerFactory = [