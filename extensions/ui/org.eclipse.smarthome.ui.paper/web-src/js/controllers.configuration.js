f64d068cee52ed44e85dd6ba2b94f4469a710fad
Aoun Bukhari <bukhari@itemis.de>
2016-05-19 16:51:31 +0200

    added tabs on configurations page
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/controllers.configuration.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/controllers.configuration.js
@@ -141,9 +141,22 @@ angular.module('PaperUI.controllers.configuration', []).controller('Configuratio
 }).controller('ServicesController', function($scope, $mdDialog, serviceConfigService, toastService) {
     $scope.setSubtitle([ 'Services' ]);
     $scope.setHeaderText('Shows all configurable services.');
+    $scope.tabs = [];
     $scope.refresh = function() {
         serviceConfigService.getAll(function(services) {
-            $scope.services = services;
+            // $scope.services = services;
+            var arrOfIndex = [];
+            var index = 0;
+            angular.forEach(services, function(value) {
+                if (arrOfIndex[value.category] === undefined) {
+                    arrOfIndex[value.category] = index++;
+                }
+                if ($scope.tabs[arrOfIndex[value.category]] === undefined) {
+                    $scope.tabs[arrOfIndex[value.category]] = [];
+                    $scope.tabs[arrOfIndex[value.category]].category = value.category;
+                }
+                $scope.tabs[arrOfIndex[value.category]].push(value);
+            });
         });
     };
     $scope.add = function(serviceId, event) {
