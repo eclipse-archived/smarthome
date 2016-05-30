70f1910686bf769c00b695b740102a84fa3972b6
Markus Rathgeb <maggu2810@gmail.com>
2016-05-30 16:41:59 +0200

    Revert "item-channel-link registry: check item name if link is added"
    This reverts commit d71b8190da65cce193fa71bd73708d68510c2843.

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/link/ItemChannelLinkRegistry.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/link/ItemChannelLinkRegistry.java
@@ -11,7 +11,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.smarthome.core.items.ItemRegistry;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingRegistry;
@@ -27,7 +26,6 @@ import org.eclipse.smarthome.core.thing.link.events.LinkEventFactory;
  */
 public class ItemChannelLinkRegistry extends AbstractLinkRegistry<ItemChannelLink> {
 
-    private ItemRegistry itemRegistry;
     private ThingRegistry thingRegistry;
 
     /**
@@ -48,21 +46,6 @@ public class ItemChannelLinkRegistry extends AbstractLinkRegistry<ItemChannelLin
         }
 
         return channelUIDs;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @throws IllegalArgumentException if the item name is invalid
-     */
-    @Override
-    public ItemChannelLink add(ItemChannelLink element) {
-        if (!itemRegistry.isValidItemName(element.getItemName())) {
-            throw new IllegalArgumentException(
-                    String.format("The item name '%s' for the item-channel-link is not valid.", element.getItemName()));
-        }
-
-        return super.add(element);
     }
 
     /**
@@ -96,14 +79,6 @@ public class ItemChannelLinkRegistry extends AbstractLinkRegistry<ItemChannelLin
         }
 
         return things;
-    }
-
-    protected void setItemRegistry(final ItemRegistry itemRegistry) {
-        this.itemRegistry = itemRegistry;
-    }
-
-    protected void unsetItemRegistry(final ItemRegistry itemRegistry) {
-        this.itemRegistry = null;
     }
 
     protected void setThingRegistry(ThingRegistry thingRegistry) {
