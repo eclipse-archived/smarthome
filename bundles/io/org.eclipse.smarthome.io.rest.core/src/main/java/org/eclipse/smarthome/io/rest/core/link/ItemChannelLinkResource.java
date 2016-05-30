832eae4c942e6e831e02072c21be89d4b92726d0
Markus Rathgeb <maggu2810@gmail.com>
2016-05-30 16:11:40 +0200

    Revert "create link: validate item name"
    This reverts commit f8989f0b64e7a26894f52d99ad5f50520159a2b8.

--- bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/link/ItemChannelLinkResource.java
+++ bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/link/ItemChannelLinkResource.java
@@ -22,7 +22,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
-import org.eclipse.smarthome.core.items.ItemRegistry;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.link.AbstractLink;
 import org.eclipse.smarthome.core.thing.link.ItemChannelLink;
@@ -54,7 +53,6 @@ public class ItemChannelLinkResource implements RESTResource {
     public static final String PATH_LINKS = "links";
 
     private ItemChannelLinkRegistry itemChannelLinkRegistry;
-    private ItemRegistry itemRegistry;
     private ThingLinkManager thingLinkManager;
 
     @Context
@@ -82,13 +80,9 @@ public class ItemChannelLinkResource implements RESTResource {
     @Path("/{itemName}/{channelUID}")
     @ApiOperation(value = "Links item to a channel.")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 400, message = "Item already linked to the channel."),
-            @ApiResponse(code = 405, message = "Item name invalid.") })
+            @ApiResponse(code = 400, message = "Item already linked to the channel.") })
     public Response link(@PathParam("itemName") @ApiParam(value = "itemName") String itemName,
             @PathParam("channelUID") @ApiParam(value = "channelUID") String channelUid) {
-        if (!itemRegistry.isValidItemName(itemName)) {
-            return Response.status(Status.METHOD_NOT_ALLOWED).build();
-        }
         itemChannelLinkRegistry.add(new ItemChannelLink(itemName, new ChannelUID(channelUid)));
         return Response.ok().build();
     }
@@ -131,14 +125,6 @@ public class ItemChannelLinkResource implements RESTResource {
 
     protected void unsetItemChannelLinkRegistry(ItemChannelLinkRegistry itemChannelLinkRegistry) {
         this.itemChannelLinkRegistry = null;
-    }
-
-    protected void setItemRegistry(final ItemRegistry itemRegistry) {
-        this.itemRegistry = itemRegistry;
-    }
-
-    protected void unsetItemRegistry(final ItemRegistry itemRegistry) {
-        this.itemRegistry = null;
     }
 
     private Collection<AbstractLinkDTO> toBeans(Iterable<ItemChannelLink> links) {
