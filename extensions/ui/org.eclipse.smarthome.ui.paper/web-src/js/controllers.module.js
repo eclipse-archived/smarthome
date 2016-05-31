5b621e741cf08e3519a47547c61c0d08cd8c5e1d
Aoun Bukhari <bukhari@itemis.de>
2016-05-31 18:00:52 +0200

    Material bug workaround and UI fixes
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/controllers.module.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/controllers.module.js
@@ -4,9 +4,7 @@ angular.module('PaperUI.controllers.rules').controller('addModuleDialogControlle
 
     var objectFilter = $filter('filter');
     $scope.moduleData;
-    moduleTypeService.getByType({
-        mtype : type
-    }).$promise.then(function(data) {
+    moduleTypeService.getAll().$promise.then(function(data) {
         $scope.moduleData = objectFilter(data, {
             visibility : 'VISIBLE'
         });
@@ -22,19 +20,47 @@ angular.module('PaperUI.controllers.rules').controller('addModuleDialogControlle
     $scope.editMode = false;
     $scope.configuration = {};
     var originalConfiguration = {};
+    $scope.items = [];
+
+    $scope.selectChip = function(chip, textAreaName) {
+        var textArea = $("textarea[name=" + textAreaName + "]")[0];
+        var textBefore = textArea.value.substring(0, textArea.selectionStart);
+        var textAfter = textArea.value.substring(textArea.selectionStart, textArea.value.length);
+        $scope.configuration[textAreaName] = textBefore + chip.name + textAfter;
+    }
+
     function setConfigurations() {
         if ($scope.moduleData) {
             var params = filterByUid($scope.moduleData, $scope.module);
             var res = configService.getRenderingModel(params[0].configDescriptions);
+            var hasScript = false;
             angular.forEach(res, function(value) {
                 sharedProperties.updateParams(value);
+                hasScript = $.grep(value.parameters, function(parameter) {
+                    return parameter.context == 'script';
+                }).length > 0;
             });
 
             var index = sharedProperties.searchArray(sharedProperties.getModuleArray(type), $scope.id);
             if (index != -1) {
                 $scope.configuration = configService.convertValues(sharedProperties.getModuleArray(type)[index].configuration);
+
                 angular.copy($scope.configuration, originalConfiguration);
                 $scope.configArray = configService.getConfigAsArray($scope.configuration);
+                if (hasScript && type != 'trigger') {
+                    var triggers = sharedProperties.getModuleArray('trigger');
+                    angular.forEach(triggers, function(trigger, i) {
+                        var moduleType = filterByUid($scope.moduleData, trigger.type);
+                        $scope.items = $scope.items.concat(moduleType[0].outputs);
+                    });
+                    if (type == 'action') {
+                        var actions = sharedProperties.getModuleArray('action');
+                        for (var i = 0; i < sharedProperties.searchArray(actions, $scope.id); i++) {
+                            var moduleType = filterByUid($scope.moduleData, actions[i].type);
+                            $scope.items = $scope.items.concat(moduleType[0].outputs);
+                        }
+                    }
+                }
             }
         }
     }
@@ -100,4 +126,20 @@ angular.module('PaperUI.controllers.rules').controller('addModuleDialogControlle
         });
     }
 
+}).directive('mdChips', function() {
+    return {
+        restrict : 'E',
+        require : 'mdChips',
+        link : function(scope, element, attributes, ctrl) {
+            setTimeout(deferListeners, 500);
+            function deferListeners() {
+                var chipContents = element[0].getElementsByClassName('md-chip-content');
+                for (var i = 0; i < chipContents.length; i++) {
+                    chipContents[i].addEventListener("blur", function() {
+                        ctrl.$scope.$apply();
+                    });
+                }
+            }
+        }
+    }
 });
