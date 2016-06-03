f2a8e28a27cf0cd4cccc39c49846a63bec9fe6c6
Gaël L'hopital <gael@lhopital.org>
2016-06-03 09:31:41 +0200

    Replacing SortedMap by StringList (#236)
    Signed-off-by: Gaël L'hopital <glhopital@gmail.com>

---
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/library/types/StringListTypeTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.core.library.types;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Gaël L'hopital
+ */
+public class StringListTypeTest {
+    @Test
+    public void testEquals() {
+        final int DEST_IDX = 0;
+        final int ORIG_IDX = 1;
+
+        StringListType call1 = new StringListType("0179999998", "0699222222");
+        StringListType call2 = new StringListType("0699222222,0179999998");
+
+        assertEquals(call1.getValue(ORIG_IDX), call2.getValue(DEST_IDX));
+        assertEquals(call2.toString(), "0699222222,0179999998");
+
+        String serialized = new String("value1,value2,value=with=foo,value\\,with\\,foo,,\\,\\,foo");
+        StringListType call4 = new StringListType(serialized);
+        assertTrue(call4.getValue(1).toString().equals("value2"));
+        assertTrue(call4.getValue(4).toString().isEmpty());
+        assertTrue(call4.getValue(2).toString().equals("value=with=foo"));
+        assertTrue(call4.getValue(3).toString().equals("value,with,foo"));
+        assertTrue(call4.getValue(5).toString().equals(",,foo"));
+        assertTrue(call4.toString().equals(serialized));
+    }
+
+    @Test
+    public void testError() {
+        StringListType gct = new StringListType("foo=bar", "electric", "chair");
+        try {
+
+            // Index is between 0 and number of elements -1
+            @SuppressWarnings("unused")
+            String value = gct.getValue(-1);
+            fail();
+        } catch (Exception e) {
+            try {
+
+                @SuppressWarnings("unused")
+                String value = gct.getValue(3);
+                fail();
+            } catch (Exception e2) {
+                // That's what we expect.
+            }
+
+        }
+
+    }
+
+}
---
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/library/types/StringListType.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2014-2015 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.smarthome.core.library.types;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.List;
+
+import org.eclipse.smarthome.core.types.Command;
+import org.eclipse.smarthome.core.types.State;
+
+/**
+ * This type can be used for items that are dealing with telephony functionality.
+ *
+ * @author Thomas.Eichstaedt-Engelen / Initially CallType
+ * @author Gaël L'hopital - port to Eclipse SmartHome
+ *
+ */
+public class StringListType implements Command, State {
+
+    protected List<String> typeDetails;
+
+    // constants
+    static final public String DELIMITER = ",";
+    static final public String ESCAPED_DELIMITER = "\\" + DELIMITER;
+    static final public String REGEX_SPLITTER = "(?<!\\\\)" + DELIMITER;
+
+    public StringListType(StringType... rows) {
+        typeDetails = new ArrayList<String>(rows.length);
+        for (StringType row : rows) {
+            typeDetails.add(row.toString());
+        }
+    }
+
+    public StringListType(String... rows) {
+        typeDetails = Arrays.asList(rows);
+    }
+
+    /**
+     * Deserialize the input string,
+     * splitting it on every delimiter not preceeded by a backslash
+     */
+    public StringListType(String serialized) {
+        String[] rows = serialized.split(REGEX_SPLITTER);
+        typeDetails = new ArrayList<String>(rows.length);
+        for (String row : rows) {
+            typeDetails.add(row.replace(ESCAPED_DELIMITER, DELIMITER));
+        }
+    }
+
+    public String getValue(final int index) {
+        if (index < 0 || index >= typeDetails.size()) {
+            throw new IllegalArgumentException("Index is out of range");
+        }
+        return typeDetails.get(index);
+    }
+
+    /**
+     * <p>
+     * Formats the value of this type according to a pattern (@see
+     * {@link Formatter}). One single value of this type can be referenced
+     * by the pattern using an index. The item order is defined by the natural
+     * (alphabetical) order of their keys.
+     * </p>
+     *
+     * @param pattern the pattern to use containing indexes to reference the
+     *            single elements of this type.
+     */
+    @Override
+    public String format(String pattern) {
+        return String.format(pattern, typeDetails.toArray());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (String row : typeDetails) {
+            sb.append(row.replace(DELIMITER, ESCAPED_DELIMITER));
+            sb.append(DELIMITER);
+        }
+        sb.delete(sb.length() - 1, sb.length());
+        return sb.toString();
+    }
+
+}
