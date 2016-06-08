fbf94742eebe2650aa827d6cc6b4ad43f6e80807
Markus Rathgeb <maggu2810@gmail.com>
2016-06-08 09:35:40 +0200

    add documentation for isFinishedLoading
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/BundleProcessor.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/BundleProcessor.java
@@ -4,6 +4,12 @@ import org.osgi.framework.Bundle;
 
 public interface BundleProcessor {
 
+    /**
+     * Check if the loading is finished.
+     *
+     * @param objectFromBundle an object that identifies the bundle the request is for
+     * @return null if the loading is finished, a bundle reference if loading is not finished
+     */
     public Bundle isFinishedLoading(Object objectFromBundle);
 
     public void registerListener(BundleProcessorListener listener);
