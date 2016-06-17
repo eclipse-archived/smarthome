50584c14f371eb6a0eb9faa0168fafa69c28e79f
Marcel <marcelrv@users.noreply.github.com>
2016-06-17 10:38:18 -0700

    NTP string channel (#1693)
    Additional string channel that allows proper timezone representation
    Resolves issue #1666
    
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

--- extensions/binding/org.eclipse.smarthome.binding.ntp/src/main/java/org/eclipse/smarthome/binding/ntp/NtpBindingConstants.java
+++ extensions/binding/org.eclipse.smarthome.binding.ntp/src/main/java/org/eclipse/smarthome/binding/ntp/NtpBindingConstants.java
@@ -15,27 +15,29 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 /**
  * The {@link ntpBinding} class defines common constants, which are used across
  * the whole binding.
- * 
+ *
  * @author Marcel Verpaalen - Initial contribution
- * 
+ *
  */
 public class NtpBindingConstants {
 
-	public static final String BINDING_ID = "ntp";
+    public static final String BINDING_ID = "ntp";
 
-	// List of all Thing Type UIDs
-	public final static ThingTypeUID THING_TYPE_NTP = new ThingTypeUID(BINDING_ID, "ntp");
+    // List of all Thing Type UIDs
+    public final static ThingTypeUID THING_TYPE_NTP = new ThingTypeUID(BINDING_ID, "ntp");
 
-	// List of all Channel ids
-	public final static String CHANNEL_DATE_TIME = "dateTime";
+    // List of all Channel ids
+    public final static String CHANNEL_DATE_TIME = "dateTime";
+    public final static String CHANNEL_STING = "string";
 
-	// Custom Properties
-	public final static String PROPERTY_NTP_SERVER = "hostname";
-	public final static String PROPERTY_REFRESH_INTERVAL = "refreshInterval";
-	public final static String PROPERTY_REFRESH_NTP = "refreshNtp";
-	public final static String PROPERTY_TIMEZONE = "timeZone";
-	public final static String PROPERTY_LOCALE = "locale";
+    // Custom Properties
+    public final static String PROPERTY_NTP_SERVER = "hostname";
+    public final static String PROPERTY_REFRESH_INTERVAL = "refreshInterval";
+    public final static String PROPERTY_REFRESH_NTP = "refreshNtp";
+    public final static String PROPERTY_TIMEZONE = "timeZone";
+    public final static String PROPERTY_LOCALE = "locale";
+    public final static String PROPERTY_DATE_TIME_FORMAT = "DateTimeFormat";
 
-	public final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_NTP);
+    public final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_NTP);
 
 }
