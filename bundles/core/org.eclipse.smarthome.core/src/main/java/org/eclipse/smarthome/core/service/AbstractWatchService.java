3a67b12487466007f049eae4d28326ed839798b5
Dimitar Ivanov <dstivanov@users.noreply.github.com>
2016-06-27 15:12:45 +0300

    WatchService Provides File Name Instead of Relative Path (#1597)
    * The watch service is now providing the relative path to the file when a watch event is processed. The directory modifications can be now optionally watched. Added more tests to ensure that the watch service is adequately working (some tests are platform dependent). The list of handled test events is now synchronized as several threads are accessing it. FolderObserver and ConfigDispatcher are adapted to the changes in the AbstractWatchService.
    
    Signed-off-by: Dimitar Ivanov <dstivanov@gmail.com>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/service/AbstractWatchService.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/service/AbstractWatchService.java
@@ -14,8 +14,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -28,6 +31,7 @@ import org.slf4j.LoggerFactory;
  * >java docs</a> for more details
  *
  * @author Fabio Marini
+ * @author Dimitar Ivanov - added javadoc; introduced WatchKey to directory mapping for the queue reader
  *
  */
 public abstract class AbstractWatchService {
@@ -74,6 +78,7 @@ public abstract class AbstractWatchService {
         if (StringUtils.isNotBlank(pathToWatch)) {
             Path toWatch = Paths.get(pathToWatch);
             try {
+                final Map<WatchKey, Path> registeredWatchKeys = new HashMap<>();
                 if (watchSubDirectories()) {
                     watchService = FileSystems.getDefault().newWatchService();
 
@@ -81,16 +86,16 @@ public abstract class AbstractWatchService {
                         @Override
                         public FileVisitResult preVisitDirectory(Path subDir, BasicFileAttributes attrs)
                                 throws IOException {
-                            registerDirectory(subDir);
+                            registerDirectoryInternal(subDir, registeredWatchKeys);
                             return FileVisitResult.CONTINUE;
                         }
                     });
                 } else {
                     watchService = toWatch.getFileSystem().newWatchService();
-                    registerDirectory(toWatch);
+                    registerDirectoryInternal(toWatch, registeredWatchKeys);
                 }
 
-                AbstractWatchQueueReader reader = buildWatchQueueReader(watchService, toWatch);
+                AbstractWatchQueueReader reader = buildWatchQueueReader(watchService, toWatch, registeredWatchKeys);
 
                 Thread qr = new Thread(reader, "Dir Watcher");
                 qr.start();
@@ -100,41 +105,64 @@ public abstract class AbstractWatchService {
         }
     }
 
-    protected void stopWatchService() {
-    	if(watchService != null) {
-	    	try {
-	            watchService.close();
-	        } catch (IOException e) {
-	            logger.warn("Cannot deactivate folder watcher", e);
-	        }
-	
-	        watchService = null;
-    	}
+    private void registerDirectoryInternal(Path directory, Map<WatchKey, Path> registredWatchKeys) throws IOException {
+        WatchKey registrationKey = registerDirectory(directory);
+        if (registrationKey != null) {
+            registredWatchKeys.put(registrationKey, directory);
+        } else {
+            logger.info("The directory '{}' was not registered in the watch service", directory);
+        }
     }
 
     /**
-     * 
-     * @param watchService
-     * @param toWatch
-     * @return
+     * This method will close the {@link #watchService}.
      */
-    protected abstract AbstractWatchQueueReader buildWatchQueueReader(WatchService watchService, Path toWatch);
+    protected void stopWatchService() {
+        if (watchService != null) {
+            try {
+                watchService.close();
+            } catch (IOException e) {
+                logger.warn("Cannot deactivate folder watcher", e);
+            }
+
+            watchService = null;
+        }
+    }
 
     /**
+     * Build a queue reader to process the watch events, provided by the watch service for the given directory
      * 
-     * @return
+     * @param watchService the watch service, providing the watch events for the watched directory
+     * @param toWatch the directory being watched by the watch service
+     * @param registredWatchKeys a mapping between the registered directories and their {@link WatchKey registration
+     *            keys}.
+     * @return the concrete queue reader
+     */
+    protected abstract AbstractWatchQueueReader buildWatchQueueReader(WatchService watchService, Path toWatch,
+            Map<WatchKey, Path> registredWatchKeys);
+
+    /**
+     * @return the path to be watched as a {@link String}. The returned path should be applicable for creating a
+     *         {@link Path} with the {@link Paths#get(String, String...)} method.
      */
     protected abstract String getSourcePath();
 
     /**
+     * Determines whether the subdirectories of the source path (determined by the {@link #getSourcePath()}) will be
+     * watched or not.
      * 
-     * @return
+     * @return <code>true</code> if the subdirectories will be watched and <code>false</code> if only the source path
+     *         (determined by the {@link #getSourcePath()}) will be watched
      */
     protected abstract boolean watchSubDirectories();
 
     /**
-     * @param subDir
-     * @throws IOException
+     * Registers a directory to be watched by the watch service. The {@link WatchKey} of the registration should be
+     * provided.
+     * 
+     * @param directory the directory, which will be registered in the watch service
+     * @return The {@link WatchKey} of the registration or <code>null</code> if no registration has been done.
+     * @throws IOException if an error occurs while processing the given path
      */
-    protected abstract void registerDirectory(Path subDir) throws IOException;
+    protected abstract WatchKey registerDirectory(Path directory) throws IOException;
 }
