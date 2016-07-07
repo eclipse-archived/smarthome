a399ab8eb8fa32f0f6c1f08b8e7e10e8ccb6c457
Kai Kreuzer <kai@openhab.org>
2016-07-07 17:30:29 +0200

    fixes IndexOutOfBoundsException in LazyLinkingResource (#1826)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/model/org.eclipse.smarthome.model.script/src/org/eclipse/smarthome/model/script/ScriptRuntimeModule.java
+++ bundles/model/org.eclipse.smarthome.model.script/src/org/eclipse/smarthome/model/script/ScriptRuntimeModule.java
@@ -20,6 +20,7 @@ import org.eclipse.xtext.common.types.access.reflect.ReflectionTypeScopeProvider
 import org.eclipse.xtext.common.types.xtext.AbstractTypeScopeProvider;
 import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.generator.IGenerator.NullGenerator;
+import org.eclipse.xtext.linking.lazy.LazyURIEncoder;
 import org.eclipse.xtext.scoping.IScopeProvider;
 import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
 import org.eclipse.xtext.xbase.interpreter.IExpressionInterpreter;
@@ -70,6 +71,12 @@ public class ScriptRuntimeModule extends org.eclipse.smarthome.model.script.Abst
     @Override
     public ClassLoader bindClassLoaderToInstance() {
         return new ActionClassLoader(super.bindClassLoaderToInstance());
+    }
+
+    @Override
+    public void configureUseIndexFragmentsForLazyLinking(Binder binder) {
+        binder.bind(Boolean.TYPE).annotatedWith(Names.named(LazyURIEncoder.USE_INDEXED_FRAGMENTS_BINDING))
+                .toInstance(Boolean.FALSE);
     }
 
 }
