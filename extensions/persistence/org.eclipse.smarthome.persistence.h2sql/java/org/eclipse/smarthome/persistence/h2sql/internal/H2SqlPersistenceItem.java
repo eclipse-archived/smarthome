f9d4963b6af5f85fa522bc54ed3a3b27665c8a0a
Chris Jackson <chris@cd-jackson.com>
2016-07-30 22:47:47 +0100

    Initial commit of H2 persistence service
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ extensions/persistence/org.eclipse.smarthome.persistence.h2sql/java/org/eclipse/smarthome/persistence/h2sql/internal/H2SqlPersistenceItem.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.persistence.h2sql.internal;
+
+import java.util.Date;
+
+import org.eclipse.smarthome.core.persistence.PersistenceItemInfo;
+
+/**
+ * This is a class used to return item information from the H2SQL database.
+ *
+ * @author Chris Jackson - Initial contribution
+ *
+ */
+public class H2SqlPersistenceItem implements PersistenceItemInfo {
+
+    final private String name;
+    final private Integer rows;
+    final private Date earliest;
+    final private Date latest;
+
+    public H2SqlPersistenceItem(String name, Integer rows, Date earliest, Date latest) {
+        this.name = name;
+        this.rows = rows;
+        this.earliest = earliest;
+        this.latest = latest;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Integer getCount() {
+        return rows;
+    }
+
+    @Override
+    public Date getEarliest() {
+        return earliest;
+    }
+
+    @Override
+    public Date getLatest() {
+        return latest;
+    }
+}
