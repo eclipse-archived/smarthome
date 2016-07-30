88f65ec1d1e7eb59db8368a2c1d81477f811041f
lolodomo <lg.hc@free.fr>
2016-07-30 10:14:18 +0200

    Fix mDNS discovery (bug #577 #1745) (#1742)
    One JmDNS instance is now created for each local IP address.
    Loopback and Link-local addresses are ignored.
    Only network interfaces that are up at openHAB startup are considered.
    
    This change is important when running openHAB on a machine having one IP
    v4 and one IP v6. On such a machine, two things will now work correctly:
    - the discovery of IP v4 services
    - the publishing of openHAB services on IP v4 and IP v6
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/io/org.eclipse.smarthome.io.transport.mdns/src/main/java/org/eclipse/smarthome/io/transport/mdns/internal/MDNSServiceImpl.java
+++ bundles/io/org.eclipse.smarthome.io.transport.mdns/src/main/java/org/eclipse/smarthome/io/transport/mdns/internal/MDNSServiceImpl.java
@@ -12,8 +12,6 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.Executors;
 
-import javax.jmdns.ServiceInfo;
-
 import org.eclipse.smarthome.io.transport.mdns.MDNSClient;
 import org.eclipse.smarthome.io.transport.mdns.MDNSService;
 import org.eclipse.smarthome.io.transport.mdns.ServiceDescription;
@@ -32,7 +30,7 @@ public class MDNSServiceImpl implements MDNSService {
     private final Logger logger = LoggerFactory.getLogger(MDNSServiceImpl.class);
     private MDNSClient mdnsClient;
 
-    private Set<ServiceInfo> servicesToRegisterQueue = new CopyOnWriteArraySet<>();
+    private Set<ServiceDescription> servicesToRegisterQueue = new CopyOnWriteArraySet<>();
 
     public MDNSServiceImpl() {
     }
@@ -44,13 +42,15 @@ public class MDNSServiceImpl implements MDNSService {
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    for (ServiceInfo serviceInfo : servicesToRegisterQueue) {
+                    logger.debug("Registering {} queued services", servicesToRegisterQueue.size());
+                    for (ServiceDescription description : servicesToRegisterQueue) {
                         try {
-                            logger.debug("Registering new service " + serviceInfo.getType() + " at port "
-                                    + String.valueOf(serviceInfo.getPort()));
-                            mdnsClient.getClient().registerService(serviceInfo);
+                            mdnsClient.registerService(description);
                         } catch (IOException e) {
                             logger.error(e.getMessage());
+                        } catch (IllegalStateException e) {
+                            logger.debug("Not registering service {}, because service is already deactivated!",
+                                    description.serviceType);
                         }
                     }
                     servicesToRegisterQueue.clear();
@@ -72,23 +72,18 @@ public class MDNSServiceImpl implements MDNSService {
         if (mdnsClient == null) {
             // queue the service to register it as soon as the mDNS client is
             // available
-            ServiceInfo serviceInfo = ServiceInfo.create(description.serviceType, description.serviceName,
-                    description.servicePort, 0, 0, description.serviceProperties);
-            servicesToRegisterQueue.add(serviceInfo);
+            servicesToRegisterQueue.add(description);
         } else {
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    ServiceInfo serviceInfo = ServiceInfo.create(description.serviceType, description.serviceName,
-                            description.servicePort, 0, 0, description.serviceProperties);
                     try {
-                        logger.debug("Registering new service " + description.serviceType + " at port "
-                                + String.valueOf(description.servicePort));
-                        mdnsClient.getClient().registerService(serviceInfo);
+                        mdnsClient.registerService(description);
                     } catch (IOException e) {
                         logger.error(e.getMessage());
                     } catch (IllegalStateException e) {
-                        logger.debug("Not registering service, because service is already deactivated!");
+                        logger.debug("Not registering service {}, because service is already deactivated!",
+                                description.serviceType);
                     }
                 }
             };
@@ -101,14 +96,9 @@ public class MDNSServiceImpl implements MDNSService {
      */
     @Override
     public void unregisterService(ServiceDescription description) {
-        if (mdnsClient == null) {
-            return;
+        if (mdnsClient != null) {
+            mdnsClient.unregisterService(description);
         }
-        ServiceInfo serviceInfo = ServiceInfo.create(description.serviceType, description.serviceName,
-                description.servicePort, 0, 0, description.serviceProperties);
-        logger.debug("Unregistering service " + description.serviceType + " at port "
-                + String.valueOf(description.servicePort));
-        mdnsClient.getClient().unregisterService(serviceInfo);
     }
 
     /**
@@ -116,7 +106,7 @@ public class MDNSServiceImpl implements MDNSService {
      */
     protected void unregisterAllServices() {
         if (mdnsClient != null) {
-            mdnsClient.getClient().unregisterAllServices();
+            mdnsClient.unregisterAllServices();
         }
     }
 
@@ -126,13 +116,9 @@ public class MDNSServiceImpl implements MDNSService {
 
     public void deactivate() {
         unregisterAllServices();
-        try {
-            if (mdnsClient != null) {
-                mdnsClient.getClient().close();
-                logger.debug("mDNS service has been stopped");
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+        if (mdnsClient != null) {
+            mdnsClient.close();
+            logger.debug("mDNS service has been stopped");
         }
     }
 
