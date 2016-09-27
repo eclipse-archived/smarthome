948e3939e101f4fd6c38d8677e40a65149d8bf96
aounhaider1 <aounhaider1@gmail.com>
2016-09-27 17:39:15 +0200

    Bug fix: module ids not unique (#2208)
    * Bug fix: module ids not unique
    
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/shared.properties.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/shared.properties.js
@@ -4,10 +4,8 @@ angular.module('PaperUI.controllers').service('sharedProperties', function() {
     var triggersArray = [];
     var actionsArray = [];
     var conditionsArray = [];
-    var tId = 1, aId = 1, cId = 1;
     var params = [];
     var moduleTypes = [];
-    var isDirty = false;
     return {
         updateParams : function(elem) {
             params.push(elem);
@@ -29,18 +27,16 @@ angular.module('PaperUI.controllers').service('sharedProperties', function() {
         },
         updateModule : function(type, value) {
             var modArr = this.getModuleArray(type);
-
+            var maxId = this.getMaxModuleId();
             if (!value.id) {
-                value.id = type + "_" + tId;
+                value.id = maxId;
                 modArr.push(value);
-                tId++;
             } else {
                 var index = this.searchArray(modArr, value.id);
                 if (index != -1) {
                     modArr[index] = value;
                 } else {
                     modArr.push(value);
-                    tId++;
                 }
             }
         },
@@ -61,7 +57,7 @@ angular.module('PaperUI.controllers').service('sharedProperties', function() {
             angular.forEach(modArr, function(value) {
                 var type = typeof value.uid === "undefined" ? value.type : value.uid;
                 $moduleJSON.push({
-                    "id" : value.id ? value.id : modArr.mtype + "_" + i,
+                    "id" : value.id,
                     "label" : value.label,
                     "description" : value.description,
                     "type" : type,
@@ -76,10 +72,6 @@ angular.module('PaperUI.controllers').service('sharedProperties', function() {
             triggersArray = [];
             actionsArray = [];
             conditionsArray = [];
-            tId = 1;
-            aId = 1;
-            cId = 1;
-
         },
         removeFromArray : function(opt, id) {
             var arr = null;
@@ -133,6 +125,19 @@ angular.module('PaperUI.controllers').service('sharedProperties', function() {
 
         setModuleTypes : function(mTypes) {
             moduleTypes = mTypes;
+        },
+        getMaxModuleId : function() {
+            var max_id = 0;
+            var modules = [ 'trigger', 'action', 'condition' ];
+            for (var m = 0; m < modules.length; m++) {
+                var modArr = this.getModuleArray(modules[m]);
+                for (var i = 0; i < modArr.length; i++) {
+                    if (modArr[i].id && !isNaN(parseInt(modArr[i].id)) && parseInt(modArr[i].id) > max_id) {
+                        max_id = parseInt(modArr[i].id);
+                    }
+                }
+            }
+            return ++max_id;
         }
     }
 });
