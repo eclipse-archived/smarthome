09050cef9d6f857bd2683e90836f4681f0ad975c
Miki Jankov <miki.jankov87@gmail.com>
2016-09-28 09:49:24 +0200

    Make thingTypeDTO's lighter (#2199)
    Make thingTypeDTO's lighter by not setting properties, configParameters and channels
    Introduced StrippedThingTypeDTO  to be returned via rest call and corresponding Mapper
    
    Signed-off-by: Miki Jankov <miki.jankov87@gmail.com>

---
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/dto/StrippedThingTypeDTO.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.core.thing.dto;
+
+import java.util.List;
+
+/**
+ * This is a data transfer object that is used to serialize stripped thing types.
+ * Stripped thing types exclude the parameters, configDescription and channels
+ *
+ * @author Miki Jankov - Initial contribution
+ *
+ */
+public class StrippedThingTypeDTO {
+
+    public String UID;
+    public String label;
+    public String description;
+    public boolean listed;
+    public List<String> supportedBridgeTypeUIDs;
+    public boolean bridge;
+
+    public StrippedThingTypeDTO() {
+    }
+
+    public StrippedThingTypeDTO(String UID, String label, String description, boolean listed,
+            List<String> supportedBridgeTypeUIDs, boolean bridge) {
+        this.UID = UID;
+        this.label = label;
+        this.description = description;
+        this.listed = listed;
+        this.supportedBridgeTypeUIDs = supportedBridgeTypeUIDs;
+        this.bridge = bridge;
+    }
+}
