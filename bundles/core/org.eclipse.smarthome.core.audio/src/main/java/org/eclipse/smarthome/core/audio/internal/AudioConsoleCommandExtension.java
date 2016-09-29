3589b58c845f017aba5150f45e9b55973766b87e
Kai Kreuzer <kai@openhab.org>
2016-09-29 07:48:43 +0200

    Split general audio from voice support (#2225)
    * - moved core.audio into a seperate bundle
    - introduced AudioManager and made VoiceManager depend on it
    - added audio console commands
    - introduced URLAudioStream
    - introduced FileAudioStream
    - added volume control to sinks
    
    Also-by: Karel Goderis <karel.goderis@me.com>
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * addressed review comments
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ bundles/core/org.eclipse.smarthome.core.audio/src/main/java/org/eclipse/smarthome/core/audio/internal/AudioConsoleCommandExtension.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.core.audio.internal;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.eclipse.smarthome.core.audio.AudioException;
+import org.eclipse.smarthome.core.audio.AudioManager;
+import org.eclipse.smarthome.io.console.Console;
+import org.eclipse.smarthome.io.console.extensions.AbstractConsoleCommandExtension;
+
+/**
+ * Console command extension for all audio features.
+ *
+ * @author Karel Goderis - Initial contribution and API
+ * @author Kai Kreuzer - refactored to match AudioManager implementation
+ *
+ */
+public class AudioConsoleCommandExtension extends AbstractConsoleCommandExtension {
+
+    private static final String SUBCMD_PLAY = "play";
+    private static final String SUBCMD_STREAM = "stream";
+    private static final String SUBCMD_SOURCES = "sources";
+    private static final String SUBCMD_SINKS = "sinks";
+
+    private AudioManager audioManager;
+
+    public AudioConsoleCommandExtension() {
+        super("audio", "Commands around audio enablement features.");
+    }
+
+    @Override
+    public List<String> getUsages() {
+        return Arrays.asList(new String[] {
+                buildCommandUsage(SUBCMD_PLAY + " <sink> <filename>",
+                        "plays a sound file from the sounds folder through the optionally specified audio sink(s)"),
+                buildCommandUsage(SUBCMD_STREAM + " <sink> <url>",
+                        "streams the sound from the url through the optionally specified audio sink(s)"),
+                buildCommandUsage(SUBCMD_SOURCES, "lists the audio sources"),
+                buildCommandUsage(SUBCMD_SINKS, "lists the audio sinks") });
+
+    }
+
+    @Override
+    public void execute(String[] args, Console console) {
+        if (args.length > 0) {
+            String subCommand = args[0];
+            switch (subCommand) {
+                case SUBCMD_PLAY:
+                    if (args.length > 1) {
+                        play((String[]) ArrayUtils.subarray(args, 1, args.length), console);
+                    } else {
+                        console.println(
+                                "Specify file to play, and optionally the sink(s) to use (e.g. 'play javasound hello.mp3')");
+                    }
+                    return;
+                case SUBCMD_STREAM:
+                    if (args.length > 1) {
+                        stream((String[]) ArrayUtils.subarray(args, 1, args.length), console);
+                    } else {
+                        console.println("Specify url to stream from, and optionally the sink(s) to use");
+                    }
+                    return;
+                case SUBCMD_SOURCES:
+                    listSources(console);
+                    return;
+                case SUBCMD_SINKS:
+                    listSinks(console);
+                    return;
+                default:
+                    break;
+            }
+        } else {
+            printUsage(console);
+        }
+    }
+
+    private void listSources(Console console) {
+        if (audioManager.getSourceIds().size() > 0) {
+            for (String source : audioManager.getSourceIds()) {
+                console.println(source);
+            }
+        } else {
+            console.println("No audio sources found.");
+        }
+    }
+
+    private void listSinks(Console console) {
+        if (audioManager.getSinkIds().size() > 0) {
+            for (String sink : audioManager.getSinkIds()) {
+                console.println(sink);
+            }
+        } else {
+            console.println("No audio sinks found.");
+        }
+    }
+
+    private void play(String[] args, Console console) {
+        if (args.length == 1) {
+            try {
+                audioManager.playFile(args[0]);
+            } catch (AudioException e) {
+                console.println(e.getMessage());
+            }
+        } else if (args.length == 2) {
+            Set<String> sinks = audioManager.getSinks(args[0]);
+            for (String aSink : sinks) {
+                try {
+                    audioManager.playFile(args[1], aSink);
+                } catch (AudioException e) {
+                    console.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private void stream(String[] args, Console console) {
+        if (args.length == 1) {
+            try {
+                audioManager.stream(args[0]);
+            } catch (AudioException e) {
+                console.println(e.getMessage());
+            }
+        } else if (args.length == 2) {
+            Set<String> sinks = audioManager.getSinks(args[0]);
+            for (String aSink : sinks) {
+                try {
+                    audioManager.stream(args[1], aSink);
+                } catch (AudioException e) {
+                    console.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    protected void setAudioManager(AudioManager audioManager) {
+        this.audioManager = audioManager;
+    }
+
+    protected void unsetAudioManager(AudioManager audioManager) {
+        this.audioManager = null;
+    }
+
+}
--- extensions/io/org.eclipse.smarthome.io.javasound/src/main/java/org/eclipse/smarthome/io/javasound/internal/JavaSoundAudioSink.java
+++ extensions/io/org.eclipse.smarthome.io.javasound/src/main/java/org/eclipse/smarthome/io/javasound/internal/JavaSoundAudioSink.java
@@ -7,10 +7,18 @@
  */
 package org.eclipse.smarthome.io.javasound.internal;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Locale;
 import java.util.Set;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.Port;
+
+import org.apache.commons.collections.Closure;
 import org.eclipse.smarthome.core.audio.AudioFormat;
 import org.eclipse.smarthome.core.audio.AudioSink;
 import org.eclipse.smarthome.core.audio.AudioStream;
@@ -55,6 +63,59 @@ public class JavaSoundAudioSink implements AudioSink {
     @Override
     public String getLabel(Locale locale) {
         return "System Speaker";
+    }
+
+    @Override
+    public float getVolume() throws IOException {
+        final Float[] volumes = new Float[1];
+        runVolumeCommand(new Closure() {
+            @Override
+            public void execute(Object input) {
+                FloatControl volumeControl = (FloatControl) input;
+                volumes[0] = volumeControl.getValue();
+            }
+        });
+        if (volumes[0] != null) {
+            return volumes[0];
+        } else {
+            throw new IOException("Cannot determine master volume level");
+        }
+
+    }
+
+    @Override
+    public void setVolume(final float volume) {
+        if (volume < 0 || volume > 1) {
+            throw new IllegalArgumentException("Volume value must be in the range [0,1]!");
+        }
+        runVolumeCommand(new Closure() {
+            @Override
+            public void execute(Object input) {
+                FloatControl volumeControl = (FloatControl) input;
+                volumeControl.setValue(volume);
+            }
+        });
+    }
+
+    private void runVolumeCommand(Closure closure) {
+        Mixer.Info[] infos = AudioSystem.getMixerInfo();
+        for (Mixer.Info info : infos) {
+            Mixer mixer = AudioSystem.getMixer(info);
+            if (mixer.isLineSupported(Port.Info.SPEAKER)) {
+                Port port;
+                try {
+                    port = (Port) mixer.getLine(Port.Info.SPEAKER);
+                    port.open();
+                    if (port.isControlSupported(FloatControl.Type.VOLUME)) {
+                        FloatControl volume = (FloatControl) port.getControl(FloatControl.Type.VOLUME);
+                        closure.execute(volume);
+                    }
+                    port.close();
+                } catch (LineUnavailableException e) {
+                    logger.error("Cannot access master volume control", e);
+                }
+            }
+        }
     }
 
 }
