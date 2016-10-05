360455fa2f7ba5d226794788186018c0c27cfd87
Kai Kreuzer <kai@openhab.org>
2016-10-05 11:58:28 +0200

    expose HumanLanguageInterpreters on the REST API (#2251)
    * expose HumanLanguageInterpreters on the REST API
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * incorporated review feedback
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>
    
    * fixed potential CME issue
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/core/org.eclipse.smarthome.core.voice/src/main/java/org/eclipse/smarthome/core/voice/VoiceManager.java
+++ bundles/core/org.eclipse.smarthome.core.voice/src/main/java/org/eclipse/smarthome/core/voice/VoiceManager.java
@@ -469,6 +469,25 @@ public class VoiceManager {
     }
 
     /**
+     * Retrieves a TTS service with the given id.
+     *
+     * @param id the id of the TTS service
+     * @return a TTS service or null, if no service with this id exists
+     */
+    public TTSService getTTS(String id) {
+        return ttsServices.get(id);
+    }
+
+    /**
+     * Retrieves all TTS services.
+     *
+     * @return a collection of TTS services
+     */
+    public Collection<TTSService> getTTSs() {
+        return new HashSet<>(ttsServices.values());
+    }
+
+    /**
      * Retrieves a STT service.
      * If a default name is configured and the service available, this is returned. Otherwise, the first available
      * service is returned.
@@ -489,6 +508,25 @@ public class VoiceManager {
             logger.debug("No STT service available!");
         }
         return stt;
+    }
+
+    /**
+     * Retrieves a STT service with the given id.
+     *
+     * @param id the id of the STT service
+     * @return a STT service or null, if no service with this id exists
+     */
+    public STTService getSTT(String id) {
+        return sttServices.get(id);
+    }
+
+    /**
+     * Retrieves all STT services.
+     *
+     * @return a collection of STT services
+     */
+    public Collection<STTService> getSTTs() {
+        return new HashSet<>(sttServices.values());
     }
 
     /**
@@ -515,6 +553,25 @@ public class VoiceManager {
     }
 
     /**
+     * Retrieves a KS service with the given id.
+     *
+     * @param id the id of the KS service
+     * @return a KS service or null, if no service with this id exists
+     */
+    public KSService getKS(String id) {
+        return ksServices.get(id);
+    }
+
+    /**
+     * Retrieves all KS services.
+     *
+     * @return a collection of KS services
+     */
+    public Collection<KSService> getKSs() {
+        return new HashSet<>(ksServices.values());
+    }
+
+    /**
      * Retrieves a HumanLanguageInterpreter.
      * If a default name is configured and the service available, this is returned. Otherwise, the first available
      * service is returned.
@@ -535,6 +592,25 @@ public class VoiceManager {
             logger.debug("No HumanLanguageInterpreter available!");
         }
         return hli;
+    }
+
+    /**
+     * Retrieves a HumanLanguageInterpreter with the given id.
+     *
+     * @param id the id of the HumanLanguageInterpreter
+     * @return a HumanLanguageInterpreter or null, if no interpreter with this id exists
+     */
+    public HumanLanguageInterpreter getHLI(String id) {
+        return humanLanguageInterpreters.get(id);
+    }
+
+    /**
+     * Retrieves all HumanLanguageInterpreters.
+     *
+     * @return a collection of HumanLanguageInterpreters
+     */
+    public Collection<HumanLanguageInterpreter> getHLIs() {
+        return new HashSet<>(humanLanguageInterpreters.values());
     }
 
     /**
---
+++ bundles/io/org.eclipse.smarthome.io.rest.voice/src/main/java/org/eclipse/smarthome/io/rest/voice/internal/HLIMapper.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.rest.voice.internal;
+
+import java.util.HashSet;
+import java.util.Locale;
+
+import org.eclipse.smarthome.core.voice.text.HumanLanguageInterpreter;
+
+/**
+ * Mapper class that maps {@link HumanLanguageInterpreter} instanced to their respective DTOs.
+ *
+ * @author Kai Kreuzer - Initial contribution and API
+ *
+ */
+public class HLIMapper {
+
+    /**
+     * Maps a {@link HumanLanguageInterpreter} to an {@link HumanLanguageInterpreterDTO}.
+     *
+     * @param hli the human language interpreter
+     * @param locale the locale to use for the DTO
+     *
+     * @return the corresponding DTO
+     */
+    public static HumanLanguageInterpreterDTO map(HumanLanguageInterpreter hli, Locale locale) {
+        HumanLanguageInterpreterDTO dto = new HumanLanguageInterpreterDTO();
+        dto.id = hli.getId();
+        dto.label = hli.getLabel(locale);
+        dto.locales = new HashSet<String>(hli.getSupportedLocales().size());
+        for (Locale l : hli.getSupportedLocales()) {
+            dto.locales.add(l.toString());
+        }
+        return dto;
+    }
+
+}
