169d3e7de05cb130b069cbb31eb2ebb6950ff41f
Vlad Ivanov <vlad-mbx@ya.ru>
2016-10-10 21:18:13 +0300

    BasicUI updates (#2294)
    * BasicUI: sitemap visibility events support
    * BasicUI: fix text widget icon update
    
    Signed-off-by: Vlad Ivanov <vlad-mbx@ya.ru>

--- extensions/ui/org.eclipse.smarthome.ui.basic/src/main/java/org/eclipse/smarthome/ui/basic/internal/render/ChartRenderer.java
+++ extensions/ui/org.eclipse.smarthome.ui.basic/src/main/java/org/eclipse/smarthome/ui/basic/internal/render/ChartRenderer.java
@@ -56,10 +56,12 @@ public class ChartRenderer extends AbstractWidgetRenderer {
             }
 
             String url = "/chart?" + itemParam + "&period=" + chart.getPeriod() + "&random=1";
-            if (chart.getService() != null)
+            if (chart.getService() != null) {
                 url += "&service=" + chart.getService();
+            }
 
             String snippet = getSnippet("chart");
+            snippet = preprocessSnippet(snippet, w);
 
             if (chart.getRefresh() > 0) {
                 snippet = StringUtils.replace(snippet, "%update_interval%", Integer.toString(chart.getRefresh()));
