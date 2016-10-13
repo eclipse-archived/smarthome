b81d33e6318fa3ccbaf0784fab934bd92cac344b
Markus Rathgeb <maggu2810@gmail.com>
2016-10-13 07:49:15 +0200

    Audio servlet improvements (#2310)
    * use seconds directly
    * reorder code to allow usage of try-with-resources
    
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/core/org.eclipse.smarthome.core.audio/src/main/java/org/eclipse/smarthome/core/audio/internal/AudioServlet.java
+++ bundles/core/org.eclipse.smarthome.core.audio/src/main/java/org/eclipse/smarthome/core/audio/internal/AudioServlet.java
@@ -20,7 +20,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -99,63 +98,70 @@ public class AudioServlet extends HttpServlet implements AudioHTTPServer {
         return httpContext;
     }
 
+    private InputStream prepareInputStream(final String streamId, final HttpServletResponse resp)
+            throws AudioException {
+        final AudioStream stream;
+        final boolean multiAccess;
+        if (oneTimeStreams.containsKey(streamId)) {
+            stream = oneTimeStreams.remove(streamId);
+            multiAccess = false;
+        } else if (multiTimeStreams.containsKey(streamId)) {
+            stream = multiTimeStreams.get(streamId);
+            multiAccess = true;
+        } else {
+            return null;
+        }
+
+        logger.debug("Stream to serve is {}", streamId);
+
+        // try to set the content-type, if possible
+        final String mimeType;
+        if (stream.getFormat().getCodec() == AudioFormat.CODEC_MP3) {
+            mimeType = "audio/mpeg";
+        } else if (stream.getFormat().getContainer() == AudioFormat.CONTAINER_WAVE) {
+            mimeType = "audio/wav";
+        } else if (stream.getFormat().getContainer() == AudioFormat.CONTAINER_OGG) {
+            mimeType = "audio/ogg";
+        } else {
+            mimeType = null;
+        }
+        if (mimeType != null) {
+            resp.setContentType(mimeType);
+        }
+
+        // try to set the content-length, if possible
+        if (stream instanceof FixedLengthAudioStream) {
+            final Long size = ((FixedLengthAudioStream) stream).length();
+            if (size != null) {
+                resp.setContentLength(size.intValue());
+            }
+        }
+
+        if (multiAccess) {
+            // we need to care about concurrent access and have a separate stream for each thread
+            return ((FixedLengthAudioStream) stream).getClonedStream();
+        } else {
+            return stream;
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         removeTimedOutStreams();
 
-        String streamId = StringUtils.substringBefore(StringUtils.substringAfterLast(req.getRequestURI(), "/"), ".");
+        final String streamId = StringUtils.substringBefore(StringUtils.substringAfterLast(req.getRequestURI(), "/"),
+                ".");
 
-        boolean multiAccess = false;
-        AudioStream stream = oneTimeStreams.remove(streamId);
-        if (stream == null) {
-            stream = multiTimeStreams.get(streamId);
-            if (stream != null) {
-                multiAccess = true;
-            }
-        }
-        if (stream == null) {
-            logger.debug("Received request for invalid stream id at {}", req.getRequestURI());
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-        } else {
-            logger.debug("Stream to serve is {}", streamId);
-
-            // try to set the content-type, if possible
-            String mimeType = null;
-            if (stream.getFormat().getCodec() == AudioFormat.CODEC_MP3) {
-                mimeType = "audio/mpeg";
-            } else if (stream.getFormat().getContainer() == AudioFormat.CONTAINER_WAVE) {
-                mimeType = "audio/wav";
-            } else if (stream.getFormat().getContainer() == AudioFormat.CONTAINER_OGG) {
-                mimeType = "audio/ogg";
-            }
-            if (mimeType != null) {
-                resp.setContentType(mimeType);
-            }
-
-            // try to set the content-length, if possible
-            if (stream instanceof FixedLengthAudioStream) {
-                Long size = ((FixedLengthAudioStream) stream).length();
-                if (size != null) {
-                    resp.setContentLength(size.intValue());
-                }
-            }
-
-            ServletOutputStream os = resp.getOutputStream();
-
-            InputStream is = null;
-            if (multiAccess) {
-                // we need to care about concurrent access and have a separate stream for each thread
-                try {
-                    is = ((FixedLengthAudioStream) stream).getClonedStream();
-                } catch (AudioException e) {
-                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
-                }
+        try (final InputStream stream = prepareInputStream(streamId, resp)) {
+            if (stream == null) {
+                logger.debug("Received request for invalid stream id at {}", req.getRequestURI());
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             } else {
-                is = stream;
+                IOUtils.copy(stream, resp.getOutputStream());
+                resp.flushBuffer();
             }
-            IOUtils.copy(is, os);
-            resp.flushBuffer();
-            IOUtils.closeQuietly(is);
+        } catch (final AudioException ex) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, ex.getMessage());
         }
     }
 
@@ -183,7 +189,7 @@ public class AudioServlet extends HttpServlet implements AudioHTTPServer {
     public URL serve(FixedLengthAudioStream stream, int seconds) {
         String streamId = UUID.randomUUID().toString();
         multiTimeStreams.put(streamId, stream);
-        streamTimeouts.put(streamId, System.nanoTime() + (seconds * TimeUnit.SECONDS.toNanos(1)));
+        streamTimeouts.put(streamId, System.nanoTime() + TimeUnit.SECONDS.toNanos(seconds));
         return getURL(streamId);
     }
 
