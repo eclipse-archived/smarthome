e7f7e6c31f1c69a2bb3b39b558a9d2d739402063
Kai Kreuzer <kai@openhab.org>
2016-10-30 19:25:00 +0100

    Changed AudioHTTPServer to return relative urls instead of absolute
    This moves the responsibility of constructing the absolute url to call to the consumer - which is imho the much better approach, as e.g. a consumer like a web browser might request it remotely (from a different network) and thus only it knows which server+port to contact (and with this to avoid any cross origin problems).
    
    - adapted Sonos binding to make relative url absolute
    - added configuration parameter for callback url to Sonos binding (if not set, heuristic approach through scanning available network interfaces is taken)
    - tested it also successfully with webaudio sink in Paper UI
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/core/org.eclipse.smarthome.core.audio/src/main/java/org/eclipse/smarthome/core/audio/AudioHTTPServer.java
+++ bundles/core/org.eclipse.smarthome.core.audio/src/main/java/org/eclipse/smarthome/core/audio/AudioHTTPServer.java
@@ -7,8 +7,6 @@
  */
 package org.eclipse.smarthome.core.audio;
 
-import java.net.URL;
-
 import org.eclipse.smarthome.core.audio.internal.AudioServlet;
 
 /**
@@ -22,28 +20,28 @@ import org.eclipse.smarthome.core.audio.internal.AudioServlet;
 public interface AudioHTTPServer {
 
     /**
-     * Creates a url for a given {@link AudioStream} where it can be requested a single time.
+     * Creates a relative url for a given {@link AudioStream} where it can be requested a single time.
      * Note that the HTTP header only contains "Content-length", if the passed stream is an instance of
      * {@link FixedLengthAudioStream}.
      * If the client that requests the url expects this header field to be present, make sure to pass such an instance.
      * Streams are closed after having been served.
      *
      * @param stream the stream to serve on HTTP
-     * @return the absolute URL to access the stream (using the primary network interface)
+     * @return the relative URL to access the stream starting with a '/'
      */
-    URL serve(AudioStream stream);
+    String serve(AudioStream stream);
 
     /**
-     * Creates a url for a given {@link AudioStream} where it can be requested multiple times within the given time
-     * frame.
+     * Creates a relative url for a given {@link AudioStream} where it can be requested multiple times within the given
+     * time frame.
      * This method only accepts {@link FixedLengthAudioStream}s, since it needs to be able to create multiple concurrent
      * streams from it, which isn't possible with a regular {@link AudioStream}.
      * Streams are closed, once they expire.
      *
      * @param stream the stream to serve on HTTP
      * @param seconds number of seconds for which the stream is available through HTTP
-     * @return the absolute URL to access the stream (using the primary network interface)
+     * @return the relative URL to access the stream starting with a '/'
      */
-    URL serve(FixedLengthAudioStream stream, int seconds);
+    String serve(FixedLengthAudioStream stream, int seconds);
 
 }
