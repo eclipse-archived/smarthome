1f398022ff384da52389ecf3946dd92694c131db
aounhaider1 <aounhaider1@gmail.com>
2016-10-31 18:01:16 +0100

    SetupPageController tests (#2358)
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/tests/setup.test.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/tests/setup.test.js
@@ -1,10 +1,8 @@
 describe('module PaperUI.controllers.setup', function() {
-    var restConfig;
+    beforeEach(function() {
+        module('PaperUI');
+    });
     describe('tests for SetupPageController', function() {
-        var setupPageController, thingTypeService,scope;
-        beforeEach(function() {
-            module('PaperUI');
-        });
         beforeEach(inject(function($injector, $rootScope, $controller) {
             scope = $rootScope.$new();
             $httpBackend = $injector.get('$httpBackend');
@@ -17,10 +15,10 @@ describe('module PaperUI.controllers.setup', function() {
         it('should require SetupPageController', function() {
             expect(setupPageController).toBeDefined();
         });
-        it('thingTypes should be defined', function() {
+        it('should define thingTypes', function() {
             expect(scope.thingTypes).toBeDefined();
         });
-        it('thingTypeService should GET data', function() {
+        it('should get data from thingTypeService', function() {
             expect(thingTypeService).toBeDefined();
             var success = false;
             var thingTypes = [ {
@@ -34,56 +32,206 @@ describe('module PaperUI.controllers.setup', function() {
             expect(success).toBeTruthy();
         });
     });
-    
+
     describe('tests for InboxController', function() {
-        var inboxController,scope;
-        beforeEach(function() {
-            module('PaperUI');
-        });
         beforeEach(inject(function($injector, $rootScope, $controller) {
-           scope=$rootScope.$new();
-           $controller('BodyController', {
-               '$scope' : scope
-           });
-           inboxController= $controller('InboxController', {
-               '$scope' : scope
-           });
+            scope = $rootScope.$new();
+            $controller('BodyController', {
+                '$scope' : scope
+            });
+            inboxController = $controller('InboxController', {
+                '$scope' : scope
+            });
+            discoveryResultRepository = $injector.get('discoveryResultRepository');
+            spyOn(discoveryResultRepository, "getAll");
         }));
-        it('should require InboxController', function() {       
+        it('should require InboxController', function() {
             expect(inboxController).toBeDefined();
         });
-    });
-    
-    describe('tests for InboxEntryController', function() {
-        var inboxEntryController,scope,inboxService;
-        beforeEach(function() {
-            module('PaperUI');
+        it('should refresh discovery results', function() {
+            scope.refresh();
+            expect(discoveryResultRepository.getAll).toHaveBeenCalled();
         });
-        beforeEach(inject(function($injector, $rootScope, $controller,$mdDialog) {
-           scope=$rootScope.$new();
-           inboxService=$injector.get('inboxService');
-           inboxEntryController= $controller('InboxEntryController', {
-               '$scope' : scope
-           });
-           restConfig = $injector.get('restConfig');
+    });
+
+    describe('tests for InboxEntryController', function() {
+
+        beforeEach(inject(function($injector, $rootScope, $controller) {
+            scope = $rootScope.$new();
+            mdDialog = $injector.get('$mdDialog');
+            inboxService = $injector.get('inboxService');
+            inboxEntryController = $controller('InboxEntryController', {
+                '$scope' : scope
+            });
+            $rootScope.data.discoveryResults = [ {
+                label : "LABEL",
+                thingUID : "A:B"
+            } ];
+            restConfig = $injector.get('restConfig');
+            $rootScope.$apply();
         }));
-        it('should require InboxEntryController', function() {       
+
+        beforeEach(inject(function($q, $rootScope) {
+            rootScope = $rootScope;
+            deferred = $q.defer();
+            spyOn(mdDialog, 'show').and.returnValue(deferred.promise);
+            $rootScope.$apply();
+            spyOn(mdDialog.show(), 'then').and.returnValue(deferred.promise);
+        }));
+
+        it('should require InboxEntryController', function() {
             expect(inboxEntryController).toBeDefined();
         });
-        it('should call function approve', function() {       
-            spyOn(scope, "approve");
-            scope.approve(0,0);
+        it('should call function approve', function() {
+            rootScope.$apply();
+            spyOn(scope, "approve").and.callThrough();
+            scope.approve(0, 0);
             expect(scope.approve).toHaveBeenCalled();
+            expect(mdDialog.show().then).toHaveBeenCalled();
         });
-        it('should call function ignore', function() {       
+        it('should call function ignore', function() {
             spyOn(inboxService, "ignore");
             scope.ignore(1);
             expect(inboxService.ignore).toHaveBeenCalled();
         });
-        it('should call function unignore', function() {       
+        it('should call function unignore', function() {
             spyOn(inboxService, "unignore");
             scope.unignore(1);
             expect(inboxService.unignore).toHaveBeenCalled();
+        });
+        it('should call function remove', function() {
+            rootScope.$apply();
+            scope.remove("A:B");
+            expect(mdDialog.show).toHaveBeenCalled();
+            expect(mdDialog.show().then).toHaveBeenCalled();
+        });
+    });
+
+    describe('tests for ScanDialogController', function() {
+        beforeEach(inject(function($injector, $rootScope, $controller) {
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            $httpBackend = $injector.get('$httpBackend');
+            scanDialogController = $controller('ScanDialogController', {
+                '$scope' : scope
+            });
+            discoveryService = $injector.get('discoveryService');
+            rootScope.data.bindings = [ {
+                id : 1,
+                name : 'binding'
+            } ];
+            spyOn(discoveryService, "scan");
+
+        }));
+        it('should require ScanDialogController', function() {
+            expect(scanDialogController).toBeDefined();
+            $httpBackend.when('GET', restConfig.restPath + "/bindings").respond();
+            $httpBackend.when('GET', restConfig.restPath + "/discovery").respond([ 1, 2, 3 ]);
+            $httpBackend.flush();
+            expect(scope.supportedBindings.length).toEqual(3);
+        });
+        it('should add bindingId to scans', function() {
+            scope.scan(1);
+            expect(scope.activeScans).toEqual([ 1 ]);
+        });
+        it('should call discoveryService.scan', function() {
+            scope.scan(1);
+            expect(discoveryService.scan).toHaveBeenCalled();
+        });
+        it('should get binding by Id', function() {
+            var binding = scope.getBindingById(1);
+            expect(binding.name).toEqual('binding');
+        });
+        it('should return empty object', function() {
+            var binding = scope.getBindingById(2);
+            expect(binding).toEqual({});
+        });
+    });
+
+    describe('tests for ApproveInboxEntryDialogController', function() {
+        beforeEach(inject(function($injector, $rootScope, $controller) {
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            $httpBackend = $injector.get('$httpBackend');
+            mdDialog = $injector.get('$mdDialog');
+            thingTypeRepository = $injector.get('thingTypeRepository');
+            spyOn(thingTypeRepository, "getOne").and.callThrough();
+            rootScope.data.thingTypes = [ {
+                UID : "A:B",
+                label : 'LABEL'
+            } ];
+            approveInboxEntryDialogController = $controller('ApproveInboxEntryDialogController', {
+                '$scope' : scope,
+                'discoveryResult' : {
+                    label : 'LABEL',
+                    thingTypeUID : "A:B"
+                }
+            });
+            discoveryService = $injector.get('discoveryService');
+        }));
+        it('should require ApproveInboxEntryDialogController', function() {
+            expect(approveInboxEntryDialogController).toBeDefined();
+        });
+        it('should require parameters to be passed', function() {
+            expect(scope.discoveryResult).toBeDefined();
+            expect(scope.label).toEqual('LABEL');
+            expect(scope.thingTypeUID).toEqual('A:B');
+        });
+        it('should get thingType', function() {
+            expect(thingTypeRepository.getOne).toHaveBeenCalled();
+            expect(scope.thingType.label).toEqual("LABEL");
+        });
+        it('should call approve', function() {
+            spyOn(mdDialog, "hide").and.callThrough();
+            scope.approve();
+            expect(mdDialog.hide).toHaveBeenCalled();
+        });
+    });
+
+    describe('tests for ManualSetupConfigureController', function() {
+        var scope;
+        beforeEach(inject(function($injector, $rootScope, $controller) {
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            $httpBackend = $injector.get('$httpBackend');
+            thingService = $injector.get('thingService');
+            thingTypeRepository = $injector.get('thingTypeRepository');
+            thingRepository = $injector.get('thingRepository');          
+            var thingTypes = {
+                UID : "A:B",
+                label : 'LABEL',
+                supportedBridgeTypeUIDs : [ "A:B", 2, 3 ]
+            };
+            $httpBackend.when('GET', /rest\/thing-types.*/).respond(thingTypes);
+            $controller('BodyController', {
+                '$scope' : scope
+            });
+            manualSetupConfigureController = $controller('ManualSetupConfigureController', {
+                $scope : scope,
+                $routeParams : {
+                    thingTypeUID : "A:B"
+                }
+            });
+        }));
+        it('should require ManualSetupConfigureController', function() {
+            expect(manualSetupConfigureController).toBeDefined();
+        });
+        it('should require bridges', function() {
+            var things = [ {
+                thingTypeUID : "A:B",
+                label : "THING"
+            } ];
+            $httpBackend.when('GET', restConfig.restPath + "/things").respond(things);
+            $httpBackend.whenGET(/^((?!rest\/things).)*$/).respond(200, '');           
+            $httpBackend.flush();
+            expect(scope.needsBridge).toBeTruthy();
+            expect(scope.bridges.length).toBe(1);
+        });
+        it('should add thing', function() {
+            spyOn(thingService, "add");
+            scope.parameters=[];
+            scope.addThing({id:1});
+            expect(thingService.add).toHaveBeenCalled();
         });
     });
 
