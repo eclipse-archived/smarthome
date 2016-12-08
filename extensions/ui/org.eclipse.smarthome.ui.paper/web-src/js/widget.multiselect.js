766ee44c92134dabcd6ed395cd4d0bf7b7bc015d
aounhaider1 <aounhaider1@gmail.com>
2016-12-08 16:15:17 +0100

    Show selected values in multi dropdown (#2625)
    * multiple dropdown changes
    
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/widget.multiselect.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/widget.multiselect.js
@@ -4,7 +4,7 @@ angular.module('PaperUI').directive('multiSelect', function() {
         link : function(scope, element, attrs) {
             scope.filterText = "";
             scope.parameter.optionList = [];
-
+            var originalList = [];
             if (scope.configuration[scope.parameter.name])
                 for (var i = 0; i < scope.configuration[scope.parameter.name].length; i++) {
                     var inParam = $.grep(scope.parameter.options, function(option) {
@@ -28,11 +28,11 @@ angular.module('PaperUI').directive('multiSelect', function() {
             });
             scope.openDropdown = function($event) {
                 $event.stopImmediatePropagation();
-                var visible=element.find("dd ul").is( ":visible" );
+                var visible = element.find("dd ul").is(":visible");
                 angular.element(document).find("dd ul").hide();
-                if(!visible)
-                element.find("dd ul").slideDown('fast');
-                else{
+                if (!visible)
+                    element.find("dd ul").slideDown('fast');
+                else {
                     element.find("dd ul").slideUp('fast');
                 }
             }
@@ -56,7 +56,7 @@ angular.module('PaperUI').directive('multiSelect', function() {
             }
 
             scope.onEnterPress = function($event) {
-                if (((scope.parameter.options.length==0) || (scope.parameter.options.length>0 && !scope.parameter.limitToOptions)) && $event.keyCode == 13) {
+                if (((scope.parameter.options.length == 0) || (scope.parameter.options.length > 0 && !scope.parameter.limitToOptions)) && $event.keyCode == 13) {
                     scope.addItemToList($event);
                 }
             }
@@ -85,14 +85,37 @@ angular.module('PaperUI').directive('multiSelect', function() {
             }
 
             scope.$watch('parameter.options', function() {
-                    if (!('$promise' in scope.parameter.options)) {
+                if (!('$promise' in scope.parameter.options)) {
+                    addOptionToParam();
+                } else {
+                    scope.parameter.options.$promise.then(function() {
                         addOptionToParam();
-                    } else {
-                        scope.parameter.options.$promise.then(function() {
-                            addOptionToParam();
-                        });
-                    }
+                    });
+                }
             });
+
+            scope.$watch('filterText', function() {
+                if (scope.parameter.optionList && scope.parameter.optionList.length > 0) {
+                    originalList = originalList.length == 0 ? scope.parameter.optionList : originalList;
+                    var filteredOptions = $.grep(originalList, function(option) {
+                        var optionValue = (option.label + "").toLowerCase();
+                        return optionValue.indexOf(("" + scope.filterText).toLowerCase()) != -1;
+                    });
+                    scope.parameter.optionList = filteredOptions && filteredOptions.length > 0 ? filteredOptions : originalList;
+                }
+            });
+
+            scope.getPlaceHolderText = function(configuration, parameter) {
+                if (configuration[parameter.name] && configuration[parameter.name].length > 0) {
+                    if (parameter.context == "thing" || parameter.context == "item") {
+                        return configuration[parameter.name].length == 1 ? '1 option selected' : configuration[parameter.name].length + ' options selected';
+                    } else {
+                        return configuration[parameter.name].toString();
+                    }
+                }
+                return parameter.options.length == 0 || (parameter.options.length > 0 && !parameter.limitToOptions) ? 'Add or search' : 'Search';
+            }
+
             function addOptionToParam() {
                 for (var i = 0; i < scope.parameter.options.length; i++) {
                     var value = scope.parameter.context == 'item' ? scope.parameter.options[i].name : scope.parameter.context == 'thing' ? scope.parameter.options[i].UID : scope.parameter.options[i].value;
