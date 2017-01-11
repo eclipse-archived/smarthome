25af7215e8c0dae39202450ec3abeec17d624612
aounhaider1 <aounhaider1@gmail.com>
2017-01-11 13:58:21 +0100

    Support Rule context (#2791)
    * Support rule and channel context
    
    Signed-off-by: Aoun Bukhari <bukhari@itemis.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/services.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/services.js
@@ -88,7 +88,7 @@ angular.module('PaperUI.services', [ 'PaperUI.constants' ]).config(function($htt
             self.showToast('success', text, actionText, actionUrl);
         }
     };
-}).factory('configService', function(itemService, thingService, $filter, itemRepository) {
+}).factory('configService', function(itemService, thingRepository, ruleRepository, $filter, itemRepository) {
     return {
         getRenderingModel : function(configParameters, configGroups) {
             var parameters = [];
@@ -137,6 +137,33 @@ angular.module('PaperUI.services', [ 'PaperUI.constants' ]).config(function($htt
                         } else {
                             parameter.element = 'select';
                         }
+                    } else if (parameter.context.toUpperCase() === 'CHANNEL') {
+                        if (parameter.multiple) {
+                            parameter.element = 'multiSelect';
+                            parameter.limitToOptions = true;
+                        } else {
+                            parameter.element = 'select';
+                        }
+                        parameter.context = 'channel';
+                    } else if (parameter.context.toUpperCase() === "RULE") {
+                        if (parameter.multiple) {
+                            parameter.element = 'multiSelect';
+                            parameter.limitToOptions = true;
+                        } else {
+                            parameter.element = 'select';
+                        }
+                        function encloseParameter(parameter) {
+                            var param = parameter;
+                            ruleRepository.getAll(function(rules) {
+                                for (var j_r = 0; j_r < rules.length; j_r++) {
+                                    rules[j_r].value = rules[j_r].uid;
+                                    rules[j_r].label = rules[j_r].name;
+                                }
+                                param.options = rules;
+                            });
+                        }
+                        encloseParameter(parameter);
+                        parameter.context = 'rule';
                     } else if (parameter.context.toUpperCase() === 'DATE') {
                         if (parameter.type.toUpperCase() === 'TEXT') {
                             parameter.element = 'date';
@@ -151,8 +178,6 @@ angular.module('PaperUI.services', [ 'PaperUI.constants' ]).config(function($htt
                         } else {
                             parameter.element = 'select';
                         }
-                        thingList = thingList === undefined ? thingService.getAll() : thingList;
-                        parameter.options = thingList;
                     } else if (parameter.context.toUpperCase() === 'TIME') {
                         parameter.element = 'input';
                         if (parameter.type.toUpperCase() === 'TEXT') {
@@ -230,11 +255,52 @@ angular.module('PaperUI.services', [ 'PaperUI.constants' ]).config(function($htt
                 }
 
             }
-            return this.getItemConfigs(parameters);
+            parameters = this.getItemConfigs(parameters)
+            return this.getChannelsConfig(parameters);
+        },
+        getChannelsConfig : function(configParams) {
+            var self = this, hasOneItem;
+            var configParameters = configParams;
+            for (var i = 0; !hasOneItem && i < configParameters.length; i++) {
+                var parameterItems = $.grep(configParameters[i].parameters, function(value) {
+                    return value.context && (value.context.toUpperCase() == "THING" || value.context.toUpperCase() == "CHANNEL");
+                });
+                if (parameterItems.length > 0) {
+                    hasOneItem = true;
+                }
+                if (hasOneItem) {
+                    thingRepository.getAll(function(things) {
+                        for (var g_i = 0; g_i < configParameters.length; g_i++) {
+                            for (var i = 0; i < configParameters[g_i].parameters.length; i++) {
+                                if (configParameters[g_i].parameters[i].context) {
+                                    if (configParameters[g_i].parameters[i].context.toUpperCase() === "THING") {
+                                        configParameters[g_i].parameters[i].options = self.filterByAttributes(things, configParameters[g_i].parameters[i].filterCriteria);
+                                    } else if (configParameters[g_i].parameters[i].context.toUpperCase() === "CHANNEL") {
+                                        configParameters[g_i].parameters[i].options = getChannelsFromThings(things, configParameters[g_i].parameters[i].filterCriteria);
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+                function getChannelsFromThings(arr, filter) {
+                    var channels = [];
+                    for (var i = 0; i < arr.length; i++) {
+                        var filteredChannels = self.filterByAttributes(arr[i].channels, filter);
+                        for (var j = 0; j < filteredChannels.length; j++) {
+                            filteredChannels[j].label = arr[i].label;
+                            filteredChannels[j].value = filteredChannels[j].uid;
+                        }
+                        channels = channels.concat(filteredChannels);
+                    }
+                    return channels;
+                }
+            }
+            return configParameters;
         },
         getItemConfigs : function(configParams) {
             var self = this, hasOneItem = false;
-            configParameters = configParams;
+            var configParameters = configParams;
             for (var i = 0; !hasOneItem && i < configParameters.length; i++) {
                 var parameterItems = $.grep(configParameters[i].parameters, function(value) {
                     return value.context && value.context.toUpperCase() == "ITEM";
