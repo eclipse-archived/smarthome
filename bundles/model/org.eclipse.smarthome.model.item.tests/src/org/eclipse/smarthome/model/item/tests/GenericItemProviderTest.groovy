b4507a4ad76a7153709ef29248d984ce44e54bb3
Simon Kaufmann <simon.kfm@gmail.com>
2017-01-23 13:19:58 +0100

    validate models before loading them (#2872)
    with this change,
    
    * there are two "layers" of validation
      1. errors when loading the resource. Usually these are syntax
         violations which irritate the parser.
         They will be logged as a WARNING and the DSL file will be
         ignored completely, i.e. all its content will not be loaded
         or removed if it was an update
      2. all kinds of other errors (i.e. violations of validation checks)
         will only be logged, but the DSL file will be loaded and
         processed nevertheless.
    
    * validation will be done on a separate resource, in order to
      keep the original one intact in case its content needs to
      be removed because of syntactical errors.
    
    * empty [] for channel properties are allowed now in the Thing DSL
    
    fixes #2870
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/model/org.eclipse.smarthome.model.item.tests/src/org/eclipse/smarthome/model/item/tests/GenericItemProviderTest.groovy
+++ bundles/model/org.eclipse.smarthome.model.item.tests/src/org/eclipse/smarthome/model/item/tests/GenericItemProviderTest.groovy
@@ -21,12 +21,12 @@ import org.junit.Test
 
 class GenericItemProviderTest extends OSGiTest {
 
-    
+
     private final static String TESTMODEL_NAME = "testModel.items"
-    
+
     ModelRepository modelRepository
     ItemRegistry itemRegistry
-    
+
     @Before
     void setUp() {
         itemRegistry = getService ItemRegistry
@@ -35,19 +35,19 @@ class GenericItemProviderTest extends OSGiTest {
         assertThat modelRepository, is(notNullValue())
         modelRepository.removeModel(TESTMODEL_NAME)
     }
-    
+
     @After
     void tearDown() {
         modelRepository.removeModel(TESTMODEL_NAME)
     }
-    
+
     @Test
     void 'assert that items from test model were added to item registry'() {
         def items = itemRegistry.getAll()
         assertThat items.size(), is(0)
 
         String model =
-            '''
+                '''
             Group Weather [TAG1]
             Group Weather_Chart (Weather)
             Number Weather_Temperature      "Outside Temperature [%.1f °C]" <temperature> (Weather_Chart) [TAG1, TAG2] { channel="yahooweather:weather:berlin:temperature" }
@@ -68,16 +68,63 @@ class GenericItemProviderTest extends OSGiTest {
         assertThat items.size(), is(0)
 
         String model =
-            '''
+                '''
             DateTime Weather_LastUpdate     "Last Update [%1$ta %1$tR]" <clock> [TAG1, TAG2, TAG3, TAG4-WITH-DASHES, "TAG5 String Tag"]
             '''
         modelRepository.addOrRefreshModel(TESTMODEL_NAME, new ByteArrayInputStream(model.bytes))
         def actualItems = itemRegistry.getAll()
 
         assertThat actualItems.size(), is(1)
-        
+
         def lastItem = actualItems.last()
         assertThat lastItem.getTags().sort().join(", "), is(equalTo("TAG1, TAG2, TAG3, TAG4-WITH-DASHES, TAG5 String Tag"))
     }
-        
+
+    @Test
+    void 'assert that a broken model is ignored'() {
+        assertThat itemRegistry.getAll().size(), is(0)
+
+        String model =
+                '''
+            String test "Test Item [%s]" { channel="test:test:test:test" }
+            String {something is wrong} test "Test Item [%s]" { channel="test:test:test:test" }
+            '''
+        modelRepository.addOrRefreshModel(TESTMODEL_NAME, new ByteArrayInputStream(model.bytes))
+
+        assertThat itemRegistry.getAll().size(), is(0)
+    }
+
+    @Test
+    void 'assert that items are removed correctly if the model gets broken'() {
+        assertThat itemRegistry.getAll().size(), is(0)
+
+        String model =
+                '''
+            String test1 "Test Item [%s]" { channel="test:test:test:test" }
+            String test2 "Test Item [%s]" { channel="test:test:test:test" }
+            '''
+        modelRepository.addOrRefreshModel(TESTMODEL_NAME, new ByteArrayInputStream(model.bytes))
+
+        assertThat itemRegistry.getAll().size(), is(2)
+
+        model =
+                '''
+            String test1 "Test Item [%s]" { channel="test:test:test:test" }
+            String {something is wrong} test "Test Item [%s]" { channel="test:test:test:test" }
+            '''
+        modelRepository.addOrRefreshModel(TESTMODEL_NAME, new ByteArrayInputStream(model.bytes))
+
+        waitForAssert {
+            assertThat itemRegistry.getAll().size(), is(0)
+        }
+
+        model =
+                '''
+            String test1 "Test Item [%s]" { channel="test:test:test:test" }
+            String test2 "Test Item [%s]" { channel="test:test:test:test" }
+            '''
+        modelRepository.addOrRefreshModel(TESTMODEL_NAME, new ByteArrayInputStream(model.bytes))
+
+        assertThat itemRegistry.getAll().size(), is(2)
+    }
 }
