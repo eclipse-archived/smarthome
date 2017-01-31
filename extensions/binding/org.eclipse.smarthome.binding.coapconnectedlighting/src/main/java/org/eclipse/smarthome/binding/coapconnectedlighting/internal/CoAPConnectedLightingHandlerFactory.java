accb2e955b65dc618929b5374c8c3709e4aa7174
Gaurang Gunde <gaurang.gunde@microchip.com>
2017-01-30 20:42:24 -0700

    Signed-off-by: Gaurang Gunde <gaurang.gunde@microchip.com> Adding the coap connected lighting microchip technology binding along with the license file.

---
+++ extensions/binding/org.eclipse.smarthome.binding.coapconnectedlighting/src/main/java/org/eclipse/smarthome/binding/coapconnectedlighting/internal/CoAPConnectedLightingHandlerFactory.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2016 Microchip Technology Inc. and its subsidiaries and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.eclipse.smarthome.binding.coapconnectedlighting.internal;
+
+import static org.eclipse.smarthome.binding.coapconnectedlighting.CoAPConnectedLightingBindingConstants.THING_TYPE_SAMPLE;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.smarthome.binding.coapconnectedlighting.handler.CoAPConnectedLightingHandler;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+
+/**
+ * The {@link CoAPConnectedLightingHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author Microchip Technology - Initial contribution
+ */
+public class CoAPConnectedLightingHandlerFactory extends BaseThingHandlerFactory {
+
+    private final static Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected ThingHandler createHandler(Thing thing) {
+
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (thingTypeUID.equals(THING_TYPE_SAMPLE)) {
+            return new CoAPConnectedLightingHandler(thing);
+        }
+
+        return null;
+    }
+}
