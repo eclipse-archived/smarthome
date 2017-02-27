b285180194d5327355c5ec5d855a57a280c8cba8
Markus Rathgeb <maggu2810@gmail.com>
2017-02-27 14:50:48 +0100

    fix unstable test (it depends on auto update = false)
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/core/org.eclipse.smarthome.core.test/src/test/groovy/org/eclipse/smarthome/core/items/events/AbstractItemEventSubscriberOSGiTest.groovy
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/groovy/org/eclipse/smarthome/core/items/events/AbstractItemEventSubscriberOSGiTest.groovy
@@ -15,16 +15,13 @@ import org.eclipse.smarthome.core.events.EventFactory
 import org.eclipse.smarthome.core.events.EventPublisher
 import org.eclipse.smarthome.core.events.EventSubscriber
 import org.eclipse.smarthome.core.items.ItemProvider
-import org.eclipse.smarthome.core.items.events.ItemEventFactory.ItemEventPayloadBean
 import org.eclipse.smarthome.core.library.items.SwitchItem
 import org.eclipse.smarthome.core.library.types.OnOffType
-import org.eclipse.smarthome.core.types.Command
 import org.eclipse.smarthome.test.OSGiTest
 import org.junit.Before
 import org.junit.Test
 
 import com.google.common.collect.Sets
-import com.google.gson.Gson
 
 /**
  * The {@link AbstractItemEventSubscriberOSGiTest} runs inside an OSGi container and tests the {@link AbstractItemEventSubscriber}.
@@ -44,20 +41,23 @@ class AbstractItemEventSubscriberOSGiTest extends OSGiTest {
         commandEvent = null
         updateEvent = null
         eventPublisher = getService(EventPublisher)
-        
+
         itemProvider = [
             getAll: {[new SwitchItem(ITEM_NAME)]},
             addProviderChangeListener: {},
             removeProviderChangeListener: {},
-            allItemsChanged: {}
+            allItemsChanged: {
+            }
         ] as ItemProvider
         registerService(itemProvider)
-        
+
         def itemEventSubscriber = [
             receiveCommand: { event -> commandEvent = event },
             receiveUpdate: { event -> updateEvent = event },
         ] as AbstractItemEventSubscriber
         registerService(itemEventSubscriber, EventSubscriber.class.getName())
+
+        disableItemAutoUpdate()
     }
 
     @Test
@@ -65,14 +65,14 @@ class AbstractItemEventSubscriberOSGiTest extends OSGiTest {
         eventPublisher.post(ItemEventFactory.createCommandEvent(ITEM_NAME, OnOffType.ON))
         waitForAssert { assertThat commandEvent, not(null)}
         waitForAssert { assertThat updateEvent, is(null)}
-        
+
         commandEvent = null
         updateEvent = null
         eventPublisher.post(ItemEventFactory.createStateEvent(ITEM_NAME, OnOffType.ON))
         waitForAssert { assertThat commandEvent, is(null)}
         waitForAssert { assertThat updateEvent, not(null)}
     }
-    
+
     @Test
     public void 'AbstractItemEventSubscriber do not receive events if event type is not supported'() {
         def someEventType = "SOME_EVENT_TYPE"
@@ -85,10 +85,9 @@ class AbstractItemEventSubscriberOSGiTest extends OSGiTest {
         registerService(someEventFactory)
 
         Event event = [ getType: { someEventType }, getPayload: { "{a: 'A', b: 'B'}" }, getTopic: { "smarthome/items" }, getSource: { null } ] as Event
-        
+
         eventPublisher.post(event)
         waitForAssert { assertThat commandEvent, is(null)}
         waitForAssert { assertThat updateEvent, is(null)}
     }
-    
 }
