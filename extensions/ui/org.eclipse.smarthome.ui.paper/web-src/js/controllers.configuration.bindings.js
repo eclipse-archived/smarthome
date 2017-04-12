ed6d158264eed6ae8ced18a35a0cbd6d582e306d
Chris Jackson <chris@cd-jackson.com>
2017-04-12 12:54:31 +0100

    Updated UI for bindings
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/controllers.configuration.bindings.js
@@ -0,0 +1,82 @@
+angular.module('PaperUI.controllers.configuration.bindings', [ 'ngRoute' ]).config([ '$routeProvider', function($routeProvider) {
+    // Configure the routes for this controller
+    $routeProvider.when('/configuration/bindings', {
+        templateUrl : 'partials/configuration.bindings.html',
+        controller : 'BindingController',
+        title : 'Configuration'
+    }).when('/configuration/bindings/:bindingId', {
+        templateUrl : 'partials/configuration.bindings.detail.html',
+        controller : 'BindingDetailController',
+        title : 'Configuration'
+    });
+} ]).controller('BindingController', function($scope, $location, bindingRepository) {
+    /**
+     * This is the main binding controller to display all bindings
+     */
+    $scope.navigateTo = function(path) {
+        $location.path(path);
+    }
+
+    $scope.setSubtitle([ 'Bindings' ]);
+    $scope.setHeaderText('Shows all installed bindings.');
+    $scope.refresh = function() {
+        bindingRepository.getAll(true);
+    };
+
+    $scope.configure = function(bindingId, configDescriptionURI, event) {
+        $mdDialog.show({
+            controller : 'ConfigureBindingDialogController',
+            templateUrl : 'partials/dialog.configurebinding.html',
+            targetEvent : event,
+            hasBackdrop : true,
+            locals : {
+                bindingId : bindingId,
+                configDescriptionURI : configDescriptionURI
+            }
+        });
+    }
+    bindingRepository.getAll();
+}).controller('BindingDetailController', function($scope, $location, thingTypeRepository, bindingRepository) {
+    /**
+     * This is the binding controller to display the binding detail
+     */
+    $scope.navigateTo = function(path) {
+        $location.path(path);
+    }
+
+    // Function to allow filtering of thing-types linked to the search box
+    $scope.filterItems = function(lookupFields, searchText) {
+        return function(item) {
+            if (searchText && searchText.length > 0) {
+                for (var i = 0; i < lookupFields.length; i++) {
+                    if (item[lookupFields[i]] && item[lookupFields[i]].toUpperCase().indexOf(searchText.toUpperCase()) != -1) {
+                        return true;
+                    }
+                }
+                return false
+            }
+            return true;
+        }
+    }
+
+    var bindingId = $scope.path[3];
+
+    $scope.searchText = "";
+    $scope.binding = undefined;
+    bindingRepository.getOne(function(binding) {
+        return binding.id === bindingId;
+    }, function(binding) {
+        $scope.setSubtitle([ 'Bindings', binding.name ]);
+        $scope.setHeaderText('Shows detailed binding information.');
+
+        $scope.binding = binding;
+        $scope.binding.thingTypes = [];
+        thingTypeRepository.getAll(function(thingTypes) {
+            $.each(thingTypes, function(index, thingType) {
+                if (thingType.UID.split(':')[0] === binding.id) {
+                    $scope.binding.thingTypes.push(thingType);
+                }
+            });
+        });
+    });
+});
