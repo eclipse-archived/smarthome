ea4c9ee639d27fea4e8d36c759a76438d5a609cc
Chris Jackson <chris@cd-jackson.com>
2017-04-21 22:36:06 +0100

    Update return to 500 following config update error
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/thing/ThingResource.java
+++ bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/thing/ThingResource.java
@@ -451,11 +451,12 @@ public class ThingResource implements SatisfiableRESTResource {
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
             @ApiResponse(code = 400, message = "Configuration of the thing is not valid."),
             @ApiResponse(code = 404, message = "Thing not found"),
-            @ApiResponse(code = 409, message = "Thing could not be updated. Maybe it is not managed.") })
+            @ApiResponse(code = 409, message = "Thing could not be updated. Maybe it is not managed."),
+            @ApiResponse(code = 500, message = "Error processing request.") })
     public Response updateConfiguration(@HeaderParam(HttpHeaders.ACCEPT_LANGUAGE) String language,
             @PathParam("thingUID") @ApiParam(value = "thing") String thingUID,
             @ApiParam(value = "configuration parameters") Map<String, Object> configurationParameters)
-                    throws IOException {
+            throws IOException {
         final Locale locale = LocaleUtil.getLocale(language);
 
         ThingUID thingUIDObject = new ThingUID(thingUID);
@@ -493,7 +494,7 @@ public class ThingResource implements SatisfiableRESTResource {
         } catch (IllegalArgumentException ex) {
             logger.info("Received HTTP PUT request for update config at '{}' for the unknown thing '{}'.",
                     uriInfo.getPath(), thingUID);
-            return getThingNotFoundResponse(thingUID);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
         }
 
         return getThingResponse(Status.OK, thing, locale, null);
