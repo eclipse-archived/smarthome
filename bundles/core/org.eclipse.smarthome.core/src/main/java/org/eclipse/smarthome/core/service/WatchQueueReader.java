1514d73881e0c8fda14def5406b6d033b18dea92
Henning Treu <henning.treu@telekom.de>
2017-04-26 16:44:00 +0200

    * fix wrong assertion in AbstractWatchServiceTest * move watch directory creation/deletion before/after each test
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/service/WatchQueueReader.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/service/WatchQueueReader.java
@@ -264,13 +264,6 @@ public class WatchQueueReader implements Runnable {
                 return null;
             }
 
-            // the resolved 'path' is the pure directory name while resolvedContextPath may be
-            // baseWatchedDir/registeredPath
-            // on systems like macOS
-            if (baseWatchedDir.resolve(path).toFile().isDirectory()) {
-                return path;
-            }
-
             return resolvedContextPath;
         }
 
