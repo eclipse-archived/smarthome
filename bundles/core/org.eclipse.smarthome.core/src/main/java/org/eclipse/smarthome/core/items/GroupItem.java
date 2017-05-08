c9e4784c54d83d2b25649969870b6aa05cdc2353
Stefan Triller <stefan.triller@telekom.de>
2017-05-08 16:59:41 +0200

    Soft fallback to plain GroupItem instead of IllegalArgumentException (#3395)
    * Soft fallback to plain GroupItem instead of IllegalArgumentException
    
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/GroupItem.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/GroupItem.java
@@ -59,13 +59,16 @@ public class GroupItem extends GenericItem implements StateChangeListener {
     public GroupItem(String name, GenericItem baseItem, GroupFunction function) {
         super(TYPE, name);
 
-        if ((baseItem != null && function == null) || (baseItem == null && function != null)) {
-            throw new IllegalArgumentException("baseItem AND function have to be passed as arguments");
+        // we only allow GroupItem with BOTH, baseItem AND function set, or NONE of them set
+        if (baseItem == null || function == null) {
+            this.baseItem = null;
+            this.function = null;
+        } else {
+            this.function = function;
+            this.baseItem = baseItem;
         }
 
         members = new CopyOnWriteArraySet<Item>();
-        this.function = function;
-        this.baseItem = baseItem;
     }
 
     /**
