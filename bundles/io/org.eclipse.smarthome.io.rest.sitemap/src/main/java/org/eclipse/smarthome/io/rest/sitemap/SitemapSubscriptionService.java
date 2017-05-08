06adb3d211aa39c117773e19bbd52fd40ce454fc
Stefan Triller <stefan.triller@telekom.de>
2017-05-08 11:11:39 +0200

    Basic UI now updates new sitemap widgets without restart (#3379)
    * Basic UI now updates new sitemap widgets without restart
    * Remove StateChange Listeners on Widget removal + cleanup
    * removed unnecessary code
    
    Fixes #2706
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/io/org.eclipse.smarthome.io.rest.sitemap/src/main/java/org/eclipse/smarthome/io/rest/sitemap/SitemapSubscriptionService.java
+++ bundles/io/org.eclipse.smarthome.io.rest.sitemap/src/main/java/org/eclipse/smarthome/io/rest/sitemap/SitemapSubscriptionService.java
@@ -10,12 +10,17 @@ package org.eclipse.smarthome.io.rest.sitemap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.smarthome.io.rest.sitemap.internal.PageChangeListener;
 import org.eclipse.smarthome.io.rest.sitemap.internal.SitemapEvent;
+import org.eclipse.smarthome.model.core.EventType;
+import org.eclipse.smarthome.model.core.ModelRepository;
+import org.eclipse.smarthome.model.core.ModelRepositoryChangeListener;
 import org.eclipse.smarthome.model.sitemap.LinkableWidget;
 import org.eclipse.smarthome.model.sitemap.Sitemap;
 import org.eclipse.smarthome.model.sitemap.SitemapProvider;
@@ -34,9 +39,10 @@ import org.slf4j.LoggerFactory;
  *
  * @author Kai Kreuzer - Initial contribution and API
  */
-public class SitemapSubscriptionService {
+public class SitemapSubscriptionService implements ModelRepositoryChangeListener {
 
     private static final String SITEMAP_PAGE_SEPARATOR = "#";
+    private static final String SITEMAP_SUFFIX = ".sitemap";
 
     private final Logger logger = LoggerFactory.getLogger(SitemapSubscriptionService.class);
 
@@ -46,6 +52,7 @@ public class SitemapSubscriptionService {
     }
 
     private ItemUIRegistry itemUIRegistry;
+    private ModelRepository modelRepo;
     private List<SitemapProvider> sitemapProviders = new ArrayList<>();
 
     /* subscription id -> sitemap+page */
@@ -86,6 +93,16 @@ public class SitemapSubscriptionService {
 
     protected void removeSitemapProvider(SitemapProvider provider) {
         sitemapProviders.remove(provider);
+    }
+
+    protected void addModelRepository(ModelRepository modelRepo) {
+        this.modelRepo = modelRepo;
+        this.modelRepo.addModelRepositoryChangeListener(this);
+    }
+
+    protected void removeModelRepository(ModelRepository modelRepo) {
+        this.modelRepo.removeModelRepositoryChangeListener(this);
+        this.modelRepo = null;
     }
 
     /**
@@ -136,7 +153,7 @@ public class SitemapSubscriptionService {
      * @return the id of the currently active page
      */
     public String getPageId(String subscriptionId) {
-        return pageOfSubscription.get(subscriptionId).split(SITEMAP_PAGE_SEPARATOR)[1];
+        return extractPageId(pageOfSubscription.get(subscriptionId));
     }
 
     /**
@@ -146,7 +163,15 @@ public class SitemapSubscriptionService {
      * @return the name of the current sitemap
      */
     public String getSitemapName(String subscriptionId) {
-        return pageOfSubscription.get(subscriptionId).split(SITEMAP_PAGE_SEPARATOR)[0];
+        return extractSitemapName(pageOfSubscription.get(subscriptionId));
+    }
+
+    private String extractSitemapName(String sitemapWithPageId) {
+        return sitemapWithPageId.split(SITEMAP_PAGE_SEPARATOR)[0];
+    }
+
+    private String extractPageId(String sitemapWithPageId) {
+        return sitemapWithPageId.split(SITEMAP_PAGE_SEPARATOR)[1];
     }
 
     /**
@@ -178,17 +203,7 @@ public class SitemapSubscriptionService {
         if (listener == null) {
             // there is no listener for this page yet, so let's try to create one
             EList<Widget> widgets = null;
-            Sitemap sitemap = getSitemap(sitemapName);
-            if (sitemap != null) {
-                if (pageId.equals(sitemap.getName())) {
-                    widgets = sitemap.getChildren();
-                } else {
-                    Widget pageWidget = itemUIRegistry.getWidget(sitemap, pageId);
-                    if (pageWidget instanceof LinkableWidget) {
-                        widgets = itemUIRegistry.getChildren((LinkableWidget) pageWidget);
-                    }
-                }
-            }
+            widgets = collectWidgets(sitemapName, pageId);
             if (widgets != null) {
                 listener = new PageChangeListener(sitemapName, pageId, itemUIRegistry, widgets);
                 pageChangeListeners.put(getValue(sitemapName, pageId), listener);
@@ -197,6 +212,23 @@ public class SitemapSubscriptionService {
         if (listener != null) {
             listener.addCallback(callback);
         }
+    }
+
+    private EList<Widget> collectWidgets(String sitemapName, String pageId) {
+        EList<Widget> widgets = null;
+
+        Sitemap sitemap = getSitemap(sitemapName);
+        if (sitemap != null) {
+            if (pageId.equals(sitemap.getName())) {
+                widgets = sitemap.getChildren();
+            } else {
+                Widget pageWidget = itemUIRegistry.getWidget(sitemap, pageId);
+                if (pageWidget instanceof LinkableWidget) {
+                    widgets = itemUIRegistry.getChildren((LinkableWidget) pageWidget);
+                }
+            }
+        }
+        return widgets;
     }
 
     private void removeCallbackFromListener(String sitemapPage, SitemapSubscriptionCallback callback) {
@@ -223,6 +255,27 @@ public class SitemapSubscriptionService {
             }
         }
         return null;
+    }
+
+    @Override
+    public void modelChanged(String modelName, EventType type) {
+        if (type != EventType.MODIFIED || !modelName.endsWith(SITEMAP_SUFFIX)) {
+            return; // we process only sitemap modifications here
+        }
+
+        String changedSitemapName = StringUtils.removeEnd(modelName, SITEMAP_SUFFIX);
+
+        for (Entry<String, PageChangeListener> listenerEntry : pageChangeListeners.entrySet()) {
+            String sitemapWithPage = listenerEntry.getKey();
+            String sitemapName = extractSitemapName(sitemapWithPage);
+            String pageId = extractPageId(sitemapWithPage);
+
+            if (sitemapName.equals(changedSitemapName)) {
+                EList<Widget> widgets = collectWidgets(sitemapName, pageId);
+                listenerEntry.getValue().sitemapContentChanged(widgets);
+            }
+        }
+
     }
 
 }
