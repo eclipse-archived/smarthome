8602cf2bbd918ed0af1704edf48d18099a643dfb
Stefan Triller <stefan.triller@telekom.de>
2017-05-09 16:03:25 +0200

    A group without a function cannot have a state
    Fixes #1738
    
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/GroupItem.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/GroupItem.java
@@ -239,6 +239,11 @@ public class GroupItem extends GenericItem implements StateChangeListener {
      */
     @Override
     public State getStateAs(Class<? extends State> typeClass) {
+        // if a group does not have a function it cannot have a state
+        if (function == null) {
+            return null;
+        }
+
         State newState = function.getStateAs(getAllMembers(), typeClass);
         if (newState == null && baseItem != null) {
             // we use the transformation method from the base item
