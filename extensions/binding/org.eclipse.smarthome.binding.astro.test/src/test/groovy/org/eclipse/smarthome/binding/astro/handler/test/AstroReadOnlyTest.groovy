af109baea3be90fa288b4012efd96f9ac1b4d9cd
Svilen <svilen.valkanov@musala.com>
2017-05-16 17:27:25 +0300

    Initial implementation of AstroBindingTests. (#3430)
    * Initial implementation of AstroBindingTests.
    * Addressed sjka's comments
    * Astro tests rewokrked to plain unit tests.
    - Discovery test removed, besause it is network service dependant;
    - Other tests changed to remove the dependencies from ESH services;
    
    See: #3213
    See: openhab/openhab2-addons#1402
    Also-by: Petar Valchev <petar.valchev@musala.com>
    Signed-off-by: Svilen Valkanov <svilen.valkanov@musala.com>

---
+++ extensions/binding/org.eclipse.smarthome.binding.astro.test/src/test/groovy/org/eclipse/smarthome/binding/astro/handler/test/AstroReadOnlyTest.groovy
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.binding.astro.handler.test
+
+import static org.mockito.Mockito.*
+import static org.eclipse.smarthome.binding.astro.test.cases.AstroBindingTestsData.*
+
+import org.eclipse.smarthome.binding.astro.AstroBindingConstants
+import org.eclipse.smarthome.binding.astro.handler.SunHandler
+import org.eclipse.smarthome.binding.astro.test.cases.AstroBindingTestsData;
+import org.eclipse.smarthome.core.library.types.DateTimeType
+import org.eclipse.smarthome.core.thing.ChannelUID
+import org.eclipse.smarthome.core.thing.Thing
+import org.eclipse.smarthome.core.thing.ThingUID
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerCallback
+import org.junit.Test
+import org.mockito.Matchers
+
+/**
+ * OSGi tests for the {@link AstroThingHandler}
+ * <p>
+ * This class tests the handleUpdate method in the {@link AstroThingHandler}.
+ *
+ * @author Petar Valchev - Initial implementation
+ * @author Svilen Valkanov - Reworked to plain unit tests
+ *
+ */
+class AstroReadOnlyTest {
+    @Test
+    public void 'test handle command doesn`t update item state'() {
+        ThingUID thingUID = new ThingUID(AstroBindingConstants.THING_TYPE_SUN, TEST_SUN_THING_ID)
+        ChannelUID channelUID = new ChannelUID(thingUID, DEFAULT_TEST_CHANNEL_ID)
+        
+        Thing thing = mock(Thing.class)
+        ThingHandlerCallback callback = mock(ThingHandlerCallback.class)
+        SunHandler sunHandler = new SunHandler(thing);
+        sunHandler.setCallback(callback)
+        
+        def expectedState = new DateTimeType(Calendar.getInstance())
+        sunHandler.handleUpdate(channelUID, expectedState)
+        verify(callback, times(0)).stateUpdated(channelUID, expectedState)
+    }
+}
