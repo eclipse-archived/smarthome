aae69b9f0d978916d42584183de77ee511449d1f
Chris Jackson <chris@cd-jackson.com>
2017-06-13 22:47:13 +0100

    Updated to use multiple
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- extensions/ui/org.eclipse.smarthome.ui.paper/gulpfile.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/gulpfile.js
@@ -66,7 +66,6 @@ var paths = {
         './node_modules/masonry-layout/dist/masonry.pkgd.min.js',
         './node_modules/sprintf-js/dist/sprintf.min.js',
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/angular-material-expansion-panel/dist/md-expansion-panel.min.js',
         './node_modules/tinycolor2/tinycolor.js'
     ],
     JQUI: [{
