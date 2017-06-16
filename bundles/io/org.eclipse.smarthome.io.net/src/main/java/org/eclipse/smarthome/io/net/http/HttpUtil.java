39ecaf5c4e52ec677e0667be96688aa5c1ad35ec
lolodomo <lg.hc@free.fr>
2017-06-16 10:16:40 +0200

    Utils to download WEB image (#3457)
    Sonos binding: download of cover art updated
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/io/org.eclipse.smarthome.io.net/src/main/java/org/eclipse/smarthome/io/net/http/HttpUtil.java
+++ bundles/io/org.eclipse.smarthome.io.net/src/main/java/org/eclipse/smarthome/io/net/http/HttpUtil.java
@@ -7,12 +7,15 @@
  */
 package org.eclipse.smarthome.io.net.http;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -33,6 +36,7 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.util.B64Code;
 import org.eclipse.jetty.util.StringUtil;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.smarthome.core.library.types.RawType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,7 +51,15 @@ public class HttpUtil {
 
     private static Logger logger = LoggerFactory.getLogger(HttpUtil.class);
 
-    private static HttpClient client = new HttpClient(new SslContextFactory());
+    private static final HttpClient CLIENT = new HttpClient(new SslContextFactory());
+
+    private static class ProxyParams {
+        public String proxyHost = null;
+        public int proxyPort = 80;
+        public String proxyUser = null;
+        public String proxyPassword = null;
+        public String nonProxyHosts = null;
+    }
 
     /**
      * Executes the given <code>url</code> with the given <code>httpMethod</code>.
@@ -104,32 +116,11 @@ public class HttpUtil {
      */
     public static String executeUrl(String httpMethod, String url, Properties httpHeaders, InputStream content,
             String contentType, int timeout) throws IOException {
-        String proxySet = System.getProperty("http.proxySet");
 
-        String proxyHost = null;
-        int proxyPort = 80;
-        String proxyUser = null;
-        String proxyPassword = null;
-        String nonProxyHosts = null;
+        final ProxyParams proxyParams = prepareProxyParams();
 
-        if ("true".equalsIgnoreCase(proxySet)) {
-            proxyHost = System.getProperty("http.proxyHost");
-            String proxyPortString = System.getProperty("http.proxyPort");
-            if (StringUtils.isNotBlank(proxyPortString)) {
-                try {
-                    proxyPort = Integer.valueOf(proxyPortString);
-                } catch (NumberFormatException e) {
-                    logger.warn("'{}' is not a valid proxy port - using port 80 instead");
-                }
-            }
-            proxyUser = System.getProperty("http.proxyUser");
-            proxyPassword = System.getProperty("http.proxyPassword");
-            nonProxyHosts = System.getProperty("http.nonProxyHosts");
-        }
-
-        return executeUrl(httpMethod, url, httpHeaders, content, contentType, timeout, proxyHost, proxyPort, proxyUser,
-                proxyPassword, nonProxyHosts);
-
+        return executeUrl(httpMethod, url, httpHeaders, content, contentType, timeout, proxyParams.proxyHost,
+                proxyParams.proxyPort, proxyParams.proxyUser, proxyParams.proxyPassword, proxyParams.nonProxyHosts);
     }
 
     /**
@@ -147,6 +138,7 @@ public class HttpUtil {
      * @param proxyUser the username to authenticate with the proxy
      * @param proxyPassword the password to authenticate with the proxy
      * @param nonProxyHosts the hosts that won't be routed through the proxy
+     *
      * @return the response body or <code>NULL</code> when the request went wrong
      * @throws IOException when the request execution failed, timed out or it was interrupted
      */
@@ -154,13 +146,48 @@ public class HttpUtil {
             String contentType, int timeout, String proxyHost, Integer proxyPort, String proxyUser,
             String proxyPassword, String nonProxyHosts) throws IOException {
 
-        startHttpClient(client);
+        ContentResponse response = executeUrlAndGetReponse(httpMethod, url, httpHeaders, content, contentType, timeout,
+                proxyHost, proxyPort, proxyUser, proxyPassword, nonProxyHosts);
+        String encoding = response.getEncoding() != null ? response.getEncoding().replaceAll("\"", "").trim() : "UTF-8";
+        String responseBody;
+        try {
+            responseBody = new String(response.getContent(), encoding);
+        } catch (UnsupportedEncodingException e) {
+            responseBody = null;
+        }
+        return responseBody;
+    }
+
+    /**
+     * Executes the given <code>url</code> with the given <code>httpMethod</code>
+     *
+     * @param httpMethod the HTTP method to use
+     * @param url the url to execute (in milliseconds)
+     * @param httpHeaders optional HTTP headers which has to be set on request
+     * @param content the content to be send to the given <code>url</code> or <code>null</code> if no content should be
+     *            send.
+     * @param contentType the content type of the given <code>content</code>
+     * @param timeout the socket timeout to wait for data
+     * @param proxyHost the hostname of the proxy
+     * @param proxyPort the port of the proxy
+     * @param proxyUser the username to authenticate with the proxy
+     * @param proxyPassword the password to authenticate with the proxy
+     * @param nonProxyHosts the hosts that won't be routed through the proxy
+     *
+     * @return the response as a ContentResponse object or <code>NULL</code> when the request went wrong
+     * @throws IOException when the request execution failed, timed out or it was interrupted
+     */
+    private static ContentResponse executeUrlAndGetReponse(String httpMethod, String url, Properties httpHeaders,
+            InputStream content, String contentType, int timeout, String proxyHost, Integer proxyPort, String proxyUser,
+            String proxyPassword, String nonProxyHosts) throws IOException {
+
+        startHttpClient(CLIENT);
 
         HttpProxy proxy = null;
         // only configure a proxy if a host is provided
         if (StringUtils.isNotBlank(proxyHost) && proxyPort != null && shouldUseProxy(url, nonProxyHosts)) {
-            AuthenticationStore authStore = client.getAuthenticationStore();
-            ProxyConfiguration proxyConfig = client.getProxyConfiguration();
+            AuthenticationStore authStore = CLIENT.getAuthenticationStore();
+            ProxyConfiguration proxyConfig = CLIENT.getProxyConfiguration();
             List<Proxy> proxies = proxyConfig.getProxies();
 
             proxy = new HttpProxy(proxyHost, proxyPort);
@@ -182,7 +209,7 @@ public class HttpUtil {
 
         HttpMethod method = HttpUtil.createHttpMethod(httpMethod);
 
-        Request request = client.newRequest(url).method(method).timeout(timeout, TimeUnit.MILLISECONDS);
+        Request request = CLIENT.newRequest(url).method(method).timeout(timeout, TimeUnit.MILLISECONDS);
 
         if (httpHeaders != null) {
             for (String httpHeaderKey : httpHeaders.stringPropertyNames()) {
@@ -223,23 +250,39 @@ public class HttpUtil {
                 logger.debug("Method failed: {}", statusLine);
             }
 
-            byte[] rawResponse = response.getContent();
-            String encoding = response.getEncoding() != null ? response.getEncoding().replaceAll("\"", "").trim()
-                    : "UTF-8";
-            String responseBody = new String(rawResponse, encoding);
-            if (!responseBody.isEmpty()) {
-                logger.trace(responseBody);
-            }
-
-            return responseBody;
+            return response;
         } catch (Exception e) {
             throw new IOException(e);
         } finally {
             if (proxy != null) {
                 // Remove the proxy, that has been added for this request
-                client.getProxyConfiguration().getProxies().remove(proxy);
+                CLIENT.getProxyConfiguration().getProxies().remove(proxy);
             }
         }
+    }
+
+    /**
+     * Load proxy parameters in global variables proxyHost, proxyPort, proxyUser, proxyPassword and nonProxyHosts
+     */
+    private static ProxyParams prepareProxyParams() {
+        final ProxyParams proxyParams = new ProxyParams();
+        String proxySet = System.getProperty("http.proxySet");
+        if ("true".equalsIgnoreCase(proxySet)) {
+            proxyParams.proxyHost = System.getProperty("http.proxyHost");
+            String proxyPortString = System.getProperty("http.proxyPort");
+            if (StringUtils.isNotBlank(proxyPortString)) {
+                try {
+                    proxyParams.proxyPort = Integer.valueOf(proxyPortString);
+                } catch (NumberFormatException e) {
+                    logger.warn("'{}' is not a valid proxy port - using default port ({}) instead", proxyPortString,
+                            proxyParams.proxyPort);
+                }
+            }
+            proxyParams.proxyUser = System.getProperty("http.proxyUser");
+            proxyParams.proxyPassword = System.getProperty("http.proxyPassword");
+            proxyParams.nonProxyHosts = System.getProperty("http.nonProxyHosts");
+        }
+        return proxyParams;
     }
 
     /**
@@ -317,6 +360,129 @@ public class HttpUtil {
                 logger.warn("Cannot start HttpClient!", e);
             }
         }
+    }
+
+    /**
+     * Download the image data from an URL.
+     *
+     * If content type is not found in the headers, the data is scanned to determine the content type.
+     *
+     * @param url the URL of the image to be downloaded
+     * @return a RawType object containing the image, null if the content type could not be found or the content type is
+     *         not an image
+     */
+    public static RawType downloadImage(String url) {
+        return downloadImage(url, true, -1);
+    }
+
+    /**
+     * Download the image data from an URL.
+     *
+     * @param url the URL of the image to be downloaded
+     * @param scanTypeInContent true to allow the scan of data to determine the content type if not found in the headers
+     * @param maxContentLength the maximum data size in bytes to trigger the download; any negative value to ignore the
+     *            data size
+     * @return a RawType object containing the image, null if the content type could not be found or the content type is
+     *         not an image or the data size is too big
+     */
+    public static RawType downloadImage(String url, boolean scanTypeInContent, long maxContentLength) {
+        return downloadData(url, "image/.*", scanTypeInContent, maxContentLength);
+    }
+
+    /**
+     * Download the data from an URL.
+     *
+     * @param url the URL of the data to be downloaded
+     * @param contentTypeRegex the REGEX the content type must match; null to ignore the content type
+     * @param scanTypeInContent true to allow the scan of data to determine the content type if not found in the headers
+     * @param maxContentLength the maximum data size in bytes to trigger the download; any negative value to ignore the
+     *            data size
+     * @return a RawType object containing the downloaded data, null if the content type does not match the expected
+     *         type or the data size is too big
+     */
+    public static RawType downloadData(String url, String contentTypeRegex, boolean scanTypeInContent,
+            long maxContentLength) {
+
+        final ProxyParams proxyParams = prepareProxyParams();
+
+        RawType rawData = null;
+        try {
+            ContentResponse response = executeUrlAndGetReponse("GET", url, null, null, null, 5000,
+                    proxyParams.proxyHost, proxyParams.proxyPort, proxyParams.proxyUser, proxyParams.proxyPassword,
+                    proxyParams.nonProxyHosts);
+            byte[] data = response.getContent();
+            long length = (data == null) ? 0 : data.length;
+            String mediaType = response.getMediaType();
+            logger.debug("Media download response: status {} content length {} media type {} (URL {})",
+                    response.getStatus(), length, mediaType, url);
+
+            if (response.getStatus() != HttpStatus.OK_200 || length == 0) {
+                logger.debug("Media download failed: unexpected return code {} (URL {})", response.getStatus(), url);
+                return null;
+            }
+
+            if (maxContentLength >= 0 && length > maxContentLength) {
+                logger.debug("Media download aborted: content length {} too big (URL {})", length, url);
+                return null;
+            }
+
+            String contentType = mediaType;
+            if (contentTypeRegex != null) {
+                if ((contentType == null || contentType.isEmpty()) && scanTypeInContent) {
+                    // We try to get the type from the data
+                    contentType = guessContentTypeFromData(data);
+                    logger.debug("Media download: content type from data: {} (URL {})", contentType, url);
+                }
+                if (contentType != null && contentType.isEmpty()) {
+                    contentType = null;
+                }
+                if (contentType == null) {
+                    logger.debug("Media download aborted: unknown content type (URL {})", url);
+                    return null;
+                } else if (!contentType.matches(contentTypeRegex)) {
+                    logger.debug("Media download aborted: unexpected content type \"{}\" (URL {})", contentType, url);
+                    return null;
+                }
+            } else if (contentType == null || contentType.isEmpty()) {
+                contentType = RawType.DEFAULT_MIME_TYPE;
+            }
+
+            rawData = new RawType(data, contentType);
+
+        } catch (IOException e) {
+            rawData = null;
+        }
+        if (rawData == null) {
+            logger.debug("Media download failed (URL {})", url);
+        } else {
+            logger.debug("Media downloaded: size {} type {} (URL {})", rawData.getBytes().length, rawData.getMimeType(),
+                    url);
+        }
+        return rawData;
+    }
+
+    /**
+     * Determine the content type from the data.
+     *
+     * @param data the data as a buffer of bytes
+     *
+     * @return the MIME type of the content, null if the content type could not be found
+     */
+    public static String guessContentTypeFromData(byte[] data) {
+        String contentType = null;
+        try (final ByteArrayInputStream input = new ByteArrayInputStream(data)) {
+            try {
+                contentType = URLConnection.guessContentTypeFromStream(input);
+                if (contentType != null && contentType.isEmpty()) {
+                    contentType = null;
+                }
+            } catch (final IOException e) {
+                logger.debug("Failed to determine content type: {}", e.getMessage());
+            }
+        } catch (final IOException ex) {
+            // Error on closing input stream -- nothing we can do here.
+        }
+        return contentType;
     }
 
 }
