9e26afbb422bb12a5516af97844bd75e98269b7a
Stefan Triller <stefan.triller@telekom.de>
2017-06-24 23:48:25 +0200

    Astro Background discovery for location changes (#3725)
    * Astro Background discovery for location changes
    
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- extensions/binding/org.eclipse.smarthome.binding.astro/src/main/java/org/eclipse/smarthome/binding/astro/discovery/AstroDiscoveryService.java
+++ extensions/binding/org.eclipse.smarthome.binding.astro/src/main/java/org/eclipse/smarthome/binding/astro/discovery/AstroDiscoveryService.java
@@ -11,8 +11,10 @@ import static org.eclipse.smarthome.binding.astro.AstroBindingConstants.*;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-import org.eclipse.smarthome.binding.astro.AstroBindingConstants;
 import org.eclipse.smarthome.config.discovery.AbstractDiscoveryService;
 import org.eclipse.smarthome.config.discovery.DiscoveryResultBuilder;
 import org.eclipse.smarthome.core.i18n.LocationProvider;
@@ -31,29 +33,60 @@ import org.slf4j.LoggerFactory;
 public class AstroDiscoveryService extends AbstractDiscoveryService {
     private final Logger logger = LoggerFactory.getLogger(AstroDiscoveryService.class);
     private static final int DISCOVER_TIMEOUT_SECONDS = 30;
+    private static final int LOCATION_CHANGED_CHECK_INTERVAL = 60;
     private LocationProvider locationProvider;
+    private ScheduledFuture<?> astroDiscoveryJob;
+    private PointType previousLocation;
+
+    private static final ThingUID sunThing = new ThingUID(THING_TYPE_SUN, LOCAL);
+    private static final ThingUID moonThing = new ThingUID(THING_TYPE_MOON, LOCAL);
 
     /**
-     * Creates a AstroDiscoveryService with disabled autostart.
+     * Creates a AstroDiscoveryService with enabled autostart.
      */
     public AstroDiscoveryService() {
-        super(new HashSet<>(Arrays.asList(new ThingTypeUID(BINDING_ID, "-"))), DISCOVER_TIMEOUT_SECONDS, false);
+        super(new HashSet<>(Arrays.asList(new ThingTypeUID(BINDING_ID, "-"))), DISCOVER_TIMEOUT_SECONDS, true);
     }
 
     @Override
     protected void startScan() {
         logger.debug("Starting Astro discovery scan");
-
         PointType location = locationProvider.getLocation();
-
         if (location == null) {
             logger.debug("LocationProvider.getLocation() is not set -> Will not provide any discovery results");
             return;
         }
+        createResults(location);
+    }
 
-        ThingUID sunThing = new ThingUID(AstroBindingConstants.THING_TYPE_SUN, LOCAL);
-        ThingUID moonThing = new ThingUID(AstroBindingConstants.THING_TYPE_MOON, LOCAL);
+    @Override
+    protected void startBackgroundDiscovery() {
+        if (astroDiscoveryJob == null) {
+            astroDiscoveryJob = scheduler.scheduleAtFixedRate(() -> {
+                PointType currentLocation = locationProvider.getLocation();
+                if (!Objects.equals(currentLocation, previousLocation)) {
+                    logger.info("Location has been changed from {} to {}: Creating new Discovery Results",
+                            previousLocation, currentLocation);
+                    createResults(currentLocation);
+                    previousLocation = currentLocation;
+                }
+            }, 0, LOCATION_CHANGED_CHECK_INTERVAL, TimeUnit.SECONDS);
+            logger.debug("Scheduled astro location-changed job every {} seconds", LOCATION_CHANGED_CHECK_INTERVAL);
+        }
+    }
 
+    @Override
+    protected void stopBackgroundDiscovery() {
+        logger.debug("Stopping Astro device background discovery");
+        if (astroDiscoveryJob != null && !astroDiscoveryJob.isCancelled()) {
+            if (astroDiscoveryJob.cancel(true)) {
+                astroDiscoveryJob = null;
+                logger.debug("Stopped Astro device background discovery");
+            }
+        }
+    }
+
+    public void createResults(PointType location) {
         String propGeolocation;
         if (location.getAltitude() != null) {
             propGeolocation = String.format("%s,%s,%s", location.getLatitude(), location.getLongitude(),
