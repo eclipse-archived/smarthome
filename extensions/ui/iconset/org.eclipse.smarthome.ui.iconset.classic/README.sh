4b7b496406dc3f90410ddeca2ea4785374dd96ba
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2017-06-24 09:22:05 +0200

    Improve README script and result (#3737)
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- extensions/ui/iconset/org.eclipse.smarthome.ui.iconset.classic/README.sh
+++ extensions/ui/iconset/org.eclipse.smarthome.ui.iconset.classic/README.sh
@@ -7,12 +7,12 @@ README="$(cd "$(dirname "$0")"; pwd)/README.md"
 cat <<EOF > "${README}"
 # Classic Icon Set
 
-This is a modernized version of the original icon set of openHAB 1.<br/><br/>
+This is a modernized version of the original icon set of openHAB 1. The set is provided with the distribution in both the PNG and SVG file format. Move your mouse over an icon to learn its name.<br/><br/>
 
 EOF
 
 
-for icon in icons/*.png; do
+for icon in $(ls icons/*.png | sort -V); do
 
   name="$(basename "${icon}" | cut -d '.' -f1)"
 
