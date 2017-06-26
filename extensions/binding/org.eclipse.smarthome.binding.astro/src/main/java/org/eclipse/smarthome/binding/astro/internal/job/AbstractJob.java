55f9a696b22de77b268b5e1d161ce96f9caa8075
Amit Kumar Mondal <admin@amitinside.com>
2017-06-26 18:30:47 +0200

    Synchronous Execution of Astro Startup Jobs and minor fixes (#3746)
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

--- extensions/binding/org.eclipse.smarthome.binding.astro/src/main/java/org/eclipse/smarthome/binding/astro/internal/job/AbstractJob.java
+++ extensions/binding/org.eclipse.smarthome.binding.astro/src/main/java/org/eclipse/smarthome/binding/astro/internal/job/AbstractJob.java
@@ -21,36 +21,6 @@ public abstract class AbstractJob implements Job {
         return thingUID;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (thingUID == null ? 0 : thingUID.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        AbstractJob other = (AbstractJob) obj;
-        if (thingUID == null) {
-            if (other.thingUID != null) {
-                return false;
-            }
-        } else if (!thingUID.equals(other.thingUID)) {
-            return false;
-        }
-        return true;
-    }
-
     /**
      * Ensures the truth of an expression involving one or more parameters to the
      * calling method.
