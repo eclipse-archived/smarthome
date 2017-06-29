68f73884a6aeb83a51f66e285f953e008c904265
Simon Kaufmann <simon.kfm@gmail.com>
2017-06-29 13:21:57 +0200

    config option provider for language & region settings (#3755)
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ParameterOption.java
+++ bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ParameterOption.java
@@ -45,4 +45,42 @@ public class ParameterOption {
         return this.getClass().getSimpleName() + " [value=\"" + value + "\", label=\"" + label + "\"]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((label == null) ? 0 : label.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ParameterOption other = (ParameterOption) obj;
+        if (label == null) {
+            if (other.label != null) {
+                return false;
+            }
+        } else if (!label.equals(other.label)) {
+            return false;
+        }
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals(other.value)) {
+            return false;
+        }
+        return true;
+    }
+
 }
