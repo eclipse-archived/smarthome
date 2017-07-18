95f98b6abfb80264f8c196b7bd19930f660a46e1
Stefan Triller <stefan.triller@telekom.de>
2017-07-18 12:09:49 +0200

    Fix licence header
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/handler/ChannelEventTriggerHandler.java
+++ bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/handler/ChannelEventTriggerHandler.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 1997, 2015 by the respective copyright holders.
+ * Copyright (c) 2017 by Deutsche Telekom AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
