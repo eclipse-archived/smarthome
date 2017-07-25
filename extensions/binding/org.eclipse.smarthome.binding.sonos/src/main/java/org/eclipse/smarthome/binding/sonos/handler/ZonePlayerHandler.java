eclipse-archived/smarthome

commit db27358d81cf39c2ec66e295c24332009b81d9ca

Improve Sonos notification timeout handling
1. Extract the notification timeout as a thing configuration parameter (#3808)
2. Increase the default notification timeout to 40 sec. (as 20 s is too short time)

Signed-off-by: Ivaylo Ivanov <ivivanov.bg@gmail.com>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/config/config.xml
@@ -1,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<config-description:config-descriptions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:config-description="http://eclipse.org/smarthome/schemas/config-description/v1.0.0"
+	xsi:schemaLocation="http://eclipse.org/smarthome/schemas/config-description/v1.0.0 http://eclipse.org/smarthome/schemas/config-description-1.0.0.xsd"
+>
+	<config-description uri="config:sonos:zoneplayer">
+		<parameter name="udn" type="text">
+			<label>Unique Device Name</label>
+			<description>The UDN identifies the Zone Player.</description>
+			<required>true</required>
+		</parameter>
+		<parameter name="notificationTimeout" type="integer">
+			<label>Notification Timeout</label>
+			<description>Specifies the amount of time for which the notification sound will be played</description>
+			<default>40</default>
+			<required>true</required>
+		</parameter>
+		<parameter name="refresh" type="integer">
+			<label>Refresh interval</label>
+			<description>Specifies the refresh interval in seconds</description>
+			<default>60</default>
+		</parameter>
+	</config-description>
+</config-description:config-descriptions>

extensions/binding/org.eclipse.smarthome.binding.sonos/src/main/java/org/eclipse/smarthome/binding/sonos/handler/ZonePlayerHandler.java
@@ -37,6 +37,7 @@ import org.eclipse.smarthome.binding.sonos.internal.SonosMetaData;
 import org.eclipse.smarthome.binding.sonos.internal.SonosXMLParser;
 import org.eclipse.smarthome.binding.sonos.internal.SonosZoneGroup;
 import org.eclipse.smarthome.binding.sonos.internal.SonosZonePlayerState;
+import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.config.discovery.DiscoveryServiceRegistry;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.IncreaseDecreaseType;
@@ -102,7 +103,12 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
     /**
      * Default notification timeout
      */
-    private static final int NOTIFICATION_TIMEOUT = 20000;
+    private static final Integer DEFAULT_NOTIFICATION_TIMEOUT = 40000;
+    
+    /**
+     * configurable notification timeout
+     */
+    private Integer notificationTimeout = null;
 
     /**
      * Intrinsic lock used to synchronize the execution of notification sounds
@@ -205,8 +211,16 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
         }
 
         if (getUDN() != null) {
-            updateStatus(ThingStatus.ONLINE);
             onUpdate();
+            
+            if (getConfigAs(ZonePlayerConfiguration.class).notificationTimeout == null) {
+                Configuration c = editConfiguration();
+                c.put(ZonePlayerConfiguration.NOTIFICATION_TIMEOUT, DEFAULT_NOTIFICATION_TIMEOUT);
+                updateConfiguration(c);
+            }
+            
+            this.notificationTimeout = getConfigAs(ZonePlayerConfiguration.class).notificationTimeout;
+            
             super.initialize();
         } else {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR);
@@ -2374,7 +2388,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
         // check Sonos state events to determine the end of the notification sound
         String notificationTitle = stateMap.get("CurrentTitle");
         long playstart = System.currentTimeMillis();
-        while (System.currentTimeMillis() - playstart < NOTIFICATION_TIMEOUT) {
+        while (System.currentTimeMillis() - playstart < this.notificationTimeout.longValue()) {
             try {
                 Thread.sleep(50);
                 if (!notificationTitle.equals(stateMap.get("CurrentTitle"))
@@ -2393,7 +2407,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
             while (!stateMap.get("TransportState").equals(state)) {
                 try {
                     Thread.sleep(50);
-                    if (System.currentTimeMillis() - start > NOTIFICATION_TIMEOUT) {
+                    if (System.currentTimeMillis() - start > this.notificationTimeout.longValue()) {
                         break;
                     }
                 } catch (InterruptedException e) {
@@ -2409,7 +2423,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
             while (stateMap.get("TransportState").equals(state)) {
                 try {
                     Thread.sleep(50);
-                    if (System.currentTimeMillis() - start > NOTIFICATION_TIMEOUT) {
+                    if (System.currentTimeMillis() - start > this.notificationTimeout.longValue()) {
                         break;
                     }
                 } catch (InterruptedException e) {

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/thing/CONNECT.xml
@@ -62,18 +62,6 @@
 			<property name="modelId"></property>
 		</properties>
 
-		<config-description>
-			<parameter name="udn" type="text">
-				<label>Unique Device Name</label>
-				<description>The UDN identifies the Zone Player.</description>
-				<required>true</required>
-			</parameter>
-
-			<parameter name="refresh" type="integer">
-				<label>Refresh interval</label>
-				<description>Specifies the refresh interval in seconds</description>
-				<default>60</default>
-			</parameter>
-		</config-description>
+		<config-description-ref uri="config:sonos:zoneplayer" />
 	</thing-type>
 </thing:thing-descriptions>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/thing/CONNECTAMP.xml
@@ -63,18 +63,6 @@
 			<property name="modelId">CONNECT:AMP</property>
 		</properties>
 
-		<config-description>
-			<parameter name="udn" type="text">
-				<label>Unique Device Name</label>
-				<description>The UDN identifies the Zone Player.</description>
-				<required>true</required>
-			</parameter>
-
-			<parameter name="refresh" type="integer">
-				<label>Refresh interval</label>
-				<description>Specifies the refresh interval in seconds</description>
-				<default>60</default>
-			</parameter>
-		</config-description>
+		<config-description-ref uri="config:sonos:zoneplayer" />
 	</thing-type>
 </thing:thing-descriptions>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/thing/PLAY1.xml
@@ -60,18 +60,6 @@
 			<property name="modelId">PLAY:1</property>
 		</properties>
 
-		<config-description>
-			<parameter name="udn" type="text">
-				<label>Unique Device Name</label>
-				<description>The UDN identifies the Zone Player.</description>
-				<required>true</required>
-			</parameter>
-
-			<parameter name="refresh" type="integer">
-				<label>Refresh interval</label>
-				<description>Specifies the refresh interval in seconds</description>
-				<default>60</default>
-			</parameter>
-		</config-description>
+		<config-description-ref uri="config:sonos:zoneplayer" />
 	</thing-type>
 </thing:thing-descriptions>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/thing/PLAY3.xml
@@ -60,18 +60,6 @@
 			<property name="modelId">PLAY:3</property>
 		</properties>
 
-		<config-description>
-			<parameter name="udn" type="text">
-				<label>Unique Device Name</label>
-				<description>The UDN identifies the Zone Player.</description>
-				<required>true</required>
-			</parameter>
-
-			<parameter name="refresh" type="integer">
-				<label>Refresh interval</label>
-				<description>Specifies the refresh interval in seconds</description>
-				<default>60</default>
-			</parameter>
-		</config-description>
+		<config-description-ref uri="config:sonos:zoneplayer" />
 	</thing-type>
 </thing:thing-descriptions>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/thing/PLAY5.xml
@@ -63,18 +63,6 @@
 			<property name="modelId">PLAY:5</property>
 		</properties>
 
-		<config-description>
-			<parameter name="udn" type="text">
-				<label>Unique Device Name</label>
-				<description>The UDN identifies the Zone Player.</description>
-				<required>true</required>
-			</parameter>
-
-			<parameter name="refresh" type="integer">
-				<label>Refresh interval</label>
-				<description>Specifies the refresh interval in seconds</description>
-				<default>60</default>
-			</parameter>
-		</config-description>
+		<config-description-ref uri="config:sonos:zoneplayer" />
 	</thing-type>
 </thing:thing-descriptions>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/thing/PLAYBAR.xml
@@ -62,18 +62,6 @@
 			<property name="modelId">PLAYBAR</property>
 		</properties>
 
-		<config-description>
-			<parameter name="udn" type="text">
-				<label>Unique Device Name</label>
-				<description>The UDN identifies the Zone Player.</description>
-				<required>true</required>
-			</parameter>
-
-			<parameter name="refresh" type="integer">
-				<label>Refresh interval</label>
-				<description>Specifies the refresh interval in seconds</description>
-				<default>60</default>
-			</parameter>
-		</config-description>
+		<config-description-ref uri="config:sonos:zoneplayer" />
 	</thing-type>
 </thing:thing-descriptions>

commit e3aea88abe60732fdbb26fc781fa75828341d643

Sonos binding: fix bug with notification timeout handling
Fix bug introduced by PR #3808

Signed-off-by: Laurent Garnier <lg.hc@free.fr>

extensions/binding/org.eclipse.smarthome.binding.sonos/ESH-INF/config/config.xml
@@ -9,11 +9,11 @@
 			<description>The UDN identifies the Zone Player.</description>
 			<required>true</required>
 		</parameter>
-		<parameter name="notificationTimeout" type="integer">
+		<parameter name="notificationTimeout" type="integer" unit="s">
 			<label>Notification Timeout</label>
-			<description>Specifies the amount of time for which the notification sound will be played</description>
-			<default>40</default>
-			<required>true</required>
+			<description>Specifies the amount of time in seconds for which the notification sound will be played</description>
+			<unitLabel>second</unitLabel>
+			<default>20</default>
 		</parameter>
 		<parameter name="refresh" type="integer">
 			<label>Refresh interval</label>

extensions/binding/org.eclipse.smarthome.binding.sonos/src/main/java/org/eclipse/smarthome/binding/sonos/handler/ZonePlayerHandler.java
@@ -37,7 +37,6 @@ import org.eclipse.smarthome.binding.sonos.internal.SonosMetaData;
 import org.eclipse.smarthome.binding.sonos.internal.SonosXMLParser;
 import org.eclipse.smarthome.binding.sonos.internal.SonosZoneGroup;
 import org.eclipse.smarthome.binding.sonos.internal.SonosZonePlayerState;
-import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.config.discovery.DiscoveryServiceRegistry;
 import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.library.types.IncreaseDecreaseType;
@@ -101,12 +100,12 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
     private static final int SOCKET_TIMEOUT = 5000;
 
     /**
-     * Default notification timeout
+     * Default notification timeout (in seconds)
      */
-    private static final Integer DEFAULT_NOTIFICATION_TIMEOUT = 40000;
-    
+    private static final Integer DEFAULT_NOTIFICATION_TIMEOUT = 20;
+
     /**
-     * configurable notification timeout
+     * configurable notification timeout (in seconds)
      */
     private Integer notificationTimeout = null;
 
@@ -212,15 +211,12 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
 
         if (getUDN() != null) {
             onUpdate();
-            
-            if (getConfigAs(ZonePlayerConfiguration.class).notificationTimeout == null) {
-                Configuration c = editConfiguration();
-                c.put(ZonePlayerConfiguration.NOTIFICATION_TIMEOUT, DEFAULT_NOTIFICATION_TIMEOUT);
-                updateConfiguration(c);
-            }
-            
+
             this.notificationTimeout = getConfigAs(ZonePlayerConfiguration.class).notificationTimeout;
-            
+            if (this.notificationTimeout == null) {
+                this.notificationTimeout = DEFAULT_NOTIFICATION_TIMEOUT;
+            }
+
             super.initialize();
         } else {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR);
@@ -2388,7 +2384,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
         // check Sonos state events to determine the end of the notification sound
         String notificationTitle = stateMap.get("CurrentTitle");
         long playstart = System.currentTimeMillis();
-        while (System.currentTimeMillis() - playstart < this.notificationTimeout.longValue()) {
+        while (System.currentTimeMillis() - playstart < this.notificationTimeout.longValue() * 1000) {
             try {
                 Thread.sleep(50);
                 if (!notificationTitle.equals(stateMap.get("CurrentTitle"))
@@ -2407,7 +2403,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
             while (!stateMap.get("TransportState").equals(state)) {
                 try {
                     Thread.sleep(50);
-                    if (System.currentTimeMillis() - start > this.notificationTimeout.longValue()) {
+                    if (System.currentTimeMillis() - start > this.notificationTimeout.longValue() * 1000) {
                         break;
                     }
                 } catch (InterruptedException e) {
@@ -2423,7 +2419,7 @@ public class ZonePlayerHandler extends BaseThingHandler implements UpnpIOPartici
             while (stateMap.get("TransportState").equals(state)) {
                 try {
                     Thread.sleep(50);
-                    if (System.currentTimeMillis() - start > this.notificationTimeout.longValue()) {
+                    if (System.currentTimeMillis() - start > this.notificationTimeout.longValue() * 1000) {
                         break;
                     }
                 } catch (InterruptedException e) {