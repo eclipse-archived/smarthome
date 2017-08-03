2d54d1967ee8f850b5272a5514a913e87f3a501c
Andre Fuechsel <afuechsel@gmail.com>
2017-08-03 13:42:40 +0200

    ignore the CronExpressionTest#findNext (#3944)
    Issue #3912
    
    Signed-off-by: Andre Fuechsel <andre.fuechsel@telekom.de>

--- bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/scheduler/CronExpressionTest.java
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/scheduler/CronExpressionTest.java
@@ -17,6 +17,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -78,6 +79,8 @@ public class CronExpressionTest {
     }
 
     @Test
+    @Ignore
+    // FIXME: see issue #3912
     public void findNext() throws ParseException {
         boolean trace = false;
 
