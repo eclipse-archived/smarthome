2921019862efa632123934424db07d3d08cb934a
Svilen <svilen.valkanov@musala.com>
2017-08-09 10:46:23 +0300

    Apply ESH code formatter (#3992)
    Signed-off-by: Svilen Valkanov <svilen.valkanov@musala.com>

--- extensions/ui/org.eclipse.smarthome.ui.classic/src/main/java/org/eclipse/smarthome/ui/classic/internal/render/MapviewRenderer.java
+++ extensions/ui/org.eclipse.smarthome.ui.classic/src/main/java/org/eclipse/smarthome/ui/classic/internal/render/MapviewRenderer.java
@@ -18,41 +18,43 @@ import org.eclipse.smarthome.ui.classic.render.RenderException;
 /**
  * This is an implementation of the {@link WidgetRenderer} interface, which
  * can produce HTML code for Text widgets.
- * 
+ *
  * @author Gaël L'hopital - Initial contribution
  *
  */
 public class MapviewRenderer extends AbstractWidgetRenderer {
 
-	public boolean canRender(Widget w) {
-		return w instanceof Mapview;
-	}
-	
-	public EList<Widget> renderWidget(Widget w, StringBuilder sb) throws RenderException {
-		Mapview mapview = (Mapview) w;
-		String snippet = getSnippet("mapview");
-		
-		State state = itemUIRegistry.getState(mapview);
-		if(state instanceof PointType) {
-			PointType pointState = (PointType) state;
-			double latitude = pointState.getLatitude().doubleValue();
-			double longitude = pointState.getLongitude().doubleValue();
-			snippet = StringUtils.replace(snippet, "%lat%", Double.toString(latitude));
-			snippet = StringUtils.replace(snippet, "%lon%", Double.toString(longitude));
-			snippet = StringUtils.replace(snippet, "%lonminus%", Double.toString(longitude-0.01));
-			snippet = StringUtils.replace(snippet, "%lonplus%", Double.toString(longitude+0.01));
-			snippet = StringUtils.replace(snippet, "%latminus%", Double.toString(latitude-0.01));
-			snippet = StringUtils.replace(snippet, "%latplus%", Double.toString(latitude+0.01));
-		}
-		
-		int height = mapview.getHeight();
-		if(height==0) {
-			height = 4;	// set default height to something viewable
-		}
-		height = height * 36;
-		snippet = StringUtils.replace(snippet, "%height%", Integer.toString(height));
-		
-		sb.append(snippet);
-		return null;
-	}
+    @Override
+    public boolean canRender(Widget w) {
+        return w instanceof Mapview;
+    }
+
+    @Override
+    public EList<Widget> renderWidget(Widget w, StringBuilder sb) throws RenderException {
+        Mapview mapview = (Mapview) w;
+        String snippet = getSnippet("mapview");
+
+        State state = itemUIRegistry.getState(mapview);
+        if (state instanceof PointType) {
+            PointType pointState = (PointType) state;
+            double latitude = pointState.getLatitude().doubleValue();
+            double longitude = pointState.getLongitude().doubleValue();
+            snippet = StringUtils.replace(snippet, "%lat%", Double.toString(latitude));
+            snippet = StringUtils.replace(snippet, "%lon%", Double.toString(longitude));
+            snippet = StringUtils.replace(snippet, "%lonminus%", Double.toString(longitude - 0.01));
+            snippet = StringUtils.replace(snippet, "%lonplus%", Double.toString(longitude + 0.01));
+            snippet = StringUtils.replace(snippet, "%latminus%", Double.toString(latitude - 0.01));
+            snippet = StringUtils.replace(snippet, "%latplus%", Double.toString(latitude + 0.01));
+        }
+
+        int height = mapview.getHeight();
+        if (height == 0) {
+            height = 4; // set default height to something viewable
+        }
+        height = height * 36;
+        snippet = StringUtils.replace(snippet, "%height%", Integer.toString(height));
+
+        sb.append(snippet);
+        return null;
+    }
 }
