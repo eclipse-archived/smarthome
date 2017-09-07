7e38493181366972733fe9bc858bd738ef99f803
Benedikt Niehues <benedikt.niehues@itemis.de>
2017-09-07 10:52:00 +0200

    fix codestyle in BaseRegistry.java

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/common/registry/BaseRegistry.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/common/registry/BaseRegistry.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.eclipse.smarthome.core.common.registry;
 
 import java.util.Collection;
@@ -9,6 +16,7 @@ import org.eclipse.jdt.annotation.Nullable;
 /**
  * This interface is needed to provide binary compatibility with the former {@Link Registry} interface.
  * It's completely the same interface as {@Link Registry} with the type restriction E extends Identifiable.
+ * @author Michael Bock - Initial contribution
  */
 public interface BaseRegistry<E, K> {
 
