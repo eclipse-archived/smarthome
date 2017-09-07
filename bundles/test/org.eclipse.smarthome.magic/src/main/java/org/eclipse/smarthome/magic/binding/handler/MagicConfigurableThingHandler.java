50a92b6a0817166bda9655e18945fff6416c39cd
Stefan Triller <stefan.triller@telekom.de>
2017-09-07 16:09:45 +0200

    Add missing licence header
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/test/org.eclipse.smarthome.magic/src/main/java/org/eclipse/smarthome/magic/binding/handler/MagicConfigurableThingHandler.java
+++ bundles/test/org.eclipse.smarthome.magic/src/main/java/org/eclipse/smarthome/magic/binding/handler/MagicConfigurableThingHandler.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.eclipse.smarthome.magic.binding.handler;
 
 import java.util.Map;
@@ -9,6 +16,12 @@ import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.binding.BaseThingHandler;
 import org.eclipse.smarthome.core.types.Command;
 
+/**
+ * Handler for thing with a configuration parameter
+ *
+ * @author Stefan Triller
+ *
+ */
 public class MagicConfigurableThingHandler extends BaseThingHandler {
 
     public MagicConfigurableThingHandler(@NonNull Thing thing) {
