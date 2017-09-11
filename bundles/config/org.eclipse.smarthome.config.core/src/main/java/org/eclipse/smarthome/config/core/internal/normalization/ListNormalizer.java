7969df380a531bc255cc1c251a0044578d821e7e
VelinYordanov <velin.iordanov@gmail.com>
2017-09-11 10:27:22 +0300

    Renamed a package in the org.eclipse.smarthome.config.core bundle. (#4224)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/internal/normalization/ListNormalizer.java
+++ bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/internal/normalization/ListNormalizer.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.config.core.normalization;
+package org.eclipse.smarthome.config.core.internal.normalization;
 
 import java.util.ArrayList;
 import java.util.List;
