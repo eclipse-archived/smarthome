470c9c2be52a3f283555a2fc2aa9cc9926b5e602
VelinYordanov <velin.iordanov@gmail.com>
2017-09-11 10:18:01 +0300

    Renamed a few packages in the org.eclipse.smarthome.automation.module.core bundle. (#4222)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/internal/exception/UncomparableException.java
+++ bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/internal/exception/UncomparableException.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.automation.module.core.handler.exception;
+package org.eclipse.smarthome.automation.module.core.internal.exception;
 
 /**
  * This Exception is used as an indicator for not matching types during comparation
