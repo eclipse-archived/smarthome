5bd0a81ed098aa3fb84c324451e4bc167c91c219
VelinYordanov <velin.iordanov@gmail.com>
2017-09-14 17:02:47 +0300

    Renamed a package in the org.eclipse.smarthome.io.rest.log bundle. (#4249)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- bundles/io/org.eclipse.smarthome.io.rest.log/src/main/java/org/eclipse/smarthome/io/rest/log/internal/LogConstants.java
+++ bundles/io/org.eclipse.smarthome.io.rest.log/src/main/java/org/eclipse/smarthome/io/rest/log/internal/LogConstants.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.io.rest.log;
+package org.eclipse.smarthome.io.rest.log.internal;
 
 /**
  * The {@link LogConstants} class defines common constants, which are
