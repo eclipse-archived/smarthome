ae86aa4b669ee3c6b151d312c57667f2c9ed94c5
VelinYordanov <velin.iordanov@gmail.com>
2017-09-21 18:53:16 +0300

    Renamed the discovery package to internal in the ntp binding. (#4310)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- extensions/binding/org.eclipse.smarthome.binding.ntp/src/main/java/org/eclipse/smarthome/binding/ntp/internal/discovery/NtpDiscovery.java
+++ extensions/binding/org.eclipse.smarthome.binding.ntp/src/main/java/org/eclipse/smarthome/binding/ntp/internal/discovery/NtpDiscovery.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.binding.ntp.discovery;
+package org.eclipse.smarthome.binding.ntp.internal.discovery;
 
 import static org.eclipse.smarthome.binding.ntp.NtpBindingConstants.*;
 
