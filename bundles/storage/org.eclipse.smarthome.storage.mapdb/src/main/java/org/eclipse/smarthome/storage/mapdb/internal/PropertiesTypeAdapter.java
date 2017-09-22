2c03992772aca0f8ec6be8f2e150818aac977fb1
VelinYordanov <velin.iordanov@gmail.com>
2017-09-22 10:48:03 +0300

    Renamed a few packages to internal in the org.eclipse.smarthome.storage.mapdb bundle. (#4316)
    See openhab/static-code-analysis#100
    
    Signed-off-by: VelinYordanov <velin.iordanov@gmail.com>

--- bundles/storage/org.eclipse.smarthome.storage.mapdb/src/main/java/org/eclipse/smarthome/storage/mapdb/internal/PropertiesTypeAdapter.java
+++ bundles/storage/org.eclipse.smarthome.storage.mapdb/src/main/java/org/eclipse/smarthome/storage/mapdb/internal/PropertiesTypeAdapter.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.eclipse.smarthome.storage.mapdb;
+package org.eclipse.smarthome.storage.mapdb.internal;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
