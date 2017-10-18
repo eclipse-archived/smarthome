783adb2c01b750d1fe4579e1e6ff079db7f4d391
Henning Treu <henning.treu@telekom.de>
2017-10-18 06:37:37 +0200

    Test the HttpContextFactoryService
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

---
+++ bundles/io/org.eclipse.smarthome.io.net.test/src/test/java/org/eclipse/smarthome/io/net/http/internal/HttpContextFactoryServiceImplTest.java
@@ -0,0 +1,43 @@
+package org.eclipse.smarthome.io.net.http.internal;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.eclipse.smarthome.io.net.http.HttpContextFactoryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.osgi.framework.Bundle;
+import org.osgi.service.http.HttpContext;
+
+public class HttpContextFactoryServiceImplTest {
+
+    private static final String RESOURCE = "resource";
+
+    private HttpContextFactoryService httpContextFactoryService;
+
+    @Mock
+    private Bundle bundle;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+        httpContextFactoryService = new HttpContextFactoryServiceImpl();
+    }
+
+    @Test
+    public void shouldCreateHttpContext() {
+        HttpContext context = httpContextFactoryService.createHttpContext(bundle);
+        assertThat(context, is(notNullValue()));
+    }
+
+    @Test
+    public void httpContextShouldCallgetResourceOnBundle() {
+        HttpContext context = httpContextFactoryService.createHttpContext(bundle);
+        context.getResource(RESOURCE);
+
+        verify(bundle).getResource(RESOURCE);
+    }
+}
