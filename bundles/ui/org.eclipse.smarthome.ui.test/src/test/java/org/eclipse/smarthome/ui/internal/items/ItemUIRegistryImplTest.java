9dfeb8a6de0d75379bbb7f1ba5eeaa8e1a55a6e6
Henning Treu <henning.treu@telekom.de>
2017-10-19 13:59:10 +0200

    refactor ItemUIRegistryImplTest
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- bundles/ui/org.eclipse.smarthome.ui.test/src/test/java/org/eclipse/smarthome/ui/internal/items/ItemUIRegistryImplTest.java
+++ bundles/ui/org.eclipse.smarthome.ui.test/src/test/java/org/eclipse/smarthome/ui/internal/items/ItemUIRegistryImplTest.java
@@ -10,6 +10,7 @@ package org.eclipse.smarthome.ui.internal.items;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
@@ -39,144 +40,133 @@ import org.eclipse.smarthome.model.sitemap.Widget;
 import org.eclipse.smarthome.ui.items.ItemUIProvider;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class ItemUIRegistryImplTest {
 
-    static private ItemRegistry registry;
-    static private ItemUIRegistryImpl uiRegistry = new ItemUIRegistryImpl();
+    static private ItemUIRegistryImpl uiRegistry;
     // we need to get the decimal separator of the default locale for our tests
     static private final char sep = (new DecimalFormatSymbols().getDecimalSeparator());
 
+    @Mock
+    static private ItemRegistry registry;
+
+    @Mock
+    private Widget widget;
+
+    @Mock
+    private Item item;
+
     @Before
-    public void prepareRegistry() {
-        registry = mock(ItemRegistry.class);
+    public void setup() throws Exception {
+        initMocks(this);
+        uiRegistry = new ItemUIRegistryImpl();
         uiRegistry.setItemRegistry(registry);
+
+        when(widget.getItem()).thenReturn("Item");
+        when(registry.getItem("Item")).thenReturn(item);
     }
 
     @Test
     public void getLabel_plainLabel() {
         String testLabel = "This is a plain text";
-        Widget w = mock(Widget.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        String label = uiRegistry.getLabel(w);
+
+        when(widget.getLabel()).thenReturn(testLabel);
+        String label = uiRegistry.getLabel(widget);
         assertEquals(testLabel, label);
     }
 
     @Test
     public void getLabel_labelWithStaticValue() {
         String testLabel = "Label [value]";
-        Widget w = mock(Widget.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        String label = uiRegistry.getLabel(w);
+
+        when(widget.getLabel()).thenReturn(testLabel);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [value]", label);
     }
 
     @Test
     public void getLabel_labelWithStringValue() throws ItemNotFoundException {
         String testLabel = "Label [%s]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [State]", label);
     }
 
     @Test
     public void getLabel_labelWithIntegerValue() throws ItemNotFoundException {
         String testLabel = "Label [%d]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DecimalType(20));
         when(item.getStateAs(DecimalType.class)).thenReturn(new DecimalType(20));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [20]", label);
     }
 
     @Test
     public void getLabel_labelWithIntegerValueAndWidth() throws ItemNotFoundException {
         String testLabel = "Label [%3d]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DecimalType(20));
         when(item.getStateAs(DecimalType.class)).thenReturn(new DecimalType(20));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [ 20]", label);
     }
 
     @Test
     public void getLabel_labelWithHexValueAndWidth() throws ItemNotFoundException {
         String testLabel = "Label [%3x]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DecimalType(20));
         when(item.getStateAs(DecimalType.class)).thenReturn(new DecimalType(20));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [ 14]", label);
     }
 
     @Test
     public void getLabel_labelWithDecimalValue() throws ItemNotFoundException {
         String testLabel = "Label [%.3f]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DecimalType(10f / 3f));
         when(item.getStateAs(DecimalType.class)).thenReturn(new DecimalType(10f / 3f));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [3" + sep + "333]", label);
     }
 
     @Test
     public void getLabel_labelWithPercent() throws ItemNotFoundException {
         String testLabel = "Label [%.1f %%]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DecimalType(10f / 3f));
         when(item.getStateAs(DecimalType.class)).thenReturn(new DecimalType(10f / 3f));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [3" + sep + "3 %]", label);
     }
 
     @Test
     public void getLabel_labelWithDate() throws ItemNotFoundException {
         String testLabel = "Label [%1$td.%1$tm.%1$tY]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DateTimeType("2011-06-01T00:00:00"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [01.06.2011]", label);
     }
 
     @Test
     public void getLabel_labelWithTime() throws ItemNotFoundException {
         String testLabel = "Label [%1$tT]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new DateTimeType("2011-06-01T15:30:59"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [15:30:59]", label);
     }
 
@@ -189,24 +179,17 @@ public class ItemUIRegistryImplTest {
 
     @Test
     public void getLabel_widgetWithoutLabel() throws ItemNotFoundException {
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Item", label);
     }
 
     @Test
     public void getLabel_labelFromUIProvider() throws ItemNotFoundException {
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
+
         ItemUIProvider provider = mock(ItemUIProvider.class);
         uiRegistry.addItemUIProvider(provider);
         when(provider.getLabel(anyString())).thenReturn("ProviderLabel");
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("ProviderLabel", label);
         uiRegistry.removeItemUIProvider(provider);
     }
@@ -214,93 +197,73 @@ public class ItemUIRegistryImplTest {
     @Test
     public void getLabel_labelForUndefinedStringItemState() throws ItemNotFoundException {
         String testLabel = "Label [%s]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(UnDefType.UNDEF);
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [-]", label);
     }
 
     @Test
     public void getLabel_labelForUndefinedIntegerItemState() throws ItemNotFoundException {
         String testLabel = "Label [%d]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(UnDefType.UNDEF);
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [-]", label);
     }
 
     @Test
     public void getLabel_labelForUndefinedDecimalItemState() throws ItemNotFoundException {
         String testLabel = "Label [%.2f]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(UnDefType.UNDEF);
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [-]", label);
     }
 
     @Test
     public void getLabel_labelForUndefinedDateItemState() throws ItemNotFoundException {
         String testLabel = "Label [%1$td.%1$tm.%1$tY]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(UnDefType.UNDEF);
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [-.-.-]", label);
     }
 
     @Test
     public void getLabel_itemNotFound() throws ItemNotFoundException {
         String testLabel = "Label [%s]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(w.eClass()).thenReturn(SitemapFactory.eINSTANCE.createText().eClass());
+
+        when(widget.getLabel()).thenReturn(testLabel);
+        when(widget.eClass()).thenReturn(SitemapFactory.eINSTANCE.createText().eClass());
         when(registry.getItem("Item")).thenThrow(new ItemNotFoundException("Item"));
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [-]", label);
     }
 
     @Test
     public void getLabel_labelWithFunctionValue() throws ItemNotFoundException {
         String testLabel = "Label [MAP(de.map):%s]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [State]", label);
     }
 
     @Test
     public void getLabel_groupLabelWithValue() throws ItemNotFoundException {
         String testLabel = "Label [%d]";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getState()).thenReturn(OnOffType.ON);
         when(item.getStateAs(DecimalType.class)).thenReturn(new DecimalType(5));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [5]", label);
     }
 
@@ -388,102 +351,84 @@ public class ItemUIRegistryImplTest {
     @Test
     public void getLabel_labelWithoutStateDescription() throws ItemNotFoundException {
         String testLabel = "Label";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getStateDescription()).thenReturn(null);
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label", label);
     }
 
     @Test
     public void getLabel_labelWithoutPatternInStateDescription() throws ItemNotFoundException {
         String testLabel = "Label";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
+
         StateDescription stateDescription = mock(StateDescription.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getStateDescription()).thenReturn(stateDescription);
         when(stateDescription.getPattern()).thenReturn(null);
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label", label);
     }
 
     @Test
     public void getLabel_labelWithPatternInStateDescription() throws ItemNotFoundException {
         String testLabel = "Label";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
+
         StateDescription stateDescription = mock(StateDescription.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getStateDescription()).thenReturn(stateDescription);
         when(stateDescription.getPattern()).thenReturn("%s");
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [State]", label);
     }
 
     @Test
     public void getLabel_labelWithEmptyPattern() throws ItemNotFoundException {
         String testLabel = "Label []";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
+
         StateDescription stateDescription = mock(StateDescription.class);
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getStateDescription()).thenReturn(stateDescription);
         when(stateDescription.getPattern()).thenReturn("%s");
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label", label);
     }
 
     @Test
     public void getLabel_labelWithMappedOption() throws ItemNotFoundException {
         String testLabel = "Label";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
+
         StateDescription stateDescription = mock(StateDescription.class);
         List<StateOption> options = new ArrayList<>();
         options.add(new StateOption("State0", "This is the state 0"));
         options.add(new StateOption("State1", "This is the state 1"));
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getStateDescription()).thenReturn(stateDescription);
         when(stateDescription.getPattern()).thenReturn("%s");
         when(stateDescription.getOptions()).thenReturn(options);
         when(item.getState()).thenReturn(new StringType("State1"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [This is the state 1]", label);
     }
 
     @Test
     public void getLabel_labelWithUnmappedOption() throws ItemNotFoundException {
         String testLabel = "Label";
-        Widget w = mock(Widget.class);
-        Item item = mock(Item.class);
+
         StateDescription stateDescription = mock(StateDescription.class);
         List<StateOption> options = new ArrayList<>();
         options.add(new StateOption("State0", "This is the state 0"));
         options.add(new StateOption("State1", "This is the state 1"));
-        when(w.getLabel()).thenReturn(testLabel);
-        when(w.getItem()).thenReturn("Item");
-        when(registry.getItem("Item")).thenReturn(item);
+        when(widget.getLabel()).thenReturn(testLabel);
         when(item.getStateDescription()).thenReturn(stateDescription);
         when(stateDescription.getPattern()).thenReturn("%s");
         when(stateDescription.getOptions()).thenReturn(options);
         when(item.getState()).thenReturn(new StringType("State"));
-        String label = uiRegistry.getLabel(w);
+        String label = uiRegistry.getLabel(widget);
         assertEquals("Label [State]", label);
     }
 
