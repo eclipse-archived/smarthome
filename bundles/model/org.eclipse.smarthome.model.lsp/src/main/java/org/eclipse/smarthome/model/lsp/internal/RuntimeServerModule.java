44909de6ef4a9db8628942c3a6332be691a9cb80
Simon Kaufmann <simon.kfm@googlemail.com>
2017-11-17 11:05:31 +0100

    Support path mapping in Language Server
    ...as server and clients can look at the same files while
    having them under different paths (e.g. due to SMB, SSH,...)
    
    fixes #4564
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/model/org.eclipse.smarthome.model.lsp/src/main/java/org/eclipse/smarthome/model/lsp/internal/RuntimeServerModule.java
+++ bundles/model/org.eclipse.smarthome.model.lsp/src/main/java/org/eclipse/smarthome/model/lsp/internal/RuntimeServerModule.java
@@ -10,6 +10,7 @@ package org.eclipse.smarthome.model.lsp.internal;
 import java.util.concurrent.ExecutorService;
 
 import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.smarthome.config.core.ConfigConstants;
 import org.eclipse.smarthome.model.script.ScriptServiceUtil;
 import org.eclipse.smarthome.model.script.engine.ScriptEngine;
 import org.eclipse.xtext.ide.ExecutorServiceProvider;
@@ -18,6 +19,7 @@ import org.eclipse.xtext.ide.server.IProjectDescriptionFactory;
 import org.eclipse.xtext.ide.server.IWorkspaceConfigFactory;
 import org.eclipse.xtext.ide.server.LanguageServerImpl;
 import org.eclipse.xtext.ide.server.ProjectWorkspaceConfigFactory;
+import org.eclipse.xtext.ide.server.UriExtensions;
 import org.eclipse.xtext.resource.IContainer;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.containers.ProjectDescriptionBasedContainerManager;
@@ -44,6 +46,7 @@ public class RuntimeServerModule extends AbstractModule {
     protected void configure() {
         binder().bind(ExecutorService.class).toProvider(ExecutorServiceProvider.class);
 
+        bind(UriExtensions.class).toInstance(new MappingUriExtensions(ConfigConstants.getConfigFolder()));
         bind(LanguageServer.class).to(LanguageServerImpl.class);
         bind(IResourceServiceProvider.Registry.class).toProvider(new RegistryProvider(scriptServiceUtil, scriptEngine));
         bind(IWorkspaceConfigFactory.class).to(ProjectWorkspaceConfigFactory.class);
