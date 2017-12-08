d2812e6d1696ee4ab7705405e3c5d4f7b3c1d994
Markus Rathgeb <maggu2810@gmail.com>
2017-12-08 15:34:27 +0100

    remove old license header
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/dto/RuleTemplateDTOMapper.java
+++ bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/dto/RuleTemplateDTOMapper.java
@@ -10,13 +10,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-/**
-* Copyright (c) 2015, 2017 by Bosch Software Innovations and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*/
 package org.eclipse.smarthome.automation.dto;
 
 import org.eclipse.smarthome.automation.template.RuleTemplate;
