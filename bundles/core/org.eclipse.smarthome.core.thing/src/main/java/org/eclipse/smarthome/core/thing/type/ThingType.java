7cace139ddd082079c9edd0e2b427c6e8b2cd51e
Simon Kaufmann <simon.kfm@googlemail.com>
2017-12-19 16:58:33 +0100

    remove TypeResolver from ThingType
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/type/ThingType.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/type/ThingType.java
@@ -23,6 +23,9 @@ import org.eclipse.smarthome.config.core.ConfigDescription;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
 /**
  * The {@link ThingType} describes a concrete type of a {@link Thing}.
@@ -265,6 +268,7 @@ public class ThingType extends AbstractDescriptionType {
      * @param channelUID channel UID
      * @return channel type UID or null if no matching channel type UID could be found in the thing type
      */
+    @SuppressWarnings("unchecked")
     public @Nullable ChannelTypeUID getChannelTypeUID(ChannelUID channelUID) {
         if (!channelUID.isInGroup()) {
             for (ChannelDefinition channelDefinition : this.getChannelDefinitions()) {
@@ -274,9 +278,21 @@ public class ThingType extends AbstractDescriptionType {
             }
         } else {
             List<ChannelGroupDefinition> channelGroupDefinitions = this.getChannelGroupDefinitions();
+
+            BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+            @SuppressWarnings("rawtypes")
+            ServiceReference ref = bundleContext.getServiceReference(ChannelTypeRegistry.class.getName());
+            ChannelTypeRegistry channelTypeRegistry = null;
+            if (ref != null) {
+                channelTypeRegistry = (ChannelTypeRegistry) bundleContext.getService(ref);
+            }
+
             for (ChannelGroupDefinition channelGroupDefinition : channelGroupDefinitions) {
                 if (channelGroupDefinition.getId().equals(channelUID.getGroupId())) {
-                    ChannelGroupType channelGroupType = TypeResolver.resolve(channelGroupDefinition.getTypeUID());
+                    ChannelGroupType channelGroupType = null;
+                    if (channelTypeRegistry != null) {
+                        channelGroupType = channelTypeRegistry.getChannelGroupType(channelGroupDefinition.getTypeUID());
+                    }
                     if (channelGroupType != null) {
                         for (ChannelDefinition channelDefinition : channelGroupType.getChannelDefinitions()) {
                             if (channelDefinition.getId().equals(channelUID.getIdWithoutGroup())) {
@@ -285,6 +301,10 @@ public class ThingType extends AbstractDescriptionType {
                         }
                     }
                 }
+            }
+
+            if (ref != null) {
+                bundleContext.ungetService(ref);
             }
         }
         return null;
