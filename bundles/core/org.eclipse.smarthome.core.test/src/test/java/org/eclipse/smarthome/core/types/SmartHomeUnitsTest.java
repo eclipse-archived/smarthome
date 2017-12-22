9676f34124b21c584862b43db445e9e0bc63b9c9
Henning Treu <henning.treu@telekom.de>
2017-12-22 12:07:21 +0100

    add license header & JavaDoc
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/types/SmartHomeUnitsTest.java
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/types/SmartHomeUnitsTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.eclipse.smarthome.core.types;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,6 +27,12 @@ import tec.uom.se.quantity.Quantities;
 import tec.uom.se.unit.MetricPrefix;
 import tec.uom.se.unit.Units;
 
+/**
+ * Test for the framework defined {@link SmartHomeUnits}.
+ *
+ * @author Henning Treu - initial contribution
+ *
+ */
 public class SmartHomeUnitsTest {
 
     private static final double DEFAULT_ERROR = 0.0000000000000001d;
--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/types/util/UnitUtils.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/types/util/UnitUtils.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.eclipse.smarthome.core.types.util;
 
 import javax.measure.Quantity;
@@ -11,6 +18,12 @@ import org.slf4j.LoggerFactory;
 
 import tec.uom.se.quantity.Quantities;
 
+/**
+ * A utility for parsing dimensions to interface classes of {@link Quantity} and parsing units from format strings.
+ *
+ * @author Henning Treu - initial contribution
+ *
+ */
 @NonNullByDefault
 public class UnitUtils {
 
