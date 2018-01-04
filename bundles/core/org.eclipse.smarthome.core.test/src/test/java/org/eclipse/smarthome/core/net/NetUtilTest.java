1f28ed42bf43e5d096de3ed08c72088d768daba0
Mark Herwege <mark.herwege@telenet.be>
2018-01-04 22:27:02 +0100

    Added tests for broadcast address calculation.
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/net/NetUtilTest.java
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/net/NetUtilTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014,2017 Contributors to the Eclipse Foundation
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -21,6 +21,7 @@ import org.junit.Test;
  * Tests for NetUtil class
  *
  * @author Stefan Triller - initial contribution
+ * @author Mark Herwege - added tests for broadcast address
  *
  */
 public class NetUtilTest {
@@ -29,27 +30,46 @@ public class NetUtilTest {
     public void testNetwork() {
         String network = NetUtil.getIpv4NetAddress("192.168.0.1", (short) 24);
         assertThat(network, is("192.168.0.0"));
+        String broadcast = NetUtil.getIpv4NetBroadcastAddress("192.168.0.1", (short) 24);
+        assertThat(broadcast, is("192.168.0.255"));
 
         network = NetUtil.getIpv4NetAddress("192.168.23.5", (short) 24);
         assertThat(network, is("192.168.23.0"));
+        broadcast = NetUtil.getIpv4NetBroadcastAddress("192.168.23.5", (short) 24);
+        assertThat(broadcast, is(" 192.168.23.255"));
 
         network = NetUtil.getIpv4NetAddress("172.16.42.23", (short) 16);
         assertThat(network, is("172.16.0.0"));
+        broadcast = NetUtil.getIpv4NetBroadcastAddress("172.16.42.23", (short) 16);
+        assertThat(broadcast, is("172.16.255.255"));
 
         network = NetUtil.getIpv4NetAddress("10.8.13.5", (short) 8);
         assertThat(network, is("10.0.0.0"));
+        broadcast = NetUtil.getIpv4NetBroadcastAddress("10.8.13.5", (short) 8);
+        assertThat(broadcast, is("10.255.255.255"));
 
         network = NetUtil.getIpv4NetAddress("192.168.5.8", (short) 23);
         assertThat(network, is("192.168.4.0"));
+        broadcast = NetUtil.getIpv4NetBroadcastAddress("192.168.5.8", (short) 23);
+        assertThat(broadcast, is("192.168.5.255"));
 
         network = NetUtil.getIpv4NetAddress("192.168.5.8", (short) 27);
         assertThat(network, is("192.168.5.0"));
+        broadcast = NetUtil.getIpv4NetBroadcastAddress("192.168.5.8", (short) 27);
+        assertThat(broadcast, is("192.168.5.31"));
 
         network = NetUtil.getIpv4NetAddress("192.168.5.8", (short) 29);
         assertThat(network, is("192.168.5.8"));
+        broadcast = NetUtil.getIpv4NetBroadcastAddress("192.168.5.8", (short) 29);
+        assertThat(broadcast, is("192.168.5.15"));
 
         try {
             network = NetUtil.getIpv4NetAddress("192.168.5.8", (short) 33);
+        } catch (IllegalArgumentException iae) {
+            assertThat(iae.getMessage(), is("Netmask '33' is out of bounds (1-32)"));
+        }
+        try {
+            network = NetUtil.getIpv4NetBroadcastAddress("192.168.5.8", (short) 33);
         } catch (IllegalArgumentException iae) {
             assertThat(iae.getMessage(), is("Netmask '33' is out of bounds (1-32)"));
         }
@@ -59,12 +79,27 @@ public class NetUtilTest {
             assertThat(iae.getMessage(), is("IP '192.168.58' is not a valid IPv4 address"));
         }
         try {
+            network = NetUtil.getIpv4NetBroadcastAddress("192.168.58", (short) 24);
+        } catch (IllegalArgumentException iae) {
+            assertThat(iae.getMessage(), is("IP '192.168.58' is not a valid IPv4 address"));
+        }
+        try {
             network = NetUtil.getIpv4NetAddress("SOME_TEXT", (short) 24);
         } catch (IllegalArgumentException iae) {
             assertThat(iae.getMessage(), is("IP 'SOME_TEXT' is not a valid IPv4 address"));
         }
         try {
+            network = NetUtil.getIpv4NetBroadcastAddress("192.168.58", (short) 24);
+        } catch (IllegalArgumentException iae) {
+            assertThat(iae.getMessage(), is("IP '192.168.58' is not a valid IPv4 address"));
+        }
+        try {
             network = NetUtil.getIpv4NetAddress("SOME_TEXT", (short) 42);
+        } catch (IllegalArgumentException iae) {
+            assertThat(iae.getMessage(), is("IP 'SOME_TEXT' is not a valid IPv4 address"));
+        }
+        try {
+            network = NetUtil.getIpv4NetBroadcastAddress("SOME_TEXT", (short) 42);
         } catch (IllegalArgumentException iae) {
             assertThat(iae.getMessage(), is("IP 'SOME_TEXT' is not a valid IPv4 address"));
         }
