c2b1a078c14724324cb8b703d507afc05a1e6057
adimova <a.dimova@prosyst.bg>
2018-01-08 15:40:13 +0200

    Rebase Provide serialization/deserialization of multiple configuration values. (#4642)
    Closes #4641
    
    Signed-off-by: Ana Dimova <a.dimova@prosyst.bg>

--- bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ConfigurationSerializer.java
+++ bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ConfigurationSerializer.java
@@ -10,15 +10,16 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-package org.eclipse.smarthome.automation.parser.gson.internal;
+package org.eclipse.smarthome.config.core;
 
 import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Set;
 
-import org.eclipse.smarthome.config.core.Configuration;
-
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
@@ -27,29 +28,44 @@ import com.google.gson.JsonSerializer;
  * configuration.properties object).
  *
  * @author Yordan Mihaylov - initial content
+ * @author Ana Dimova - provide serialization of multiple configuration values.
  */
 public class ConfigurationSerializer implements JsonSerializer<Configuration> {
 
+    @SuppressWarnings("unchecked")
     @Override
     public JsonElement serialize(Configuration src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject result = null;
         if (src != null) {
             Set<String> kyes = src.keySet();
-            result = new JsonObject();
             if (kyes.size() > 0) {
+                result = new JsonObject();
                 for (String propName : kyes) {
                     Object value = src.get(propName);
-                    if (value instanceof String) {
-                        result.addProperty(propName, (String) value);
-                    } else if (value instanceof Number) {
-                        result.addProperty(propName, (Number) value);
-                    } else if (value instanceof Boolean) {
-                        result.addProperty(propName, (Boolean) value);
+                    if (value instanceof List) {
+                        JsonArray array = new JsonArray();
+                        for (Object element : (List<Object>) value) {
+                            array.add(serializePrimitive(element));
+                        }
+                        result.add(propName, array);
+                    } else {
+                        result.add(propName, serializePrimitive(value));
                     }
                 }
             }
         }
         return result;
+    }
+
+    private JsonPrimitive serializePrimitive(Object primitive) {
+        if (primitive instanceof String) {
+            return new JsonPrimitive((String) primitive);
+        } else if (primitive instanceof Number) {
+            return new JsonPrimitive((Number) primitive);
+        } else if (primitive instanceof Boolean) {
+            return new JsonPrimitive((Boolean) primitive);
+        }
+        return null;
     }
 
 }
