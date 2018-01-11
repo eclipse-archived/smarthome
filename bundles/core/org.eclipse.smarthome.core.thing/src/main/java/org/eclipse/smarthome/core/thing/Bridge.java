a49ca7b1bcbce27371dd514a92b86a412c449930
Stefan Triller <stefan.triller@telekom.de>
2018-01-11 16:42:57 +0100

    fix duplicate words
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/Bridge.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/Bridge.java
@@ -36,7 +36,7 @@ public interface Bridge extends Thing {
     /**
      * Gets the bridge handler.
      *
-     * @return the handler the handler can be null for a Thing which is not initialized. Note that a Bridge is
+     * @return the handler which can be null for a Thing that is not initialized. Note that a Bridge is
      *         guaranteed to be initialized before its children. It is therefore safe to call getBridge().getHandler()
      *         for a subordinate Thing
      */
