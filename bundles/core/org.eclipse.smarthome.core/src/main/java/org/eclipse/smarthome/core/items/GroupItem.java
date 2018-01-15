a711ae11d2c1b54ebee153ef4f4cb82857a0a644
Henning Treu <henning.treu@gmail.com>
2018-01-15 13:57:57 +0100

    Fix compilation error, add missing import (#4926)
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/GroupItem.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/items/GroupItem.java
@@ -15,6 +15,7 @@ package org.eclipse.smarthome.core.items;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
