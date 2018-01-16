f6cd6bf97c60e1f4d618323a0c7ce4a6f8de16fe
Henning Treu <henning.treu@telekom.de>
2018-01-16 11:55:31 +0100

    Get item from registry instead of model repository
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- bundles/ui/org.eclipse.smarthome.ui/src/main/java/org/eclipse/smarthome/ui/internal/items/GenericItemUIProvider.java
+++ bundles/ui/org.eclipse.smarthome.ui/src/main/java/org/eclipse/smarthome/ui/internal/items/GenericItemUIProvider.java
@@ -14,9 +14,6 @@ package org.eclipse.smarthome.ui.internal.items;
 
 import org.eclipse.smarthome.core.items.Item;
 import org.eclipse.smarthome.core.items.ItemRegistry;
-import org.eclipse.smarthome.model.core.ModelRepository;
-import org.eclipse.smarthome.model.items.ItemModel;
-import org.eclipse.smarthome.model.items.ModelItem;
 import org.eclipse.smarthome.model.sitemap.Widget;
 import org.eclipse.smarthome.ui.items.ItemUIProvider;
 
@@ -26,16 +23,7 @@ import org.eclipse.smarthome.ui.items.ItemUIProvider;
  */
 public class GenericItemUIProvider implements ItemUIProvider {
 
-    private ModelRepository modelRepository = null;
     private ItemRegistry itemRegistry = null;
-
-    public void setModelRepository(ModelRepository modelRepository) {
-        this.modelRepository = modelRepository;
-    }
-
-    public void unsetModelRepository(ModelRepository modelRepository) {
-        this.modelRepository = null;
-    }
 
     public void setItemRegistry(ItemRegistry itemRegistry) {
         this.itemRegistry = itemRegistry;
@@ -47,8 +35,11 @@ public class GenericItemUIProvider implements ItemUIProvider {
 
     @Override
     public String getCategory(String itemName) {
-        ModelItem item = getItem(itemName);
-        return item != null ? item.getIcon() : null;
+        if (itemRegistry != null) {
+            Item item = itemRegistry.get(itemName);
+            return item != null ? item.getCategory() : null;
+        }
+        return null;
     }
 
     @Override
@@ -67,22 +58,6 @@ public class GenericItemUIProvider implements ItemUIProvider {
 
     @Override
     public Widget getDefaultWidget(Class<? extends org.eclipse.smarthome.core.items.Item> itemType, String itemName) {
-        return null;
-    }
-
-    public ModelItem getItem(String itemName) {
-        if (itemName != null && modelRepository != null) {
-            for (String modelName : modelRepository.getAllModelNamesOfType("items")) {
-                ItemModel model = (ItemModel) modelRepository.getModel(modelName);
-                if (model != null) {
-                    for (ModelItem item : model.getItems()) {
-                        if (itemName.equals(item.getName())) {
-                            return item;
-                        }
-                    }
-                }
-            }
-        }
         return null;
     }
 
