051ed32c045148adaedc5baa31d8911767eb912b
Chris Jackson <chris@cd-jackson.com>
2018-01-19 08:04:41 +0000

    Added THING_GONE enum for ThingStatusDetail (#4865)
    * Added GONE enum for ThingStatusDetail
    
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/ThingStatusDetail.java
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/ThingStatusDetail.java
@@ -16,6 +16,7 @@ package org.eclipse.smarthome.core.thing;
  * {@link ThingStatusDetail} defines possible status details of a {@link ThingStatusInfo}.
  *
  * @author Stefan Bußweiler - Initial contribution, added new status details
+ * @author Chris Jackson - Added GONE status
  */
 public enum ThingStatusDetail {
     NONE,
@@ -29,7 +30,12 @@ public enum ThingStatusDetail {
     BRIDGE_OFFLINE,
     FIRMWARE_UPDATING,
     DUTY_CYCLE,
-    BRIDGE_UNINITIALIZED;
+    BRIDGE_UNINITIALIZED,
+    /**
+     * Device has been removed. Used for example when the device has been removed from its bridge and
+     * the thing handler should be removed.
+     */
+    GONE;
 
     public static OnlineStatus ONLINE = new OnlineStatus();
 
