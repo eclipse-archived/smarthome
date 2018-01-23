678303e6116a36bc93b9d020effd2f11a260b3bf
Simon Kaufmann <simon.kfm@gmail.com>
2018-01-23 10:20:36 +0100

    handle indefinite results in JsonPath transformations (#4951)
    ...as JsonPath may return a List if multiple elements matched,
    even if they were correctly filtered down to a single element.
    
    fixes #4862
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- extensions/transform/org.eclipse.smarthome.transform.jsonpath/src/main/java/org/eclipse/smarthome/transform/jsonpath/internal/JSonPathTransformationService.java
+++ extensions/transform/org.eclipse.smarthome.transform.jsonpath/src/main/java/org/eclipse/smarthome/transform/jsonpath/internal/JSonPathTransformationService.java
@@ -12,6 +12,8 @@
  */
 package org.eclipse.smarthome.transform.jsonpath.internal;
 
+import java.util.List;
+
 import org.eclipse.smarthome.core.transform.TransformationException;
 import org.eclipse.smarthome.core.transform.TransformationService;
 import org.eclipse.smarthome.core.types.UnDefType;
@@ -55,13 +57,31 @@ public class JSonPathTransformationService implements TransformationService {
         try {
             Object transformationResult = JsonPath.read(source, jsonPathExpression);
             logger.debug("transformation resulted in '{}'", transformationResult);
-            return (transformationResult != null) ? transformationResult.toString() : UnDefType.NULL.toFullString();
+            if (transformationResult == null) {
+                return UnDefType.NULL.toFullString();
+            } else if (transformationResult instanceof List) {
+                return flattenList((List<?>) transformationResult);
+            } else {
+                return transformationResult.toString();
+            }
         } catch (PathNotFoundException e) {
             throw new TransformationException("Invalid path '" + jsonPathExpression + "' in '" + source + "'");
         } catch (InvalidPathException | InvalidJsonException e) {
             throw new TransformationException("An error occurred while transforming JSON expression.", e);
         }
 
+    }
+
+    private String flattenList(List<?> list) {
+        if (list.size() == 1) {
+            return list.get(0).toString();
+        }
+        if (list.size() > 1) {
+            logger.warn(
+                    "JsonPath expressions with more than one result are not allowed, please adapt your selector. Result: {}",
+                    list);
+        }
+        return UnDefType.NULL.toFullString();
     }
 
 }
