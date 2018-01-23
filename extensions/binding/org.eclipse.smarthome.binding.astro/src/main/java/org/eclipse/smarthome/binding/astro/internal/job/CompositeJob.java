c87830efe00e7a7ff7edddd56063e92b2b47ed52
Simon Kaufmann <simon.kfm@gmail.com>
2018-01-23 14:05:31 +0100

    made debug logs about scheduled jobs readable again (#4970)
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- extensions/binding/org.eclipse.smarthome.binding.astro/src/main/java/org/eclipse/smarthome/binding/astro/internal/job/CompositeJob.java
+++ extensions/binding/org.eclipse.smarthome.binding.astro/src/main/java/org/eclipse/smarthome/binding/astro/internal/job/CompositeJob.java
@@ -13,6 +13,7 @@
 package org.eclipse.smarthome.binding.astro.internal.job;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * {@link CompositeJob} comprises multiple {@link Job}s to be executed in order
@@ -26,7 +27,7 @@ public final class CompositeJob extends AbstractJob {
 
     /**
      * Constructor
-     * 
+     *
      * @param thingUID thing UID
      * @param jobs the jobs to execute
      * @throws IllegalArgumentException
@@ -52,5 +53,10 @@ public final class CompositeJob extends AbstractJob {
                 logger.warn("Job execution failed.", ex);
             }
         });
+    }
+
+    @Override
+    public String toString() {
+        return jobs.stream().map(j -> j.toString()).collect(Collectors.joining(" + "));
     }
 }
