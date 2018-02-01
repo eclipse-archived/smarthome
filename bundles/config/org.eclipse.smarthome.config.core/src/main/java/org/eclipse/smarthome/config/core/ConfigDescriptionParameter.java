276478c95377a3d92585bd277b96f2cc9f7945a2
Stefan Triller <stefan.triller@telekom.de>
2018-02-01 15:47:14 +0100

    Config parameter: limitToOptions Sync code with documentation
    Fixes #2844
    
    Since limitToOptions = false is currently broken anyway, see #4014 I think we can now sync the code with the documentation to at least starting to fix all related issues.
    
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ConfigDescriptionParameter.java
+++ bundles/config/org.eclipse.smarthome.config.core/src/main/java/org/eclipse/smarthome/config/core/ConfigDescriptionParameter.java
@@ -12,7 +12,6 @@
  */
 package org.eclipse.smarthome.config.core;
 
-import com.google.gson.annotations.SerializedName;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +20,8 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+
+import com.google.gson.annotations.SerializedName;
 
 /**
  * The {@link ConfigDescriptionParameter} class contains the description of a
@@ -95,7 +96,7 @@ public class ConfigDescriptionParameter {
     private List<ParameterOption> options = new ArrayList<ParameterOption>();
     private List<FilterCriteria> filterCriteria = new ArrayList<FilterCriteria>();
 
-    private boolean limitToOptions = false;
+    private boolean limitToOptions = true;
     private boolean advanced = false;
     private boolean verify = false;
 
