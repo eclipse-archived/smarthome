c3e1c23763ee543b7dfa21800971afe2295a4c1b
Simon Kaufmann <simon.kfm@gmail.com>
2018-02-06 16:12:08 +0100

    deprecated the Channel constructors (#5009)
    ...in order to point people to using the builder.
    
    Also, cleaned up all the tests which were still using the constructors.
    
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/core/org.eclipse.smarthome.core.thing.test/src/test/groovy/org/eclipse/smarthome/core/thing/util/ThingHelperTest.groovy
+++ bundles/core/org.eclipse.smarthome.core.thing.test/src/test/groovy/org/eclipse/smarthome/core/thing/util/ThingHelperTest.groovy
@@ -17,78 +17,84 @@ import static org.junit.Assert.*
 import static org.junit.matchers.JUnitMatchers.*
 
 import org.eclipse.smarthome.config.core.Configuration
-import org.eclipse.smarthome.core.thing.Channel
 import org.eclipse.smarthome.core.thing.ChannelUID
 import org.eclipse.smarthome.core.thing.Thing
 import org.eclipse.smarthome.core.thing.ThingTypeUID
 import org.eclipse.smarthome.core.thing.ThingUID
+import org.eclipse.smarthome.core.thing.binding.builder.ChannelBuilder
 import org.eclipse.smarthome.core.thing.binding.builder.ThingBuilder
 import org.junit.Test
 
 class ThingHelperTest {
 
-	@Test
-	void 'Two technical equal Thing instances are detected as "equal"'() {
-		Thing thingA = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
-				.withChannels(new Channel(new ChannelUID("binding:type:thingId:channel1"), "itemType"),
-				new Channel(new ChannelUID("binding:type:thingId:channel2"), "itemType"))
-				.withConfiguration(new Configuration())
-				.build()
-		thingA.getConfiguration().put("prop1", "value1")
-		thingA.getConfiguration().put("prop2", "value2")
+    @Test
+    void 'Two technical equal Thing instances are detected as "equal"'() {
+        Thing thingA = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
+                .withChannels(
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel1"), "itemType").build(),
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel2"), "itemType").build()
+                )
+                .withConfiguration(new Configuration())
+                .build()
+        thingA.getConfiguration().put("prop1", "value1")
+        thingA.getConfiguration().put("prop2", "value2")
 
-		assertTrue ThingHelper.equals(thingA, thingA)
+        assertTrue ThingHelper.equals(thingA, thingA)
 
-		Thing thingB = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
-				.withChannels(new Channel(new ChannelUID("binding:type:thingId:channel2"), "itemType"),
-				new Channel(new ChannelUID("binding:type:thingId:channel1"), "itemType"))
-				.withConfiguration(new Configuration())
-				.build()
-		thingB.getConfiguration().put("prop2", "value2")
-		thingB.getConfiguration().put("prop1", "value1")
+        Thing thingB = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
+                .withChannels(
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel2"), "itemType").build(),
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel1"), "itemType").build()
+                )
+                .withConfiguration(new Configuration())
+                .build()
+        thingB.getConfiguration().put("prop2", "value2")
+        thingB.getConfiguration().put("prop1", "value1")
 
-		assertTrue ThingHelper.equals(thingA, thingB)
-	}
+        assertTrue ThingHelper.equals(thingA, thingB)
+    }
 
-	@Test
-	void 'Two things are different after properties were modified'() {
-		Thing thingA = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
-				.withChannels(new Channel(new ChannelUID("binding:type:thingId:channel1"), "itemType"),
-				new Channel(new ChannelUID("binding:type:thingId:channel2"), "itemType"))
-				.withConfiguration(new Configuration())
-				.build()
-		thingA.getConfiguration().put("prop1", "value1")
+    @Test
+    void 'Two things are different after properties were modified'() {
+        Thing thingA = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
+                .withChannels(
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel1"), "itemType").build(),
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel2"), "itemType").build()
+                )
+                .withConfiguration(new Configuration())
+                .build()
+        thingA.getConfiguration().put("prop1", "value1")
 
-		Thing thingB = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
-				.withChannels(new Channel(new ChannelUID("binding:type:thingId:channel2"), "itemType"),
-				new Channel(new ChannelUID("binding:type:thingId:channel1"), "itemType"))
-				.withConfiguration(new Configuration())
-				.build()
-		thingB.getConfiguration().put("prop1", "value1")
+        Thing thingB = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
+                .withChannels(
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel2"), "itemType").build(),
+                ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel1"), "itemType").build()
+                )
+                .withConfiguration(new Configuration())
+                .build()
+        thingB.getConfiguration().put("prop1", "value1")
 
-		assertTrue ThingHelper.equals(thingA, thingB)
+        assertTrue ThingHelper.equals(thingA, thingB)
 
-		thingB.getConfiguration().put("prop3", "value3")
+        thingB.getConfiguration().put("prop3", "value3")
 
-		assertFalse ThingHelper.equals(thingA, thingB)
-	}
+        assertFalse ThingHelper.equals(thingA, thingB)
+    }
 
-	@Test
-	void 'Two things are different after channels were modified'() {
-		Thing thingA = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
-				.withConfiguration(new Configuration()).build()
+    @Test
+    void 'Two things are different after channels were modified'() {
+        Thing thingA = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
+                .withConfiguration(new Configuration()).build()
 
-		Thing thingB = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
-				.withConfiguration(new Configuration()).build()
+        Thing thingB = ThingBuilder.create(new ThingUID(new ThingTypeUID("binding:type"), "thingId"))
+                .withConfiguration(new Configuration()).build()
 
-		assertTrue ThingHelper.equals(thingA, thingB)
+        assertTrue ThingHelper.equals(thingA, thingB)
 
-		thingB.setChannels([
-			new Channel(new ChannelUID("binding:type:thingId:channel3"), "itemType3")
-		])
+        thingB.setChannels([ChannelBuilder.create(new ChannelUID("binding:type:thingId:channel3"), "itemType3").build()])
 
-		assertFalse ThingHelper.equals(thingA, thingB)
-	}
+        assertFalse ThingHelper.equals(thingA, thingB)
+    }
 
 
     @Test
@@ -127,10 +133,13 @@ class ThingHelperTest {
         ThingUID THING_UID = new ThingUID(THING_TYPE_UID, "test")
 
         Thing thing = ThingBuilder.create(THING_TYPE_UID, THING_UID).withChannels(
-                new Channel(new ChannelUID(THING_UID, "channel1"), ""),
-                new Channel(new ChannelUID(THING_UID, "channel2"), "")
+                ChannelBuilder.create(new ChannelUID(THING_UID, "channel1"), "").build(),
+                ChannelBuilder.create(new ChannelUID(THING_UID, "channel2"), "").build()
                 ).build();
 
-        ThingHelper.addChannelsToThing(thing, [new Channel(new ChannelUID(THING_UID, "channel2"), ""), new Channel(new ChannelUID(THING_UID, "channel3"), "")])
+        ThingHelper.addChannelsToThing(thing, [
+            ChannelBuilder.create(new ChannelUID(THING_UID, "channel2"), "").build(),
+            ChannelBuilder.create(new ChannelUID(THING_UID, "channel3"), "").build()
+        ])
     }
 }
--- bundles/core/org.eclipse.smarthome.core.thing.test/src/test/java/org/eclipse/smarthome/core/thing/internal/ChannelItemProviderTest.java
+++ bundles/core/org.eclipse.smarthome.core.thing.test/src/test/java/org/eclipse/smarthome/core/thing/internal/ChannelItemProviderTest.java
@@ -30,9 +30,9 @@ import org.eclipse.smarthome.core.items.Item;
 import org.eclipse.smarthome.core.items.ItemFactory;
 import org.eclipse.smarthome.core.items.ItemRegistry;
 import org.eclipse.smarthome.core.library.items.NumberItem;
-import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.ThingRegistry;
+import org.eclipse.smarthome.core.thing.binding.builder.ChannelBuilder;
 import org.eclipse.smarthome.core.thing.link.ItemChannelLink;
 import org.eclipse.smarthome.core.thing.link.ItemChannelLinkRegistry;
 import org.junit.Before;
@@ -76,7 +76,8 @@ public class ChannelItemProviderTest {
         props.put("initialDelay", "false");
         provider.activate(props);
 
-        when(thingRegistry.getChannel(same(CHANNEL_UID))).thenReturn(new Channel(CHANNEL_UID, "Number"));
+        when(thingRegistry.getChannel(same(CHANNEL_UID)))
+                .thenReturn(ChannelBuilder.create(CHANNEL_UID, "Number").build());
         when(itemFactory.createItem("Number", ITEM_NAME)).thenReturn(ITEM);
         when(localeProvider.getLocale()).thenReturn(Locale.ENGLISH);
     }
