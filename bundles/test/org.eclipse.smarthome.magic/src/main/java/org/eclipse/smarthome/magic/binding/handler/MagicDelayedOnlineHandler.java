5f66d47eaf1c9617da83f5cf703000460870391b
Stefan Triller <stefan.triller@telekom.de>
2018-02-07 11:52:15 +0100

    Possibility to set thing to online or offline after a given delay
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- bundles/test/org.eclipse.smarthome.magic/src/main/java/org/eclipse/smarthome/magic/binding/handler/MagicDelayedOnlineHandler.java
+++ bundles/test/org.eclipse.smarthome.magic/src/main/java/org/eclipse/smarthome/magic/binding/handler/MagicDelayedOnlineHandler.java
@@ -1,8 +1,21 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.smarthome.magic.binding.handler;
 
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.library.types.DecimalType;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
@@ -32,7 +45,14 @@ public class MagicDelayedOnlineHandler extends BaseThingHandler {
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-        // empty because it is a dummy thing
+        if (channelUID.getId().equals("number")) {
+            if (command instanceof DecimalType) {
+                DecimalType cmd = (DecimalType) command;
+                int cmdInt = cmd.intValue();
+                ThingStatus status = cmdInt > 0 ? ThingStatus.ONLINE : ThingStatus.OFFLINE;
+                int waitTime = Math.abs(cmd.intValue());
+                scheduler.schedule(() -> updateStatus(status), waitTime, TimeUnit.SECONDS);
+            }
+        }
     }
-
 }
