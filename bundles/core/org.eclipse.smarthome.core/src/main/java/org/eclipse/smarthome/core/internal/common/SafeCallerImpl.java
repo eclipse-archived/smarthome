acee7ba4695ddcbc5a68218fc1987cee0edf7c39
Simon Kaufmann <simon.kfm@googlemail.com>
2018-02-09 13:20:13 +0100

    drop SafeCaller.create(Class) method signature
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/internal/common/SafeCallerImpl.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/internal/common/SafeCallerImpl.java
@@ -12,10 +12,7 @@
  */
 package org.eclipse.smarthome.core.internal.common;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -74,27 +71,12 @@ public class SafeCallerImpl implements SafeCaller {
     }
 
     @Override
-    public <T> SafeCallerBuilder<T> create(T target, Class<T> interfaceType) {
+    public <T extends I, I> SafeCallerBuilder<T> create(T target, Class<I> interfaceType) {
         return new SafeCallerBuilderImpl<T>(target, new Class<?>[] { interfaceType }, manager);
-    }
-
-    @Override
-    public <T> SafeCallerBuilder<T> create(T target) {
-        return new SafeCallerBuilderImpl<T>(target, getAllInterfaces(target), manager);
     }
 
     protected ExecutorService getScheduler() {
         return ThreadPoolManager.getPool(SAFE_CALL_POOL_NAME);
-    }
-
-    private static <T> Class<?>[] getAllInterfaces(T target) {
-        Set<Class<?>> ret = new HashSet<>();
-        Class<?> clazz = target.getClass();
-        while (clazz != null) {
-            ret.addAll(Arrays.asList(clazz.getInterfaces()));
-            clazz = clazz.getSuperclass();
-        }
-        return ret.toArray(new Class<?>[ret.size()]);
     }
 
 }
