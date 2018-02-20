ebe8ade19cb627db39ba4ace59b428b25d6638c5
Simon Kaufmann <simon.kfm@googlemail.com>
2018-02-20 11:17:26 +0100

    fixed RuleRegistryImpl
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/automation/org.eclipse.smarthome.automation.core/src/main/java/org/eclipse/smarthome/automation/core/internal/RuleRegistryImpl.java
+++ bundles/automation/org.eclipse.smarthome.automation.core/src/main/java/org/eclipse/smarthome/automation/core/internal/RuleRegistryImpl.java
@@ -18,7 +18,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -538,15 +537,6 @@ public class RuleRegistryImpl extends AbstractRegistry<Rule, String, RuleProvide
             Rule rule = get(rUID);
             updateRuleByTemplate(getProvider(rule), rule);
         }
-    }
-
-    private Provider<Rule> getProvider(Rule rule) {
-        for (Entry<Provider<Rule>, Collection<Rule>> entry : elementMap.entrySet()) {
-            if (entry.getValue().contains(rule)) {
-                return entry.getKey();
-            }
-        }
-        return null;
     }
 
     @Override
