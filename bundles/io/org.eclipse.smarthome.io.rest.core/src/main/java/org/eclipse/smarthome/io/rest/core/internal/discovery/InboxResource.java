c2b3c11b0d5d7a4feeab76a91b7d86ed449373c6
Maw-Jung <maw.albrecht.jung@gmail.com>
2018-03-11 21:07:42 +0100

    [5198] prevent Responses to be interpreted as xml (#5199)
    * [5198] prevent Responses to be interpreted as xml
    
    The ResponseBuilder "guesses" the best mimetype for the Response by
    interpreting the accept headers from the Request. When the client
    sets no preferation and xml is valid, the response will be interpreted
    as xml. If FF doesn't find the xml root element in this case, which happens
    when the Response is empty, a console error is printed out.
    
    Bug: https://github.com/eclipse/smarthome/issues/5198
    Signed-off-by: Marcel Walwei <maw.albrecht.jung@gmail.com>

--- bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/internal/discovery/InboxResource.java
+++ bundles/io/org.eclipse.smarthome.io.rest.core/src/main/java/org/eclipse/smarthome/io/rest/core/internal/discovery/InboxResource.java
@@ -108,7 +108,7 @@ public class InboxResource implements RESTResource {
             return JSONResponse.createErrorResponse(Status.CONFLICT, "No binding found that can create the thing");
         }
 
-        return Response.ok().build();
+        return Response.ok(null, MediaType.TEXT_PLAIN).build();
     }
 
     @DELETE
@@ -118,7 +118,7 @@ public class InboxResource implements RESTResource {
             @ApiResponse(code = 404, message = "Discovery result not found in the inbox.") })
     public Response delete(@PathParam("thingUID") @ApiParam(value = "thingUID", required = true) String thingUID) {
         if (inbox.remove(new ThingUID(thingUID))) {
-            return Response.ok().build();
+            return Response.ok(null, MediaType.TEXT_PLAIN).build();
         } else {
             return JSONResponse.createErrorResponse(Status.NOT_FOUND, "Thing not found in inbox");
         }
@@ -139,7 +139,7 @@ public class InboxResource implements RESTResource {
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public Response ignore(@PathParam("thingUID") @ApiParam(value = "thingUID", required = true) String thingUID) {
         inbox.setFlag(new ThingUID(thingUID), DiscoveryResultFlag.IGNORED);
-        return Response.ok().build();
+        return Response.ok(null, MediaType.TEXT_PLAIN).build();
     }
 
     @POST
@@ -148,7 +148,7 @@ public class InboxResource implements RESTResource {
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public Response unignore(@PathParam("thingUID") @ApiParam(value = "thingUID", required = true) String thingUID) {
         inbox.setFlag(new ThingUID(thingUID), DiscoveryResultFlag.NEW);
-        return Response.ok().build();
+        return Response.ok(null, MediaType.TEXT_PLAIN).build();
     }
 
     @Override
