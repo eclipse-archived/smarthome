771b049ebe2becad5e4c2652ba63afcbd6a92c6c
ivivanov-bg <ivivanov.bg@gmail.com>
2018-03-22 14:29:37 +0200

    Add some imperial units (#5284)
    Signed-off-by: Ivaylo Ivanov <ivivanov.bg@gmail.com>

--- bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/types/SmartHomeUnitsTest.java
+++ bundles/core/org.eclipse.smarthome.core.test/src/test/java/org/eclipse/smarthome/core/types/SmartHomeUnitsTest.java
@@ -89,7 +89,7 @@ public class SmartHomeUnitsTest {
 
     @Test
     public void testKelvin2Fahrenheit2() {
-        Quantity<Temperature> kelvin = Quantities.getQuantity(new BigDecimal(300), SmartHomeUnits.KELVIN);
+        Quantity<Temperature> kelvin = Quantities.getQuantity(new BigDecimal("300"), SmartHomeUnits.KELVIN);
 
         assertThat(kelvin.to(ImperialUnits.FAHRENHEIT),
                 is(Quantities.getQuantity(new BigDecimal("80.33"), ImperialUnits.FAHRENHEIT)));
@@ -97,7 +97,7 @@ public class SmartHomeUnitsTest {
 
     @Test
     public void testFahrenheit2Kelvin() {
-        Quantity<Temperature> fahrenheit = Quantities.getQuantity(new BigDecimal(100), ImperialUnits.FAHRENHEIT);
+        Quantity<Temperature> fahrenheit = Quantities.getQuantity(new BigDecimal("100"), ImperialUnits.FAHRENHEIT);
 
         Quantity<Temperature> kelvin = fahrenheit.to(SmartHomeUnits.KELVIN);
         assertThat(kelvin.getUnit(), is(SmartHomeUnits.KELVIN));
@@ -106,7 +106,7 @@ public class SmartHomeUnitsTest {
 
     @Test
     public void testKmh2Mih() {
-        Quantity<Speed> kmh = Quantities.getQuantity(new BigDecimal(10), SIUnits.KILOMETRE_PER_HOUR);
+        Quantity<Speed> kmh = Quantities.getQuantity(BigDecimal.TEN, SIUnits.KILOMETRE_PER_HOUR);
 
         Quantity<Speed> mph = kmh.to(ImperialUnits.MILES_PER_HOUR);
         assertThat(mph.getUnit(), is(ImperialUnits.MILES_PER_HOUR));
@@ -115,16 +115,34 @@ public class SmartHomeUnitsTest {
 
     @Test
     public void testCm2In() {
-        Quantity<Length> cm = Quantities.getQuantity(new BigDecimal(10), MetricPrefix.CENTI(SIUnits.METRE));
+        Quantity<Length> cm = Quantities.getQuantity(BigDecimal.TEN, MetricPrefix.CENTI(SIUnits.METRE));
 
         Quantity<Length> in = cm.to(ImperialUnits.INCH);
         assertThat(in.getUnit(), is(ImperialUnits.INCH));
         assertThat(in.getValue().doubleValue(), is(closeTo(3.93700787401574803d, DEFAULT_ERROR)));
     }
+    
+    @Test
+    public void testM2Ft() {
+        Quantity<Length> cm = Quantities.getQuantity(new BigDecimal("30"), MetricPrefix.CENTI(SIUnits.METRE));
+
+        Quantity<Length> foot = cm.to(ImperialUnits.FOOT);
+        assertThat(foot.getUnit(), is(ImperialUnits.FOOT));
+        assertThat(foot.getValue().doubleValue(), is(closeTo(0.9842519685039369d, DEFAULT_ERROR)));
+    }
+    
+    @Test
+    public void testM2Yd() {
+        Quantity<Length> m = Quantities.getQuantity(BigDecimal.ONE, SIUnits.METRE);
+
+        Quantity<Length> yard = m.to(ImperialUnits.YARD);
+        assertThat(yard.getUnit(), is(ImperialUnits.YARD));
+        assertThat(yard.getValue().doubleValue(), is(closeTo(1.0936132983377076d, DEFAULT_ERROR)));
+    }
 
     @Test
     public void testM2Ml() {
-        Quantity<Length> km = Quantities.getQuantity(new BigDecimal(10), MetricPrefix.KILO(SIUnits.METRE));
+        Quantity<Length> km = Quantities.getQuantity(BigDecimal.TEN, MetricPrefix.KILO(SIUnits.METRE));
 
         Quantity<Length> mile = km.to(ImperialUnits.MILE);
         assertThat(mile.getUnit(), is(ImperialUnits.MILE));
@@ -153,8 +171,8 @@ public class SmartHomeUnitsTest {
     public void test_one_UnitSymbol() {
         assertThat(SmartHomeUnits.ONE.getSymbol(), is(""));
 
-        Quantity<Dimensionless> one1 = Quantities.getQuantity(new BigDecimal(1), SmartHomeUnits.ONE);
-        Quantity<Dimensionless> one2 = Quantities.getQuantity(new BigDecimal(1), SmartHomeUnits.ONE);
+        Quantity<Dimensionless> one1 = Quantities.getQuantity(BigDecimal.ONE, SmartHomeUnits.ONE);
+        Quantity<Dimensionless> one2 = Quantities.getQuantity(BigDecimal.ONE, SmartHomeUnits.ONE);
 
         assertThat(one1.add(one2).toString(), is("2 one"));
     }
