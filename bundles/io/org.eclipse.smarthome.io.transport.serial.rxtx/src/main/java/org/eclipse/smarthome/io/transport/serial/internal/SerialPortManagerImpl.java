f725e2b3a3331376072af0e15c332fccf89664e5
Markus Rathgeb <maggu2810@gmail.com>
2018-03-25 13:09:14 +0200

    serial: add serial transport API and implementations
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

---
+++ bundles/io/org.eclipse.smarthome.io.transport.serial.rxtx/src/main/java/org/eclipse/smarthome/io/transport/serial/internal/SerialPortManagerImpl.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.io.transport.serial.internal;
+
+import java.util.Enumeration;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.io.transport.serial.SerialPortIdentifier;
+import org.eclipse.smarthome.io.transport.serial.SerialPortManager;
+import org.osgi.service.component.annotations.Component;
+
+import gnu.io.CommPortIdentifier;
+
+/**
+ * Specific serial port manager implementation.
+ *
+ * @author Markus Rathgeb - Initial contribution
+ */
+@NonNullByDefault
+@Component
+public class SerialPortManagerImpl implements SerialPortManager {
+
+    private static class SplitIteratorForEnumeration<T> extends Spliterators.AbstractSpliterator<T> {
+        private final Enumeration<T> e;
+
+        public SplitIteratorForEnumeration(final Enumeration<T> e) {
+            super(Long.MAX_VALUE, Spliterator.ORDERED);
+            this.e = e;
+        }
+
+        @Override
+        @NonNullByDefault({})
+        public boolean tryAdvance(Consumer<? super T> action) {
+            if (e.hasMoreElements()) {
+                action.accept(e.nextElement());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        @NonNullByDefault({})
+        public void forEachRemaining(Consumer<? super T> action) {
+            while (e.hasMoreElements()) {
+                action.accept(e.nextElement());
+            }
+        }
+    }
+
+    @Override
+    public Stream<SerialPortIdentifier> getIdentifiers() {
+        @SuppressWarnings("unchecked")
+        final Enumeration<CommPortIdentifier> ids = CommPortIdentifier.getPortIdentifiers();
+        return StreamSupport.stream(new SplitIteratorForEnumeration<>(ids), false)
+                .filter(id -> id.getPortType() == CommPortIdentifier.PORT_SERIAL)
+                .map(sid -> new SerialPortIdentifierImpl(sid));
+    }
+}
