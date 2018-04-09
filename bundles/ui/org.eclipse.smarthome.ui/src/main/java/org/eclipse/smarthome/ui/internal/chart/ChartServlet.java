096efc63e89b220bdedb20a7200c913758cea064
lolodomo <lg.hc@free.fr>
2018-04-09 08:25:30 +0200

    Chart servlet: define configurationPid (#5382)
    Fixes issue #5376
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/ui/org.eclipse.smarthome.ui/src/main/java/org/eclipse/smarthome/ui/internal/chart/ChartServlet.java
+++ bundles/ui/org.eclipse.smarthome.ui/src/main/java/org/eclipse/smarthome/ui/internal/chart/ChartServlet.java
@@ -67,9 +67,9 @@ import org.slf4j.LoggerFactory;
  * @author Holger Reichert - Support for themes, DPI, legend hiding
  *
  */
-@Component(immediate = true, service = Servlet.class, property = { "service.pid=org.eclipse.smarthome.chart",
-        "service.config.description.uri=system:chart", "service.config.label=Charts",
-        "service.config.category=system" })
+@Component(immediate = true, service = Servlet.class, configurationPid = "org.eclipse.smarthome.chart", property = {
+        "service.pid=org.eclipse.smarthome.chart", "service.config.description.uri=system:chart",
+        "service.config.label=Charts", "service.config.category=system" })
 public class ChartServlet extends HttpServlet {
 
     private static final long serialVersionUID = 7700873790924746422L;
