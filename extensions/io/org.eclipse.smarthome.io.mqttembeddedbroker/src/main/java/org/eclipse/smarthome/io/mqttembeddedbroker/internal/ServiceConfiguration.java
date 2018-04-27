592b52c8cfa451c7b3a9e05392e9e79087ce4335
David Gräff <david.graeff@web.de>
2018-04-27 07:50:20 +0200

    MqttEmbeddedBroker service (#3910)
    This io/extension can add a Mqtt broker connection and start an embedded broker (Moquette).
    It allows secure connection via a provided and configured certificate or creates
    a self-signed certificate and key on the first start otherwise.
    
    Signed-off-by: David Graeff <david.graeff@web.de>

---
+++ extensions/io/org.eclipse.smarthome.io.mqttembeddedbroker/src/main/java/org/eclipse/smarthome/io/mqttembeddedbroker/internal/ServiceConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.io.mqttembeddedbroker.internal;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * Configuration of the {@link EmbeddedBrokerService}.
+ *
+ * @author David Graeff - Initial contribution
+ */
+public class ServiceConfiguration {
+    public @Nullable Integer port;
+    public @NonNull Boolean secure = false;
+    public @NonNull String persistenceFile = "mqttembedded.bin";
+
+    public @Nullable String username;
+    public @Nullable String password;
+}
