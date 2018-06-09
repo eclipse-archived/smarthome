db441975d22840c0638f5df4164b0e6247b46d51
Simon Kaufmann <simon.kfm@gmail.com>
2018-06-09 19:32:55 +0200

    Only write tags if needed (#5704)
    ...i.e. if no other metadata provider already provided them.
    
    fixes #5670
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/internal/items/ItemRegistryImpl.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/internal/items/ItemRegistryImpl.java
@@ -477,7 +477,9 @@ public class ItemRegistryImpl extends AbstractRegistry<Item, String, ItemProvide
         try {
             SortedSet<String> itemTags = readTags(itemName);
             boolean ret = itemTags.addAll(tags);
-            writeTags(itemName, itemTags);
+            if (ret) {
+                writeTags(itemName, itemTags);
+            }
             return ret;
         } finally {
             tagLock.writeLock().unlock();
