30a6ee8904e8642d2562a2f9a124450e157e7e5c
Martin van Wingerden <martinvw@users.noreply.github.com>
2018-07-04 07:46:25 +0200

    Created a builder like structure to improve the code using the HttpUtil (#5658)
    * Created a builder like structure to improve the code using the HttpUtil
    
    Fixes #4257
    
    Also-by: Wouter Born <eclipse@maindrain.net>
    Signed-off-by: Martin van Wingerden <martin@martinvw.nl>

--- bundles/io/org.eclipse.smarthome.io.net.test/src/test/java/org/eclipse/smarthome/io/net/http/HttpUtilTest.java
+++ bundles/io/org.eclipse.smarthome.io.net.test/src/test/java/org/eclipse/smarthome/io/net/http/HttpUtilTest.java
@@ -13,14 +13,53 @@
 package org.eclipse.smarthome.io.net.http;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
- * @author Thomas Eichstaedt-Engelen
+ * Tests for the HttpUtil
+ *
+ * @author Thomas Eichstaedt-Engelen - Initial contribution
+ * @author Martin van Wingerden - Added tests based on HttpClientFactory
  */
-public class HttpUtilTest {
+public class HttpUtilTest extends BaseHttpUtilTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void baseTest() throws Exception {
+        mockResponse(HttpStatus.OK_200);
+
+        String result = HttpUtil.executeUrl("GET", URL, 500);
+
+        assertEquals("Some content", result);
+
+        verify(httpClientMock).newRequest(URL);
+        verify(requestMock).method(HttpMethod.GET);
+        verify(requestMock).timeout(500, TimeUnit.MILLISECONDS);
+        verify(requestMock).send();
+    }
+
+    @Test
+    public void testAuthentication() throws Exception {
+        when(httpClientMock.newRequest("http://john:doe@example.org/")).thenReturn(requestMock);
+        mockResponse(HttpStatus.OK_200);
+
+        String result = HttpUtil.executeUrl("GET", "http://john:doe@example.org/", 500);
+
+        assertEquals("Some content", result);
+
+        verify(requestMock).header(HttpHeader.AUTHORIZATION, "Basic am9objpkb2U=");
+    }
 
     @Test
     public void testCreateHttpMethod() {
@@ -30,4 +69,19 @@ public class HttpUtilTest {
         assertEquals(HttpMethod.DELETE, HttpUtil.createHttpMethod("DELETE"));
     }
 
+    @Test
+    public void testCreateHttpMethodForUnsupportedFake() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Given HTTP Method 'FAKE' is unknown");
+
+        HttpUtil.createHttpMethod("FAKE");
+    }
+
+    @Test
+    public void testCreateHttpMethodForUnsupportedTrace() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Given HTTP Method 'TRACE' is unknown");
+
+        HttpUtil.createHttpMethod("TRACE");
+    }
 }
