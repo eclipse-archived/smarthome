b5f6f0ec9318d47e5348b01cfe15fe19589ab462
Stefan Triller <stefan.triller@telekom.de>
2018-07-05 18:52:54 +0200

    Implement new SystemChannelTypes (#5855)
    * Implement new SystemChannelTypes
    
    Also includes a ChannelTypeBuilder for more readability on creating
    ChannelTypes.
    
    Fixes #3756
    
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

---
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/type/IChannelTypeBuilder.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.core.thing.type;
+
+import java.net.URI;
+import java.util.Collection;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Interface for ChannelTypeBuilder
+ *
+ * @author Stefan Triller - Initial contribution
+ *
+ */
+@NonNullByDefault
+interface IChannelTypeBuilder<T extends IChannelTypeBuilder<T>> {
+    /**
+     * Specify whether this is an advanced channel, default is false
+     *
+     * @param advanced true is this is an advanced {@link ChannelType}
+     * @return this Builder
+     */
+    T isAdvanced(boolean advanced);
+
+    /**
+     * Sets the Description for the ChannelType
+     *
+     * @param description StateDescription for the ChannelType
+     * @return this Builder
+     */
+    T withDescription(String description);
+
+    /**
+     * Sets the Category for the ChannelType
+     *
+     * @param category Category for the ChannelType
+     * @return this Builder
+     */
+    T withCategory(String category);
+
+    /**
+     * Adds a tag to the ChannelType
+     *
+     * @param tag Tag to be added to the ChannelType
+     * @return this Builder
+     */
+    T withTag(String tag);
+
+    /**
+     * Sets the StateDescription for the ChannelType
+     *
+     * @param tags Collection of tags to be added to the ChannelType
+     * @return this Builder
+     */
+    T withTags(Collection<String> tags);
+
+    /**
+     * Sets the ConfigDescriptionURI for the ChannelType
+     *
+     * @param configDescriptionURI URI that references the ConfigDescription of the ChannelType
+     * @return this Builder
+     */
+    T withConfigDescriptionURI(URI configDescriptionURI);
+
+    /**
+     * Build the ChannelType with the given values
+     *
+     * @return the created ChannelType
+     */
+    ChannelType build();
+}
---
+++ bundles/core/org.eclipse.smarthome.core.thing/src/main/java/org/eclipse/smarthome/core/thing/type/TriggerChannelTypeBuilderImpl.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.core.thing.type;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.types.EventDescription;
+
+/**
+ * Implementation of {@link TriggerChannelTypeBuilder} to build {@link ChannelType}s of kind TRIGGER
+ *
+ * @author Stefan Triller - Initial contribution
+ *
+ */
+@NonNullByDefault
+class TriggerChannelTypeBuilderImpl extends AbstractChannelTypeBuilder<TriggerChannelTypeBuilder>
+        implements TriggerChannelTypeBuilder {
+
+    private @Nullable EventDescription eventDescription;
+
+    TriggerChannelTypeBuilderImpl(ChannelTypeUID channelTypeUID, String label) {
+        super(channelTypeUID, label);
+    }
+
+    @Override
+    public ChannelType build() {
+        return new ChannelType(channelTypeUID, advanced, null, ChannelKind.TRIGGER, label, description, category,
+                tags.isEmpty() ? null : tags, null, eventDescription, configDescriptionURI);
+    }
+
+    @Override
+    public TriggerChannelTypeBuilder withEventDescription(EventDescription eventDescription) {
+        this.eventDescription = eventDescription;
+        return this;
+    }
+}
