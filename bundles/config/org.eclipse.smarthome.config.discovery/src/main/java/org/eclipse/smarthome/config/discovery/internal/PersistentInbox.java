d8c4b2302fb98dbf67f40e9a08a6d035cce7324b
Jan <Jan.Hendriks@telekom.de>
2018-07-25 10:48:54 +0200

    NPE in PersistentInbox provide error info (#5920)
    Addresses a NPE in PersistentInbox and provide additional information in case of this NPE seemingly caused by a constraint violation.
    
    Signed-off-by: Jan Hendriks <Jan.Hendriks@telekom.de>

--- bundles/config/org.eclipse.smarthome.config.discovery/src/main/java/org/eclipse/smarthome/config/discovery/internal/PersistentInbox.java
+++ bundles/config/org.eclipse.smarthome.config.discovery/src/main/java/org/eclipse/smarthome/config/discovery/internal/PersistentInbox.java
@@ -300,10 +300,21 @@ public final class PersistentInbox implements Inbox, DiscoveryListener, ThingReg
     @Override
     @NonNullByDefault({})
     public Stream<DiscoveryResult> stream() {
-        if (this.discoveryResultStorage == null || this.discoveryResultStorage.getValues() == null) {
+        final Storage<DiscoveryResult> discoveryResultStorage = this.discoveryResultStorage;
+        if (discoveryResultStorage == null) {
+            final ScheduledFuture<?> timeToLiveChecker = this.timeToLiveChecker;
+            logger.error("The OSGi lifecycle has been violated (storage: {}, ttl checker cancelled: {}).",
+                    this.discoveryResultStorage == null ? "null" : this.discoveryResultStorage, timeToLiveChecker == null ? "null" : timeToLiveChecker.isCancelled());
             return Stream.empty();
         }
-        return this.discoveryResultStorage.getValues().stream().filter(Objects::nonNull);
+        final Collection<@Nullable DiscoveryResult> values = discoveryResultStorage.getValues();
+        if (values == null) {
+            logger.warn(
+                    "The storage service violates the nullness requirements (get values must not return null) (storage class: {}).",
+                    discoveryResultStorage.getClass());
+            return Stream.empty();
+        }
+        return values.stream().filter(Objects::nonNull);
     }
 
     @Override
