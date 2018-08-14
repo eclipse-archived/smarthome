7bbfdfcd33870538762cb7ba3b3a90c122064af5
Henning Treu <henning.treu@gmail.com>
2018-08-14 16:54:58 +0200

    Paper UI: refactor config description to component API (#6045)
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/configuration/directive.configDescription.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/configuration/directive.configDescription.js
@@ -1,18 +1,9 @@
-angular.module('PaperUI.directive.configDescription', []) //
-.directive('configDescription', function() {
+;
+(function() {
+    'use strict';
 
-    var controller = function($scope) {
-        $scope.getName = function(parameter, option) {
-            if (!option) {
-                return undefined;
-            }
-            return option.name ? option.name : parameter.context == 'thing' ? option.UID : parameter.context == 'channel' ? option.id : undefined;
-        }
-    }
-
-    return {
-        restrict : 'E',
-        scope : {
+    angular.module('PaperUI.directive.configDescription', []).component('configDescription', {
+        bindings : {
             configuration : '=',
             parameters : '=',
             expertMode : '=?',
@@ -20,6 +11,20 @@ angular.module('PaperUI.directive.configDescription', []) //
             form : '=?'
         },
         templateUrl : 'partials/configuration/directive.configDescription.html',
-        controller : controller
+        controller : ConfigDescriptionController
+    });
+
+    function ConfigDescriptionController() {
+        var ctrl = this;
+
+        this.getName = getName;
+
+        function getName(parameter, option) {
+            if (!option) {
+                return undefined;
+            }
+            return option.name ? option.name : parameter.context == 'thing' ? option.UID : parameter.context == 'channel' ? option.id : undefined;
+        }
     }
-});
+
+})()
