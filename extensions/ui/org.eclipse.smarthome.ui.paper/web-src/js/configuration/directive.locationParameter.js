40aab29d4b691f6876dde5acde2bd178ed287b71
Henning Treu <henning.treu@gmail.com>
2018-08-14 16:54:58 +0200

    Paper UI: refactor config description to component API (#6045)
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/configuration/directive.locationParameter.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/configuration/directive.locationParameter.js
@@ -1,9 +1,14 @@
-angular.module('PaperUI.directive.locationParameter', [ 'PaperUI.component' ]) //
-.component('locationParameter', {
-    bindings : {
-        model : '=',
-        parameter : '=',
-        form : '='
-    },
-    templateUrl : 'partials/configuration/directive.locationParameter.html'
-});
+;
+(function() {
+    'use strict';
+
+    angular.module('PaperUI.directive.locationParameter', [ 'PaperUI.component' ]).component('locationParameter', {
+        bindings : {
+            model : '=',
+            parameter : '=',
+            form : '='
+        },
+        templateUrl : 'partials/configuration/directive.locationParameter.html'
+    });
+
+})()
