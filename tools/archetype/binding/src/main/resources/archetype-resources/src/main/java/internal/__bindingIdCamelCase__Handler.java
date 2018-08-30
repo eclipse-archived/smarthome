34141ed1306d1b9219eba2a9e3eb5a87b3e95937
Henning Treu <henning.treu@gmail.com>
2018-08-30 12:52:19 +0200

    Extend documentation about test assertions, archetype cosmetics (#6134)
    * Extend documentation about test assertions, archetype cosmetics
    
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- tools/archetype/binding/src/main/resources/archetype-resources/src/main/java/internal/__bindingIdCamelCase__Handler.java
+++ tools/archetype/binding/src/main/resources/archetype-resources/src/main/java/internal/__bindingIdCamelCase__Handler.java
@@ -61,7 +61,7 @@ public class ${bindingIdCamelCase}Handler extends BaseThingHandler {
             // TODO: handle command
 
             // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information
+            // indicate that by setting the status with detail information:
             // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
             // "Could not control device at IP address x.x.x.x");
         }
@@ -69,19 +69,36 @@ public class ${bindingIdCamelCase}Handler extends BaseThingHandler {
 
     @Override
     public void initialize() {
+        // logger.debug("Start initializing!");
         config = getConfigAs(${bindingIdCamelCase}Configuration.class);
 
-        // TODO: Initialize the thing. If done set status to ONLINE to indicate proper working.
-        // Long running initialization should be done asynchronously in background.
+        // TODO: Initialize the handler.
+        // The framework requires you to return from this method quickly. Also, before leaving this method a thing 
+        // status from one of ONLINE, OFFLINE or UNKNOWN must be set. This might already be the real thing status in 
+        // case you can decide it directly.
+        // In case you can not decide the thing status directly (e.g. for long running connection handshake using WAN
+        // access or similar) you should set status UNKNOWN here and then decide the real status asynchronously in the
+        // background.
 
-        // logger.debug("Start initializing!");
-        updateStatus(ThingStatus.ONLINE);
+        // Example for background initialization:
+        scheduler.execute(() -> {
+            boolean thingReachable = true; // <background task with long running initialization here>
+            // when done do:
+            if (thingReachable) {
+                // updateStatus(ThingStatus.ONLINE); // commented for stable test execution
+            } else {
+                // updateStatus(ThingStatus.OFFLINE);
+            }
+        });
+
+        // set the thing status to UNKNOWN temporarily and let the background task decide for the real status.
+        // the framework is then able to reuse the resources from the thing handler initialization.
+        updateStatus(ThingStatus.UNKNOWN);
         // logger.debug("Finished initializing!");
-        
+
         // Note: When initialization can NOT be done set the status with more details for further
         // analysis. See also class ThingStatusDetail for all available status details.
-        // Add a description to give user information to understand why thing does not work
-        // as expected. E.g.
+        // Add a description to give user information to understand why thing does not work as expected. E.g.
         // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.CONFIGURATION_ERROR,
         // "Can not access device as username and/or password are invalid");
     }
