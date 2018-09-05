d78b3ad6c324d0fbd0ec97d7dc6584653a1cd078
Hilbrand Bouwkamp <hilbrand@h72.nl>
2018-09-05 06:35:25 +0200

    Minor fixes (#6152)
    * Improved log warning, added space: exception was append to last word.
    * Added util methods to OnOFFType
    
    The pattern "boolean ? ON : OFF" (or inverted) was found more then 350 times in the code, so I think it deserves a util method.
    
    Signed-off-by: Hilbrand Bouwkamp <hilbrand@h72.nl>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/library/types/OnOffType.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/library/types/OnOffType.java
@@ -27,6 +27,24 @@ public enum OnOffType implements PrimitiveType, State, Command {
     ON,
     OFF;
 
+    /**
+     * Converts a String value "ON" or "1" to {@link OnOffType#ON} or else to {@link OnOffType#OFF}.
+     *
+     * @param state String to convert to {@link OnOffType}
+     * @return returns the ON or OFF state based on the String
+     */
+    public static OnOffType from(String state) {
+        return from("ON".equalsIgnoreCase(state) || "1".equalsIgnoreCase(state));
+    }
+
+    /**
+     * @param state boolean to convert to {@link OnOffType}
+     * @return returns the ON or OFF state based on the boolean
+     */
+    public static OnOffType from(boolean state) {
+        return state ? ON : OFF;
+    }
+
     @Override
     public String format(String pattern) {
         return String.format(pattern, this.toString());
