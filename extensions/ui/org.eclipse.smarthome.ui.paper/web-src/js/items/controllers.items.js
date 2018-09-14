96ae7203c650ca511baa130781974a57fd70cc1e
Henning Treu <henning.treu@telekom.de>
2018-09-14 12:59:02 +0200

    Paper UI: resolve promise after all matedata updates passed
    Signed-off-by: Henning Treu <henning.treu@telekom.de>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/items/controllers.items.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/items/controllers.items.js
@@ -233,6 +233,7 @@ angular.module('PaperUI.items')//
     function updateMetadata(itemName, metadata, originalItem, toastText) {
         return $q(function(resolve, reject) {
             var resolveLater = false;
+            var namespaceUpdatePromisses = [];
             if (!originalItem.metadata || JSON.stringify(metadata) !== JSON.stringify(originalItem.metadata)) {
                 for ( var namespace in metadata) {
                     if (!metadata.hasOwnProperty(namespace) || metadata[namespace].value === undefined || metadata[namespace].value.length === 0) {
@@ -241,21 +242,23 @@ angular.module('PaperUI.items')//
 
                     if (!originalItem.metadata || JSON.stringify(metadata[namespace]) !== JSON.stringify(originalItem.metadata[namespace])) {
                         resolveLater = true;
-                        itemService.updateMetadata({
+                        var namespaceUpdatePromise = itemService.updateMetadata({
                             itemName : itemName,
                             namespace : namespace
-                        }, metadata[namespace], function() {
+                        }, metadata[namespace]).$promise.then(function() {
                             if (toastText) {
                                 toastService.showDefaultToast(toastText);
                             }
-
-                            resolve();
                         });
+
+                        namespaceUpdatePromisses.push(namespaceUpdatePromise);
                     }
                 }
             }
             if (!resolveLater) {
                 resolve();
+            } else {
+                $q.all(namespaceUpdatePromisses).then(resolve());
             }
         });
     }
