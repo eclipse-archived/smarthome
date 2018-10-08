c237ee2e99209d3b8dfb9166043ae3929e1de139
msteigenberger <matthias.steigenberger@gmail.com>
2018-10-08 08:41:08 +0200

    `UnitUtils#getDimensionName` introduced (#6303)
    * `UnitUtils#getDimensionName` introduced
    
    This makes it possible to dynamically get the `itemType` from value + unit which is sent from a device.
    
    Signed-off-by: Matthias Steigenberger <matthias.steigenberger@gmail.com>

--- bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/types/util/UnitUtils.java
+++ bundles/core/org.eclipse.smarthome.core/src/main/java/org/eclipse/smarthome/core/types/util/UnitUtils.java
@@ -14,11 +14,18 @@ package org.eclipse.smarthome.core.types.util;
 
 import static java.util.stream.Collectors.toSet;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
 
 import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.measure.UnitConverter;
+import javax.measure.spi.SystemOfUnits;
 
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -33,6 +40,7 @@ import org.slf4j.LoggerFactory;
 import tec.uom.se.quantity.Quantities;
 import tec.uom.se.unit.MetricPrefix;
 import tec.uom.se.unit.TransformedUnit;
+import tec.uom.se.unit.Units;
 
 /**
  * A utility for parsing dimensions to interface classes of {@link Quantity} and parsing units from format strings.
@@ -50,6 +58,9 @@ public class UnitUtils {
 
     private static final String JAVAX_MEASURE_QUANTITY_PREFIX = "javax.measure.quantity.";
     private static final String FRAMEWORK_DIMENSION_PREFIX = "org.eclipse.smarthome.core.library.dimension.";
+
+    private static final Collection<Class<? extends SystemOfUnits>> ALL_SYSTEM_OF_UNITS = Arrays.asList(SIUnits.class,
+            ImperialUnits.class, SmartHomeUnits.class, Units.class);
 
     static {
         UnitInitializer.init();
@@ -80,6 +91,49 @@ public class UnitUtils {
                         "Error creating a dimension Class instance for name '" + dimension + "'.");
             }
         }
+    }
+
+    /**
+     * The name of the dimension as stated in the ChannelType configuration.
+     * e.g.
+     * <p>
+     * <code> Unit: 'm' -> "Length"</code>
+     * <p>
+     * <code> Unit: 'kWh' -> "Energy"</code>
+     * <p>
+     * If the {@link Unit} can not be found in any of the available Measurement systems, it returns <code>null</code>
+     *
+     * @param unit The {@link Unit} to get the Dimension's name from.
+     * @return The Dimension string or null if the unit can not be found in any of the SystemOfUnits.
+     */
+    public static @Nullable String getDimensionName(Unit<?> unit) {
+        for (Class<? extends SystemOfUnits> system : ALL_SYSTEM_OF_UNITS) {
+            for (Field field : system.getDeclaredFields()) {
+                if (field.getType().isAssignableFrom(Unit.class) && Modifier.isStatic(field.getModifiers())) {
+
+                    Type genericType = field.getGenericType();
+                    if (genericType instanceof ParameterizedType) {
+                        String dimension = ((Class<?>) ((ParameterizedType) genericType).getActualTypeArguments()[0])
+                                .getSimpleName();
+                        Unit<?> systemUnit;
+                        try {
+                            systemUnit = (Unit<?>) field.get(null);
+                            if (systemUnit == null) {
+                                LOGGER.warn("Unit field points to a null value: {}", field);
+                            } else if (systemUnit.isCompatible(unit)) {
+                                return dimension;
+                            }
+                        } catch (IllegalArgumentException | IllegalAccessException e) {
+                            LOGGER.error("The unit field '{}' seems to be not accessible", field, e);
+                        }
+                    } else {
+                        LOGGER.warn("There is a unit field defined which has no generic type parametrization: {}",
+                                field);
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     /**
