ee2089903c0269e8da614db2b49850870625f038
Scott Rushworth <21967853+openhab-5iver@users.noreply.github.com>
2018-10-11 09:51:18 -0400

    Changed ItemStateTriggerHandler to properly filter relevant topics
    Resolves #6317
    
    Signed-off-by: Scott Rushworth <openhab@5iver.com>

--- bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/handler/ItemStateTriggerHandler.java
+++ bundles/automation/org.eclipse.smarthome.automation.module.core/src/main/java/org/eclipse/smarthome/automation/module/core/handler/ItemStateTriggerHandler.java
@@ -144,7 +144,7 @@ public class ItemStateTriggerHandler extends BaseTriggerModuleHandler implements
     @Override
     public boolean apply(Event event) {
         logger.trace("->FILTER: {}:{}", event.getTopic(), itemName);
-        return event.getTopic().contains("/" + itemName + "/");
+        return event.getTopic().contains("smarthome/items/" + itemName + "/");
     }
 
 }
