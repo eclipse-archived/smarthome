11f3d08aa23b2cfcffb2e69306fc53ba47ac4f07
mgwoj <32574975+mgwoj@users.noreply.github.com>
2018-10-16 08:16:03 +0200

    Fixed refresh issue, was 10 seconds instead of 300 (#6368)
    Signed-off-by: Michal Wojcik <mgwojcik@interia.pl>

--- extensions/binding/org.eclipse.smarthome.binding.onewire/src/main/java/org/eclipse/smarthome/binding/onewire/internal/handler/MultisensorThingHandler.java
+++ extensions/binding/org.eclipse.smarthome.binding.onewire/src/main/java/org/eclipse/smarthome/binding/onewire/internal/handler/MultisensorThingHandler.java
@@ -82,7 +82,7 @@ public class MultisensorThingHandler extends OwBaseThingHandler {
         if (configuration.get(CONFIG_DIGITALREFRESH) != null) {
             digitalRefreshInterval = ((BigDecimal) configuration.get(CONFIG_DIGITALREFRESH)).intValue() * 1000;
         } else {
-            refreshInterval = 10 * 1000;
+            digitalRefreshInterval = 10 * 1000;
         }
         digitalLastRefresh = 0;
 
