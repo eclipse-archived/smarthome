879b0d876cc6f0d14bfee165b72d8d3fdc386adb
Jochen Hiller <jo.hiller@googlemail.com>
2018-10-20 07:48:17 +0000

    Fixed type casts where JDK compiler complained
    Signed-off-by: Jochen Hiller <jochen.hiller@telekom.de>

--- bundles/automation/org.eclipse.smarthome.automation.core/src/main/java/org/eclipse/smarthome/automation/core/internal/ruleengine/WrappedRule.java
+++ bundles/automation/org.eclipse.smarthome.automation.core/src/main/java/org/eclipse/smarthome/automation/core/internal/ruleengine/WrappedRule.java
@@ -37,7 +37,8 @@ public class WrappedRule {
 
     private static <T extends WrappedModule, U extends Module> List<T> map(final List<U> in, Function<U, T> factory,
             final Collection<WrappedModule<Module, ModuleHandler>> coll) {
-        return Collections.unmodifiableList(in.stream().map(module -> {
+        // explicit cast to List <? extends T> as JDK compiler complains
+        return Collections.unmodifiableList((List <? extends T>)in.stream().map(module -> {
             final T impl = factory.apply(module);
             coll.add(impl);
             return impl;
