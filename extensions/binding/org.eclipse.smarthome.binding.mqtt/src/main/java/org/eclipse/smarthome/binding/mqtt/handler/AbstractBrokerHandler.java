85c46bd086ee9b070bb3f981a60c8cf0b5869c9a
David Graeff <david.graeff@web.de>
2018-11-06 00:38:25 +0100

    MQTT Brokers: Add trigger channel
    Configured brokers are now extensible.
    A trigger channel can be added that triggers if a specific value or
    any value got received on a configured MQTT topic.
    
    Signed-off-by: David Graeff <david.graeff@web.de>

--- extensions/binding/org.eclipse.smarthome.binding.mqtt/src/main/java/org/eclipse/smarthome/binding/mqtt/handler/AbstractBrokerHandler.java
+++ extensions/binding/org.eclipse.smarthome.binding.mqtt/src/main/java/org/eclipse/smarthome/binding/mqtt/handler/AbstractBrokerHandler.java
@@ -12,12 +12,15 @@
  */
 package org.eclipse.smarthome.binding.mqtt.handler;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeoutException;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.ThingStatus;
 import org.eclipse.smarthome.core.thing.ThingStatusDetail;
@@ -27,6 +30,8 @@ import org.eclipse.smarthome.io.transport.mqtt.MqttBrokerConnection;
 import org.eclipse.smarthome.io.transport.mqtt.MqttConnectionObserver;
 import org.eclipse.smarthome.io.transport.mqtt.MqttConnectionState;
 import org.eclipse.smarthome.io.transport.mqtt.MqttService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This base implementation handles connection changes of the {@link MqttBrokerConnection}
@@ -37,8 +42,10 @@ import org.eclipse.smarthome.io.transport.mqtt.MqttService;
  */
 @NonNullByDefault
 public abstract class AbstractBrokerHandler extends BaseBridgeHandler implements MqttConnectionObserver {
+    private final Logger logger = LoggerFactory.getLogger(AbstractBrokerHandler.class);
     public static int TIMEOUT_DEFAULT = 1200; /* timeout in milliseconds */
     protected final String brokerID;
+    final Map<ChannelUID, PublishTriggerChannel> channelStateByChannelUID = new HashMap<>();
 
     @NonNullByDefault({})
     protected MqttBrokerConnection connection;
@@ -77,6 +84,13 @@ public abstract class AbstractBrokerHandler extends BaseBridgeHandler implements
      */
     @Override
     public void initialize() {
+        for (Channel channel : thing.getChannels()) {
+            final PublishTriggerChannelConfig channelConfig = channel.getConfiguration()
+                    .as(PublishTriggerChannelConfig.class);
+            PublishTriggerChannel c = new PublishTriggerChannel(channelConfig, channel.getUID(), connection, this);
+            channelStateByChannelUID.put(channel.getUID(), c);
+        }
+
         connection.addConnectionObserver(this);
 
         connection.start().exceptionally(e -> {
@@ -96,7 +110,9 @@ public abstract class AbstractBrokerHandler extends BaseBridgeHandler implements
     public void connectionStateChanged(MqttConnectionState state, @Nullable Throwable error) {
         if (state == MqttConnectionState.CONNECTED) {
             updateStatus(ThingStatus.ONLINE);
+            channelStateByChannelUID.values().forEach(c -> c.start());
         } else {
+            channelStateByChannelUID.values().forEach(c -> c.stop());
             if (error == null) {
                 updateStatus(ThingStatus.OFFLINE);
             } else {
@@ -105,11 +121,18 @@ public abstract class AbstractBrokerHandler extends BaseBridgeHandler implements
         }
     }
 
+    @Override
+    protected void triggerChannel(ChannelUID channelUID, String event) {
+        super.triggerChannel(channelUID, event);
+    }
+
     /**
      * Removes listeners to the {@link MqttBrokerConnection}.
      */
     @Override
     public void dispose() {
+        channelStateByChannelUID.values().forEach(c -> c.stop());
+        channelStateByChannelUID.clear();
         connection.removeConnectionObserver(this);
         this.connection = null;
         super.dispose();
