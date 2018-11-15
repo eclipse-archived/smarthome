86ee6826deaf2dc4f4b0520ed40ae0f189c20d63
Martin van Wingerden <martin@martinvw.nl>
2018-11-15 10:10:57 +0100

    Small cleanup and attempt to unbrick build (#6520)
    - Added missing file-level copyright
    - Moved the @Deprecated to the correct place
    - Ignore test using the common client
    
    Signed-off-by: Martin van Wingerden <martin@martinvw.nl>

--- bundles/io/org.eclipse.smarthome.io.net.test/src/test/java/org/eclipse/smarthome/io/net/http/internal/ExtensibleTrustManagerTest.java
+++ bundles/io/org.eclipse.smarthome.io.net.test/src/test/java/org/eclipse/smarthome/io/net/http/internal/ExtensibleTrustManagerTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.smarthome.io.net.http.internal;
 
 import static org.mockito.Mockito.*;
@@ -20,6 +32,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+/**
+ * Tests which validate the behaviour of the ExtensibleTrustManager
+ *
+ * @author Martin van Wingerden - Initial contribution
+ */
 public class ExtensibleTrustManagerTest {
     private ExtensibleTrustManager subject;
 
