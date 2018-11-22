eclipse-archived/smarthome

commit 800cceca05a12be8fa447b2f9a11b420fac6e612

RuleActions via ActionProvider with annotations (#6398)
* RuleActions via ActionProvider with annotations

Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/services.js
@@ -168,26 +168,28 @@ angular.module('PaperUI.services', [ 'PaperUI.services.repositories', 'PaperUI.c
 
         switch (context) {
             case "ITEM":
-                getItemOptions(parameter);
+                getItemOptions(parameter).then(setMissingLabels);
                 break;
             case "THING":
-                getThingOptions(parameter);
+                getThingOptions(parameter).then(setMissingLabels);
                 break;
             case "CHANNEL":
-                getChannelOptions(parameter);
+                getChannelOptions(parameter).then(setMissingLabels);
                 break;
             case "RULE":
                 getRuleOptions(parameter);
                 break;
         }
 
+        return true;
+    }
+
+    function setMissingLabels(parameter) {
         angular.forEach(parameter.options, function(option) {
             if (!option.label || option.label.length == 0) {
                 option.label = option.value;
             }
         });
-
-        return true;
     }
 
     function applyParameterType(parameter) {
@@ -275,30 +277,56 @@ angular.module('PaperUI.services', [ 'PaperUI.services.repositories', 'PaperUI.c
     }
 
     function getItemOptions(parameter) {
-        itemRepository.getAll().then(function(items) {
+        angular.forEach(parameter.options, function(option) {
+            if (!option.value && option.name) {
+                option.value = option.name;
+            }
+        });
+        return itemRepository.getAll().then(function(items) {
             var filteredItems = filterByAttributes(items, parameter.filterCriteria);
             if (parameter.options && parameter.options.length > 0) {
                 lookupOptionLabels(parameter.options, filteredItems, 'name', 'label')
             } else {
                 parameter.options = $filter('orderBy')(filteredItems, 'label');
             }
+
+            return parameter;
         });
     }
 
     function getThingOptions(parameter) {
-        thingRepository.getAll().then(function(things) {
-            filteredThings = filterByAttributes(things, parameter.filterCriteria);
+        angular.forEach(parameter.options, function(option) {
+            if (!option.value && option.UID) {
+                option.value = option.UID;
+            }
+        });
+        return thingRepository.getAll().then(function(things) {
+            var filteredThings = filterByAttributes(things, parameter.filterCriteria);
             if (parameter.options && parameter.options.length > 0) {
                 lookupOptionLabels(parameter.options, filteredThings, 'UID', 'label')
             } else {
                 parameter.options = filteredThings;
             }
+
+            return parameter;
         });
     }
 
     function getChannelOptions(parameter) {
-        thingRepository.getAll().then(function(things) {
-            parameter.options = getChannelsFromThings(things, parameter.filterCriteria);
+        angular.forEach(parameter.options, function(option) {
+            if (!option.value && option.id) {
+                option.value = option.id;
+            }
+        });
+        return thingRepository.getAll().then(function(things) {
+            var filteredChannels = getChannelsFromThings(things, parameter.filterCriteria);
+            if (parameter.options && parameter.options.length > 0) {
+                lookupOptionLabels(parameter.options, filteredChannels, 'id', 'label')
+            } else {
+                parameter.options = filteredChannels;
+            }
+
+            return parameter;
         });
     }
 

bundles/automation/org.eclipse.smarthome.automation.api/META-INF/MANIFEST.MF
@@ -7,6 +7,7 @@ Bundle-Vendor: Eclipse.org/SmartHome
 Bundle-Version: 0.10.0.qualifier
 Export-Package: 
  org.eclipse.smarthome.automation,
+ org.eclipse.smarthome.automation.annotation,
  org.eclipse.smarthome.automation.dto,
  org.eclipse.smarthome.automation.events,
  org.eclipse.smarthome.automation.handler,

bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/AnnotatedActions.java
@@ -1,0 +1,25 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.automation;
+
+/**
+ * Marker interface for RuleActions
+ *
+ * Every method in the implementation should provide annotations which are used to create the ModuleTypes
+ *
+ * @author Stefan Triller - initial contribution
+ *
+ */
+public interface AnnotatedActions {
+
+}

bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/annotation/ActionInput.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.automation.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Input parameter for an action module
+ *
+ * @author Stefan Triller - initial contribution
+ */
+@Repeatable(ActionInputs.class)
+@Retention(RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ActionInput {
+
+    String name();
+
+    String type() default "";
+
+    String label() default "";
+
+    String description() default "";
+
+    String[] tags() default {};
+
+    boolean required() default false;
+
+    String reference() default "";
+
+    String defaultValue() default "";
+}

bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/annotation/ActionInputs.java
@@ -1,0 +1,30 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.automation.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Input parameter wrapper for an action module
+ *
+ * @author Stefan Triller - initial contribution
+ */
+@Retention(RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface ActionInputs {
+    ActionInput[] value();
+}

bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/annotation/ActionOutput.java
@@ -1,0 +1,45 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.automation.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Output parameter for an action module
+ *
+ * @author Stefan Triller - initial contribution
+ */
+@Repeatable(ActionOutputs.class)
+@Retention(RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ActionOutput {
+
+    String name();
+
+    String type();
+
+    String label() default "";
+
+    String description() default "";
+
+    String[] tags() default {};
+
+    String reference() default "";
+
+    String defaultValue() default "";
+}

bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/annotation/ActionOutputs.java
@@ -1,0 +1,30 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.automation.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Output parameter wrapper for an action module
+ *
+ * @author Stefan Triller - initial contribution
+ */
+@Retention(RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ActionOutputs {
+    ActionOutput[] value();
+}

bundles/automation/org.eclipse.smarthome.automation.api/src/main/java/org/eclipse/smarthome/automation/annotation/ActionScope.java
@@ -1,0 +1,31 @@
+/**
+ * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.automation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Scope definition for an action module
+ *
+ * @author Stefan Triller - initial contribution
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActionScope {
+
+    String name();
+
+}

commit 1dc3e6cd6999c3650ce52acb6af23da3ade8fe5e

Fix PaperUI item selection
This was broken with PR #6398.

Also corrects channel and thing selection is certain cases.

Fixes #6557

Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/services.js
@@ -277,11 +277,6 @@ angular.module('PaperUI.services', [ 'PaperUI.services.repositories', 'PaperUI.c
     }
 
     function getItemOptions(parameter) {
-        angular.forEach(parameter.options, function(option) {
-            if (!option.value && option.name) {
-                option.value = option.name;
-            }
-        });
         return itemRepository.getAll().then(function(items) {
             var filteredItems = filterByAttributes(items, parameter.filterCriteria);
             if (parameter.options && parameter.options.length > 0) {
@@ -289,17 +284,17 @@ angular.module('PaperUI.services', [ 'PaperUI.services.repositories', 'PaperUI.c
             } else {
                 parameter.options = $filter('orderBy')(filteredItems, 'label');
             }
+            angular.forEach(parameter.options, function(option) {
+                if (!option.value && option.name) {
+                    option.value = option.name;
+                }
+            });
 
             return parameter;
         });
     }
 
     function getThingOptions(parameter) {
-        angular.forEach(parameter.options, function(option) {
-            if (!option.value && option.UID) {
-                option.value = option.UID;
-            }
-        });
         return thingRepository.getAll().then(function(things) {
             var filteredThings = filterByAttributes(things, parameter.filterCriteria);
             if (parameter.options && parameter.options.length > 0) {
@@ -307,17 +302,17 @@ angular.module('PaperUI.services', [ 'PaperUI.services.repositories', 'PaperUI.c
             } else {
                 parameter.options = filteredThings;
             }
+            angular.forEach(parameter.options, function(option) {
+                if (!option.value && option.UID) {
+                    option.value = option.UID;
+                }
+            });
 
             return parameter;
         });
     }
 
     function getChannelOptions(parameter) {
-        angular.forEach(parameter.options, function(option) {
-            if (!option.value && option.id) {
-                option.value = option.id;
-            }
-        });
         return thingRepository.getAll().then(function(things) {
             var filteredChannels = getChannelsFromThings(things, parameter.filterCriteria);
             if (parameter.options && parameter.options.length > 0) {
@@ -325,6 +320,11 @@ angular.module('PaperUI.services', [ 'PaperUI.services.repositories', 'PaperUI.c
             } else {
                 parameter.options = filteredChannels;
             }
+            angular.forEach(parameter.options, function(option) {
+                if (!option.value && option.id) {
+                    option.value = option.id;
+                }
+            });
 
             return parameter;
         });