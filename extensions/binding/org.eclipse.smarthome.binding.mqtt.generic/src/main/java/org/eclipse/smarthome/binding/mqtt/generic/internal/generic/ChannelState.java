d154566a355f6776262cc1473c4931d2af0c13e5
J-N-K <J-N-K@users.noreply.github.com>
2018-12-02 16:44:38 +0100

    [MQTT] fix connection handling for command-only channels (#6615)
    * fix connection handling for command-only channels
    * observe already subscribed settings
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- extensions/binding/org.eclipse.smarthome.binding.mqtt.generic/src/main/java/org/eclipse/smarthome/binding/mqtt/generic/internal/generic/ChannelState.java
+++ extensions/binding/org.eclipse.smarthome.binding.mqtt.generic/src/main/java/org/eclipse/smarthome/binding/mqtt/generic/internal/generic/ChannelState.java
@@ -236,11 +236,16 @@ public class ChannelState implements MqttMessageSubscriber {
      */
     public CompletableFuture<@Nullable Void> start(MqttBrokerConnection connection, ScheduledExecutorService scheduler,
             int timeout) {
-        if (hasSubscribed || StringUtils.isBlank(config.stateTopic)) {
+        if (hasSubscribed) {
+            return CompletableFuture.completedFuture(null);
+        }
+        
+        this.connection = connection;
+        
+        if (StringUtils.isBlank(config.stateTopic)) {
             return CompletableFuture.completedFuture(null);
         }
 
-        this.connection = connection;
         this.future = new CompletableFuture<>();
         connection.subscribe(config.stateTopic, this).thenRun(() -> {
             hasSubscribed = true;
