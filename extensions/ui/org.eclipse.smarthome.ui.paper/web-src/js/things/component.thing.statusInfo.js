0fd6acbc6a49802ce1f9126ce1800ca92df3337a
Henning Treu <henning.treu@googlemail.com>
2018-12-03 11:16:46 +0100

    Show full thing status, dimmed description for disabled thing
    Signed-off-by: Henning Treu <henning.treu@googlemail.com>

--- extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/things/component.thing.statusInfo.js
+++ extensions/ui/org.eclipse.smarthome.ui.paper/web-src/js/things/component.thing.statusInfo.js
@@ -6,6 +6,19 @@
         bindings : {
             statusInfo : '<'
         },
-        templateUrl : 'partials/things/component.thing.statusInfo.html'
+        templateUrl : 'partials/things/component.thing.statusInfo.html',
+        controller : ThingStatusController
     });
+
+    function ThingStatusController() {
+        var ctrl = this;
+        this.getThingStatus = getThingStatus;
+
+        function getThingStatus() {
+            var detail = ctrl.statusInfo.statusDetail;
+            var fullStatus = ctrl.statusInfo.status + ((detail && detail !== 'NONE') ? ' - ' + detail : '');
+            return fullStatus;
+        }
+
+    }
 })()
