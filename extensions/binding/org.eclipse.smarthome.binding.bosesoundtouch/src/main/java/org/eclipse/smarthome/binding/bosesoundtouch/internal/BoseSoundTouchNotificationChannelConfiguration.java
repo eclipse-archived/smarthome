77432e4a4965caa6829eae01e9e7a5ffd23606a5
Ivaylo Ivanov <ivivanov.bg@gmail.com>
2018-12-11 09:18:56 +0200

    Enrich Bose SoundTouch thing with some identification properties: - Manufacturer, ModelId, Firmware Version, Hardware Version - Add constants for min. fw version which supports Notification Sound
    Signed-off-by: Ivaylo Ivanov <ivivanov.bg@gmail.com>

--- extensions/binding/org.eclipse.smarthome.binding.bosesoundtouch/src/main/java/org/eclipse/smarthome/binding/bosesoundtouch/internal/BoseSoundTouchNotificationChannelConfiguration.java
+++ extensions/binding/org.eclipse.smarthome.binding.bosesoundtouch/src/main/java/org/eclipse/smarthome/binding/bosesoundtouch/internal/BoseSoundTouchNotificationChannelConfiguration.java
@@ -19,6 +19,9 @@ package org.eclipse.smarthome.binding.bosesoundtouch.internal;
  */
 public class BoseSoundTouchNotificationChannelConfiguration {
 
+    public static final String MIN_FIRMWARE = "14";
+    public static final String MODEL_TYPE = "sm2";
+
     public static final String NOTIFICATION_VOLUME = "notificationVolume";
     public static final String NOTIFICATION_SERVICE = "notificationService";
     public static final String NOTIFICATION_REASON = "notificationReason";
@@ -28,4 +31,12 @@ public class BoseSoundTouchNotificationChannelConfiguration {
     public String notificationService;
     public String notificationReason;
     public String notificationMessage;
+    
+    public static boolean isSupportedFirmware(String firmware) {
+        return firmware != null && firmware.compareTo(MIN_FIRMWARE) > 0;
+    }
+    
+    public static boolean isSupportedHardware(String hardware) {
+        return MODEL_TYPE.equals(hardware);
+    }
 }
