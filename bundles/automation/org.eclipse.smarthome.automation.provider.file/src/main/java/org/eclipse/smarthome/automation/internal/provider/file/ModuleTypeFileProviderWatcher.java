4f8fab9d9ca333517e33612fd88c8bd17f7e810d
Henning Treu <henning.treu@googlemail.com>
2018-12-13 11:04:38 +0100

    Refactor service XML to annotations for automation.provider.file
    Signed-off-by: Henning Treu <henning.treu@googlemail.com>

--- bundles/automation/org.eclipse.smarthome.automation.provider.file/src/main/java/org/eclipse/smarthome/automation/internal/provider/file/ModuleTypeFileProviderWatcher.java
+++ bundles/automation/org.eclipse.smarthome.automation.provider.file/src/main/java/org/eclipse/smarthome/automation/internal/provider/file/ModuleTypeFileProviderWatcher.java
@@ -12,7 +12,15 @@
  */
 package org.eclipse.smarthome.automation.internal.provider.file;
 
+import java.util.Map;
+
+import org.eclipse.smarthome.automation.parser.Parser;
+import org.eclipse.smarthome.automation.type.ModuleType;
 import org.eclipse.smarthome.automation.type.ModuleTypeProvider;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
  * This class is a wrapper of {@link ModuleTypeProvider}, responsible for initializing the WatchService.
@@ -20,6 +28,7 @@ import org.eclipse.smarthome.automation.type.ModuleTypeProvider;
  * @author Ana Dimova - Initial Contribution
  *
  */
+@Component(immediate = true, service = ModuleTypeProvider.class)
 public class ModuleTypeFileProviderWatcher extends ModuleTypeFileProvider {
 
     @Override
@@ -30,5 +39,16 @@ public class ModuleTypeFileProviderWatcher extends ModuleTypeFileProvider {
     @Override
     protected void deactivateWatchService(String watchingDir) {
         WatchServiceUtil.deactivateWatchService(watchingDir, this);
+    }
+
+    @Reference(cardinality = ReferenceCardinality.AT_LEAST_ONE, policy = ReferencePolicy.DYNAMIC, target = "(parser.type=parser.module.type)")
+    @Override
+    public void addParser(Parser<ModuleType> parser, Map<String, String> properties) {
+        super.addParser(parser, properties);
+    }
+
+    @Override
+    public void removeParser(Parser<ModuleType> parser, Map<String, String> properties) {
+        super.removeParser(parser, properties);
     }
 }
