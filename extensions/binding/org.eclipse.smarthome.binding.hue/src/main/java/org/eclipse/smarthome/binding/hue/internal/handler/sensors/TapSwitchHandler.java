d194d2eaa32d33f4bd85ae109cc9a586e1702e2a
Christoph Weitkamp <github@christophweitkamp.de>
2019-01-02 19:30:02 +0100

    Avoid dispatching events if 'last_updated' older than now minus two times polling interval (#6770)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- extensions/binding/org.eclipse.smarthome.binding.hue/src/main/java/org/eclipse/smarthome/binding/hue/internal/handler/sensors/TapSwitchHandler.java
+++ extensions/binding/org.eclipse.smarthome.binding.hue/src/main/java/org/eclipse/smarthome/binding/hue/internal/handler/sensors/TapSwitchHandler.java
@@ -12,22 +12,34 @@
  */
 package org.eclipse.smarthome.binding.hue.internal.handler.sensors;
 
+import static org.eclipse.smarthome.binding.hue.internal.FullSensor.STATE_LAST_UPDATED;
 import static org.eclipse.smarthome.binding.hue.internal.HueBindingConstants.*;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.binding.hue.internal.FullSensor;
 import org.eclipse.smarthome.binding.hue.internal.HueBridge;
 import org.eclipse.smarthome.binding.hue.internal.SensorConfigUpdate;
+import org.eclipse.smarthome.binding.hue.internal.handler.HueBridgeHandler;
 import org.eclipse.smarthome.binding.hue.internal.handler.HueSensorHandler;
 import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.core.library.types.DecimalType;
+import org.eclipse.smarthome.core.thing.Bridge;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
 
 /**
  * Hue Tap Switch
@@ -38,8 +50,23 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 public class TapSwitchHandler extends HueSensorHandler {
     public static final Set<ThingTypeUID> SUPPORTED_THING_TYPES = Collections.singleton(THING_TYPE_TAP_SWITCH);
 
+    private long refreshIntervalInNanos = TimeUnit.MILLISECONDS.toNanos(1000);
+
     public TapSwitchHandler(Thing thing) {
         super(thing);
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        Bridge bridge = getBridge();
+        if (bridge != null) {
+            ThingHandler bridgeHandler = bridge.getHandler();
+            if (bridgeHandler instanceof HueBridgeHandler) {
+                refreshIntervalInNanos = TimeUnit.MILLISECONDS
+                        .toNanos(((HueBridgeHandler) bridgeHandler).getSensorPollingInterval() * 2);
+            }
+        }
     }
 
     @Override
@@ -49,11 +76,29 @@ public class TapSwitchHandler extends HueSensorHandler {
 
     @Override
     protected void doSensorStateChanged(@Nullable HueBridge bridge, FullSensor sensor, Configuration config) {
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZonedDateTime now = ZonedDateTime.now(zoneId), timestamp = now;
+
+        Object lastUpdated = sensor.getState().get(STATE_LAST_UPDATED);
+        if (lastUpdated != null) {
+            try {
+                timestamp = ZonedDateTime.ofInstant(
+                        LocalDateTime.parse(String.valueOf(lastUpdated), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        ZoneOffset.UTC, zoneId);
+            } catch (DateTimeParseException e) {
+                // do nothing
+            }
+        }
+
         Object buttonState = sensor.getState().get(FullSensor.STATE_BUTTON_EVENT);
         if (buttonState != null) {
             String value = String.valueOf(buttonState);
             updateState(CHANNEL_TAP_SWITCH, new DecimalType(value));
-            triggerChannel(EVENT_TAP_SWITCH, value);
+            Instant then = timestamp.toInstant();
+            Instant someSecondsEarlier = now.minusNanos(refreshIntervalInNanos).toInstant();
+            if (then.isAfter(someSecondsEarlier) && then.isBefore(now.toInstant())) {
+                triggerChannel(EVENT_TAP_SWITCH, value);
+            }
         }
     }
 }
