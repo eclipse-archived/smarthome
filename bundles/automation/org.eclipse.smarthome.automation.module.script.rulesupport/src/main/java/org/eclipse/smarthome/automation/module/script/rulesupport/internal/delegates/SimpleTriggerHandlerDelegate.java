ed06e737a6512585a77601b8271d34142074367b
Markus Rathgeb <maggu2810@gmail.com>
2019-01-07 08:53:30 +0100

    change license year to 2019
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/automation/org.eclipse.smarthome.automation.module.script.rulesupport/src/main/java/org/eclipse/smarthome/automation/module/script/rulesupport/internal/delegates/SimpleTriggerHandlerDelegate.java
+++ bundles/automation/org.eclipse.smarthome.automation.module.script.rulesupport/src/main/java/org/eclipse/smarthome/automation/module/script/rulesupport/internal/delegates/SimpleTriggerHandlerDelegate.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ * Copyright (c) 2014,2019 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
