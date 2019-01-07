f2a1f38833610f882a645917c202bd126f4d10fd
Markus Rathgeb <maggu2810@gmail.com>
2019-01-07 11:24:45 +0000

    fix license header (add missing, update year) (#6811)
    * add missing license headers
    * change license year to 2019
    
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- extensions/binding/org.eclipse.smarthome.binding.dmx/src/main/java/org/eclipse/smarthome/binding/dmx/action/DmxActions.java
+++ extensions/binding/org.eclipse.smarthome.binding.dmx/src/main/java/org/eclipse/smarthome/binding/dmx/action/DmxActions.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014,2018 Contributors to the Eclipse Foundation
+ * Copyright (c) 2014,2019 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -10,7 +10,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-
 package org.eclipse.smarthome.binding.dmx.action;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
