f4bda51a48725d2e945cb567aeab6a400df9928d
Jan N. Klug <jan.n.klug@rub.de>
2019-01-09 13:57:18 +0100

    improve tests
    - thing handler tests know check the correct sensor update
    - some more completeness tests
    
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- extensions/binding/org.eclipse.smarthome.binding.onewire.test/src/test/java/org/eclipse/smarthome/binding/onewire/internal/CompletenessTest.java
+++ extensions/binding/org.eclipse.smarthome.binding.onewire.test/src/test/java/org/eclipse/smarthome/binding/onewire/internal/CompletenessTest.java
@@ -14,6 +14,7 @@ package org.eclipse.smarthome.binding.onewire.internal;
 
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
@@ -21,22 +22,38 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.smarthome.binding.onewire.internal.device.OwSensorType;
+import org.eclipse.smarthome.binding.onewire.internal.handler.AdvancedMultisensorThingHandler;
+import org.eclipse.smarthome.binding.onewire.internal.handler.BasicMultisensorThingHandler;
+import org.eclipse.smarthome.binding.onewire.internal.handler.CounterSensorThingHandler;
+import org.eclipse.smarthome.binding.onewire.internal.handler.DigitalIOThingHandler;
+import org.eclipse.smarthome.binding.onewire.internal.handler.EDSSensorThingHandler;
+import org.eclipse.smarthome.binding.onewire.internal.handler.IButtonThingHandler;
+import org.eclipse.smarthome.binding.onewire.internal.handler.TemperatureSensorThingHandler;
 import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Tests cases for {@link Util}.
+ * Tests cases for binding completeness
  *
  * @author Jan N. Klug - Initial contribution
  */
 public class CompletenessTest {
     // internal/temporary types, DS2409 (MicroLAN Coupler), DS2431 (EEPROM)
-    private static Set<OwSensorType> IGNORED_SENSOR_TYPES = Stream
+    private static final Set<OwSensorType> IGNORED_SENSOR_TYPES = Collections.unmodifiableSet(Stream
             .of(OwSensorType.DS2409, OwSensorType.DS2431, OwSensorType.EDS, OwSensorType.MS_TH_S, OwSensorType.UNKNOWN)
-            .collect(Collectors.toSet());
-    private static Set<ThingTypeUID> DEPRECATED_THING_TYPES = Stream
-            .of(OwBindingConstants.THING_TYPE_MS_TH, OwBindingConstants.THING_TYPE_MS_TV).collect(Collectors.toSet());
+            .collect(Collectors.toSet()));
+
+    private static final Set<OwSensorType> THINGHANDLER_SENSOR_TYPES = Collections.unmodifiableSet(Stream
+            .of(AdvancedMultisensorThingHandler.SUPPORTED_SENSOR_TYPES,
+                    BasicMultisensorThingHandler.SUPPORTED_SENSOR_TYPES,
+                    CounterSensorThingHandler.SUPPORTED_SENSOR_TYPES, DigitalIOThingHandler.SUPPORTED_SENSOR_TYPES,
+                    EDSSensorThingHandler.SUPPORTED_SENSOR_TYPES, IButtonThingHandler.SUPPORTED_SENSOR_TYPES,
+                    TemperatureSensorThingHandler.SUPPORTED_SENSOR_TYPES)
+            .flatMap(Set::stream).collect(Collectors.toSet()));
+
+    private static final Set<ThingTypeUID> DEPRECATED_THING_TYPES = Collections.unmodifiableSet(Stream
+            .of(OwBindingConstants.THING_TYPE_MS_TH, OwBindingConstants.THING_TYPE_MS_TV).collect(Collectors.toSet()));
 
     @Test
     public void allSupportedTypesInThingHandlerMap() {
@@ -44,6 +61,15 @@ public class CompletenessTest {
             if (!OwBindingConstants.THING_TYPE_MAP.containsKey(sensorType)
                     && !IGNORED_SENSOR_TYPES.contains(sensorType)) {
                 Assert.fail("missing thing type map for sensor type " + sensorType.name());
+            }
+        }
+    }
+
+    @Test
+    public void allSensorsSupportedByThingHandlers() {
+        for (OwSensorType sensorType : EnumSet.allOf(OwSensorType.class)) {
+            if (!THINGHANDLER_SENSOR_TYPES.contains(sensorType) && !IGNORED_SENSOR_TYPES.contains(sensorType)) {
+                Assert.fail("missing thing handler for sensor type " + sensorType.name());
             }
         }
     }
